aaa7090426114509e628334bc8a40530398d2e2a
Edward d'Auvergne <edward@nmr-relax.com>
2002-10-24 01:00:50 +0000

    Updating to the archive 'backup_mf_2002-10-24.tar.gz'.

--- functions/temp_mf_data.py
+++
@@ -1,9 +0,0 @@
-class data:
-	def __init__(self):
-		"""All temporary data used in the mf_functions class.
-		
-		"""
-
-		# Data structures which are required to be initialised.
-		self.relax_test = []
-		self.gradient_test = []
