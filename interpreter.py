1cbbe1c0ecb5d2a1d4d941d2d583e3da69271961
Edward d'Auvergne <edward@nmr-relax.com>
2003-02-07 07:20:32 +0000

    Updating to the archive 'backup_mf_2003-02-07.tar.gz'.

--- interpreter.py
+++ interpreter.py
@@ -1,18 +1,24 @@
 import code
 import readline
-import rlcompleter
+from tab_completion import tab_completion
 
 from macros import macros
 
 class interpreter(macros):
-	def __init__(self, mf):
+	def __init__(self, relax):
 		"The top level class."
 
-		self.mf = mf
-		self.run()
+		# Place the program class structure under self.relax
+		self.relax = relax
+		del relax
 
-	def run(self):
+		# Place the macros in the local namespace.
 		exit = self.exit
 		load = self.load
+
+		# Setup tab completion.
+		readline.set_completer(tab_completion(name_space=locals()).finish)
 		readline.parse_and_bind("tab: complete")
-		code.interact(banner=self.mf.intro_string, local=locals())
+
+		# Go to the prompt.
+		code.interact(banner=self.relax.intro_string, local=locals())
