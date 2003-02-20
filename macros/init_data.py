615d99864ea0473d0593dd34b46b73bc9f360a04
Edward d'Auvergne <edward@nmr-relax.com>
2003-02-20 07:42:00 +0000

    Updating to the archive 'backup_relax_2003-02-20.tar.gz'.

---
+++ macros/init_data.py
@@ -0,0 +1,14 @@
+from data import data
+
+
+class init_data:
+	def __init__(self, relax):
+		"Class containing the macro for reinitialising self.relax.data"
+
+		self.relax = relax
+
+
+	def init(self):
+		"Macro for reinitialising self.relax.data"
+
+		self.relax.data = data()
---
+++ macros/print_all_data.py
@@ -0,0 +1,17 @@
+from generic_functions import generic_functions
+
+
+class print_all_data(generic_functions):
+	def __init__(self, relax):
+		"Class containing the macros for manipulating the program state."
+
+		self.relax = relax
+
+
+	def go(self):
+		"Macro for printing all the data in self.relax.data"
+
+		# Loop over the data structures in self.relax.data
+		for name in dir(self.relax.data):
+			if not self.filter_data_structure(name):
+				print "self.relax.data." + name + ":\n" + `getattr(self.relax.data, name)` + "\n"
