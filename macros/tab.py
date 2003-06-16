27f0c018e043034011de9d7fae159c098261a182
Edward d'Auvergne <edward@nmr-relax.com>
2003-06-16 09:33:30 +0000

    Updating to the archive 'backup_relax_2003-06-16c.tar.gz'.

---
+++ macros/tab.py
@@ -0,0 +1,23 @@
+from re import match
+
+
+class tab:
+	def __init__(self, readline):
+		"The interpreter class."
+
+		self.readline = readline
+
+
+	def run(self, switch):
+		"""Run the python interpreter.
+
+		The namespace of this function is the namespace seen inside the interpreter.
+		All macros should be defined in this namespace.
+		"""
+
+		if match("[Oo][Ff][Ff]", switch):
+			self.readline.parse_and_bind()
+		elif match("[Oo][Nn]", switch):
+			self.readline.parse_and_bind("tab: complete")
+		else:
+			print "Invalid tab option."
