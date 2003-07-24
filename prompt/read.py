2af69b274e88e3963eaf96f696a533a3021ed687
Edward d'Auvergne <edward@nmr-relax.com>
2003-07-24 14:27:50 +0000

    Updating to the archive 'backup_relax_2003-07-25.tar.gz'.

---
+++ prompt/read.py
@@ -0,0 +1,69 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2003 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# Relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# Relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+
+class Write:
+    def __init__(self, relax):
+        """Class containing functions for writing data."""
+
+        self.relax = relax
+
+
+    def write(self, model=None, file="results", force=0):
+        """Function for writing results to a file.
+
+        Keyword Arguments
+        ~~~~~~~~~~~~~~~~~
+
+        model:  The name of the model.
+
+        file:  The name of the file to output results to.  The default is 'results'.
+
+        force:  A flag which if set to 1 will cause the results file to be overwitten if it already
+        exists.
+        """
+
+        # Macro intro text.
+        if self.relax.interpreter.intro:
+            text = self.relax.interpreter.macro_prompt + "write("
+            text = text + "model=" + `model`
+            text = text + ", file=" + `file`
+            text = text + ", force=" + `force` + ")\n"
+            print text
+
+        # The model argument.
+        if type(model) != str:
+            print "The model argument " + `model` + " must be a string."
+            return
+
+        # File.
+        if type(file) != str:
+            print "The file name must be a string."
+            return
+
+        # The force flag.
+        if type(force) != int and force != 0 and force != 1:
+            print "The force flag should be the integer values of either 0 or 1."
+            return
+
+        # Execute the functional code.
+        self.relax.write.write_data(model=model, file=file, force=force)
