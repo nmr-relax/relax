f199c274d7e35a9e2c8d58ad01bdc076c7149f95
Edward d'Auvergne <edward@nmr-relax.com>
2003-08-14 09:04:08 +0000

    Updating to the archive 'backup_relax_2003-08-14.tar.gz'.

---
+++ prompt/select.py
@@ -0,0 +1,189 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2003 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# Relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# Relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+import sys
+
+import message
+
+
+class Skin:
+    def __init__(self, relax):
+        """The class accessible to the interpreter.
+
+        The purpose of this class is to hide the variables and functions found within the namespace
+        of the macro class, found below, except for those required for interactive use.  This is an
+        abstraction layer designed to avoid user confusion as none of the macro class data
+        structures are accessible.  For more flexibility use the macro class directly.
+        """
+
+        # Load the macro class into the namespace of this __init__ function.
+        x = Macro_class(relax)
+
+        # Place references to the interactive functions within the namespace of this skin class.
+        self.all = x.all
+        self.none = x.none
+        self.res = x.res
+        self.reverse = x.reverse
+
+        # __repr__.
+        self.__repr__ = message.macro_class
+
+
+class Macro_class:
+    def __init__(self, relax):
+        """Class containing the macros for selecting and unselecting residues."""
+
+        self.relax = relax
+
+
+    def all(self):
+        """Macro for selection all residues.
+
+        Examples
+        ~~~~~~~~
+
+        To select all residues type:
+
+        relax> selection.all()
+        """
+
+        # Macro intro test.
+        if self.relax.interpreter.intro:
+            text = sys.macro_prompt + "selection.all()"
+            print text
+
+        # Execture the functional code.
+        self.relax.selection.all()
+
+
+    def none(self):
+        """Macro for unselecting all residues.
+
+        Examples
+        ~~~~~~~~
+
+        To have no residues selected type:
+
+        relax> selection.none()
+        """
+
+        # Macro intro test.
+        if self.relax.interpreter.intro:
+            text = sys.macro_prompt + "selection.none()"
+            print text
+
+        # Execture the functional code.
+        self.relax.selection.none()
+
+
+    def res(self, num=None, name=None, unselect=1):
+        """Macro for the selection of specific residues.
+
+        Keyword Arguments
+        ~~~~~~~~~~~~~~~~~
+
+        num:  The residue number.
+
+        name:  The residue name.
+
+        unselect:  A flag specifying if all other residues should be unselected.
+
+        Description
+        ~~~~~~~~~~~
+
+        The residue number can be either an integer for selecting a single residue or a python
+        regular expression in string form for selecting integers.  For details about using regular
+        expression, see the python documentation for the module 're'.
+
+        The residue name argument must be a string.  Regular expression is also allowed.
+
+        If the unselect argument is 0, then the current selection is unmodified and the residues
+        matched using the number and name arguments will be selected.  If on the other hand unselect
+        is 1, then all residues not selected using the number and name arguments will be unselected.
+
+
+        Examples
+        ~~~~~~~~
+
+        To select only glycines and alanines, assuming they have been loaded with the names GLY and
+        ALA, type:
+
+        relax> selection.res(name='GLY|ALA', unselect=1)
+        relax> selection.res(name='[GA]L[YA]', unselect=1)
+
+        To select residue 5 CYS in addition to the currently selected residues, type:
+
+        relax> selection.res(5)
+        relax> selection.res(5, 'CYS')
+        relax> selection.res('5')
+        relax> selection.res('5', 'CYS')
+        relax> selection.res(num='5', name='CYS')
+        """
+
+        # Macro intro test.
+        if self.relax.interpreter.intro:
+            text = sys.macro_prompt + "selection.res("
+            text = text + "num=" + `num`
+            text = text + ", name=" + `name`
+            text = text + ", unselect=" + `unselect` + ")"
+            print text
+
+        # Residue number.
+        if num != None:
+            if type(num) != int and type(num) != str:
+                raise RelaxNoneIntStrError, ('residue number', num)
+
+        # Residue name.
+        if name != None:
+            if type(name) != str:
+                raise RelaxNoneStrError, ('residue name', name)
+
+        # Neither are given.
+        if num == None and name == None:
+            raise RelaxError, "At least one of the number or name arguments is required."
+
+        # Unselect flag.
+        if type(unselect) != int or (unselect != 0 and unselect != 1):
+            raise RelaxBinError, ('unselect', unselect)
+
+        # Execture the functional code.
+        self.relax.selection.res(num=num, name=name, unselect=unselect)
+
+
+    def reverse(self):
+        """Macro for the reversal of the residue selection.
+
+        Examples
+        ~~~~~~~~
+
+        To unselect all currently selected residues and select those which are unselected type:
+
+        relax> selection.reverse()
+        """
+
+        # Macro intro test.
+        if self.relax.interpreter.intro:
+            text = sys.macro_prompt + "selection.reverse()"
+            print text
+
+        # Execture the functional code.
+        self.relax.selection.reverse()
