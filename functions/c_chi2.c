3e386d0bafb0bf28b029166ab1b9eddb64135fb4
Edward d'Auvergne <edward@nmr-relax.com>
2003-12-21 11:03:00 +0000

    Updating to the archive 'backup_relax_2003-12-21a.tar.bz2'.

--- functions/c_chi2.c
+++ functions/c_chi2.c
@@ -3,12 +3,12 @@ Copyright (C) 2003 Edward d'Auvergne
 
 This file is part of the program relax.
 
-Relax is free software; you can redistribute it and/or modify
+relax is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-Relax is distributed in the hope that it will be useful,
+relax is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
