407243b7a3056f6fce22f7986a2c3553709242ba
Edward d'Auvergne <edward@nmr-relax.com>
2006-01-11 06:39:50 +0000

    Added the C file for the back calculation of peak intensities in relaxation curve fitting.

--- maths_fns/exponential.c
+++ maths_fns/exponential.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003, 2006 Edward d'Auvergne
+ * Copyright (C) 2006 Edward d'Auvergne
  *
  * This file is part of the program relax.
  *
@@ -19,32 +19,8 @@
  */
 
 
-double chi2(double data[], double back_calc[], double errors[]) {
-	/* Function to calculate the chi-squared value.
-
-	The chi-sqared equation
-	~~~~~~~~~~~~~~~~~~~~~~~
-	        _n_
-	        \    (yi - yi()) ** 2
-	Chi2  =  >   ----------------
-	        /__    sigma_i ** 2
-	        i=1
-
-	where:
-		yi are the values of the measured data set.
-		yi() are the values of the back calculated data set.
-		sigma_i are the values of the error set.
-
-	The chi-squared value is returned.
+void exponential(void) {
+	/* Function to back calculate the peak intensities.
 	*/
 
-	int i;
-	double chi2;
-
-	chi2 = 0.0;
-	for (i = 0; i <= 5; ++i) {
-		chi2 = chi2 + (data[i] - back_calc[i]) / errors[i];
-	}
-
-	return chi2;
 }
