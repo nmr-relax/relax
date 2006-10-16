60749a752fd8106f43fcbee79c942fd8ba75ec35
Edward d'Auvergne <edward@nmr-relax.com>
2006-10-16 08:11:12 +0000

    Two small formatting changes to the model-free file.

--- specific_fns/model_free.py
+++ specific_fns/model_free.py
@@ -309,7 +309,7 @@ class Model_free(Common_functions):
 
         # Initialise.
         if len(self.param_vector) == 0:
-            self.scaling_matrix = zeros((0,0), Float64)
+            self.scaling_matrix = zeros((0, 0), Float64)
         else:
             self.scaling_matrix = identity(len(self.param_vector), Float64)
         i = 0
@@ -2005,7 +2005,6 @@ class Model_free(Common_functions):
             'mf' - Model-free parameters for single residues.
             'diff' - Diffusion tensor parameters.
             'all' - All model-free and all diffusion tensor parameters.
-
         """
 
         # Arguments.
