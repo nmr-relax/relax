e2e3ef8055ad71a5382ce680f90ba62364155911
Edward d'Auvergne <edward@nmr-relax.com>
2006-11-02 08:15:17 +0000

    Significant improvements and expansion of the 'pdb.create_tensor_pdb()' user function docstring.

--- prompt/pdb.py
+++ prompt/pdb.py
@@ -46,7 +46,7 @@ class PDB:
 
         run:  The run to assign the structure to.
 
-        scale:  Value to scale the diffusion rates into Angstroms.
+        scale:  Value for scaling the diffusion rates.
 
         file:  The name of the PDB file.
 
@@ -58,25 +58,71 @@ class PDB:
         Description
         ~~~~~~~~~~~
 
-        IMPORTANT:  As the units of the Brownian rotational diffusion tensor is the rate of
-        diffusion measured in inverse seconds, the size of the tensor geometric object is hence
-        proportional to the rate and is not a correlation time.  Hence the larger the geometric
-        object, the faster the diffusion of a molecule.  For example the diffusion tensor of a water
-        molecule is much larger than the diffusion tensor of a macromolecule.  Hence, XH bond
-        vectors parallel to the longest axis of the tensor tumble the fastest. 
-
         This function creates a PDB file containing an artificial geometric structure to represent
         the diffusion tensor.  A structure must have previously been read into relax.  The diffusion
         tensor is represented by an ellipsoidal, spheroidal, or spherical geometric object with its
-        origin located at the center of mass (of the selected residues).  This diffusion tensor PDB
+        origin located at the centre of mass (of the selected residues).  This diffusion tensor PDB
         file can subsequently read into any molecular viewer.
 
-        The scaling argument can be used to vary the size of the tensor geometric object.  The
-        default value is 1.8e-6.  For spherical diffusion with a global correlation time of 10 ns
-        (this is equivalent to a Diso diffusion rate of 1.66e7 s^-1), the radius of the sphere then
-        be equal to 30 Angstrom.  When the global correlation time is 30 ns, the radius is 10
-        Angstrom.  If the global correlation time is 3ns, the radius will be 100 Angstrom (hence the
-        scaling may need to be adjusted).
+        As the Brownian rotational diffusion tensor is a measure of the rate of rotation about
+        different axes - the larger the geometric object, the faster the diffusion of a molecule.
+        For example the diffusion tensor of a water molecule is much larger than that of a
+        macromolecule.
+
+        The effective global correlation time experienced by an XH bond vector, not to be confused
+        with the Lipari and Szabo parameter tau_e, will be approximately proportional to the
+        component of the diffusion tensor parallel to it.  The approximation is not exact due to the
+        multiexponential form of the correlation function of Brownian rotational diffusion.  If an
+        XH bond vector is parallel to the longest axis of the tensor, it will be unaffected by
+        rotations about that axis, which are the fastest rotations of the molecule, and therefore
+        its effective global correlation time will be maximal.
+
+        To set the size of the diffusion tensor within the PDB frame the unit vectors used to
+        generate the geometric object are first multiplied by the diffusion tensor (which has the
+        units of inverse seconds) then by the scaling factor (which has the units of second
+        Angstroms and has the default value of 1.8e-6 s.Angstrom).  Therefore the rotational
+        diffusion rate per Angstrom is equal the inverse of the scale value (which defaults to
+        5.55e5 s^-1.Angstrom^-1).  Using the default scaling value for spherical diffusion, the
+        correspondence between global correlation time, Diso diffusion rate, and the radius of the
+        sphere for a number of discrete cases will be:
+
+        _________________________________________________
+        |           |               |                   |
+        | tm (ns)   | Diso (s^-1)   | Radius (Angstrom) |
+        |___________|_______________|___________________|
+        |           |               |                   |
+        | 1         | 1.67e8        | 300               |
+        |           |               |                   |
+        | 3         | 5.55e7        | 100               |
+        |           |               |                   |
+        | 10        | 1.67e7        | 30                |
+        |           |               |                   |
+        | 30        | 5.55e6        | 10                |
+        |___________|_______________|___________________|
+
+        
+        The scaling value has been fixed to facilitate comparisons within or between publications,
+        but can be changed to vary the size of the tensor geometric object if necessary.  Reporting
+        the rotational diffusion rate per Angstrom within figure legends would be useful.
+
+        To create the tensor PDB representation, a number of algorithms are utilised.  Firstly the
+        centre of mass is calculated for the selected residues and is represented in the PDB by a C
+        atom.  Then the axes of the diffusion are calculated, as unit vectors scaled to the
+        appropriate length (multiplied by the eigenvalue Dx, Dy, Dz, Dpar, Dper, or Diso as well as
+        the scale value), and a C atom placed at the position of this vector plus the centre of
+        mass.  Finally a uniform distribution of vectors on a sphere is generated using spherical
+        coordinates.  By incrementing the polar angle using an arccos distribution, a radial array
+        of vectors representing latitude are created while incrementing the azimuthal angle evenly
+        creates the longitudinal vectors.  These unit vectors, which are distributed within the PDB
+        frame and are of 1 Angstrom in length, are first rotated into the diffusion frame using a
+        rotation matrix (the spherical diffusion tensor is not rotated).  Then they are multiplied
+        by the diffusion tensor matrix to extend the vector out to the correct length, and finally
+        multiplied by the scale value so that the vectors reasonably superimpose onto the
+        macromolecular structure.  The last set of algorithms place all this information into a PDB
+        file.  The distribution of vectors are represented by H atoms and are all connected using
+        PDB CONECT records.  Each H atom is connected to its two neighbours on the both the
+        longitude and latitude.  This creates a geometric PDB object with longitudinal and
+        latitudinal lines.
         """
 
         # Function intro text.
