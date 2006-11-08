86f7d85efebbfebea4c3fab85af2d55bb6b5ce7e
Gary Thompson <garyt@nmr-relax.com>
2006-11-08 10:15:47 +0000

    initial checkin of a unit test runner
    note this version only runs one test test_float.py!

---
+++ test_suite/unit_tests/unit_test_runner.py
@@ -0,0 +1,40 @@
+import os,re,unittest,string,sys
+
+class TestFinder:
+    pattern = re.compile('test.*\.py$')
+    suite = unittest.TestSuite()
+    def __init__(self,rootPath=None):
+        self.rootPath = rootPath
+        if self.rootPath == None:
+            self.rootPath = os.getcwd()
+    def importClass(self,name):
+        mod = __import__(name)
+        components = name.split('.')
+        for comp in components[1:]:
+            mod = getattr(mod, comp)
+        return mod     
+    
+    def scanPaths(self,path=None):
+        for (dirpath, dirnames, filenames) in os.walk(self.rootPath):
+             print dirpath, dirnames, filenames
+             for filename in filenames:
+                 if self.pattern.match(filename):
+                     moduleName  = os.path.splitext(filename)[0]
+                     className = string.upper(moduleName[0]) + moduleName[1:]
+                     module  = __import__ (moduleName)
+                     clazz =  getattr(module,className)
+                     
+                     
+                     self.suite = unittest.TestLoader().loadTestsFromTestCase(clazz)
+    #             
+    def run(self):
+        runner = unittest.TextTestRunner()
+        runner.run(self.suite)
+        
+class run_unit_tests(object):
+    def __init__(self,test_directory=None):
+        pass
+if __name__ == '__main__':
+    finder = TestFinder()
+    finder.scanPaths()
+    finder.run()
