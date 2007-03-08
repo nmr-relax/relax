94ae6fc00d8974d1ef32a88c6b04dc0eb8fed2be
Edward d'Auvergne <edward@nmr-relax.com>
2007-03-08 04:09:31 +0000

    Creation of the RunContainer object.
    This file was copied from '__init__.py' as the old top level Data() class setup will be similar to
    the new RunContainer object.  The Data class has been renamed to RunContainer and the Singleton
    setup has been eliminated.  The __all__ list object has also been removed.

---
+++ data/run_container.py
@@ -0,0 +1,78 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2007 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from math import pi
+from re import match, search
+from types import DictType, ListType
+
+# relax module imports.
+from data_classes import Element, Residue, SpecificData
+from diff_tensor import DiffTensorData
+
+
+
+class RunContainer(object):
+    def __init__(self):
+        """Class containing all the program data."""
+
+        # Fundamental constants.
+        #self.h = 6.6260755e-34    # Old low precision value.
+        self.h = 6.62606876e-34
+        self.h_bar = self.h / ( 2.0*pi )
+        self.mu0 = 4.0 * pi * 1e-7
+
+        # PDB data.
+        self.pdb = SpecificData()
+
+        # Diffusion data.
+        self.diff = DiffTensorData()
+
+        # The residue specific data.
+        self.res = Residue()
+
+        # The name of the runs.
+        self.run_names = []
+
+        # The type of the runs.
+        self.run_types = []
+
+        # Hybrid models.
+        self.hybrid_runs = {}
+
+        # Global minimisation statistics.
+        self.chi2 = {}
+        self.iter = {}
+        self.f_count = {}
+        self.g_count = {}
+        self.h_count = {}
+        self.warning = {}
+
+
+    def __repr__(self):
+        text = "The data class containing all permanent program data.\n"
+        text = text + "The class contains the following objects:\n"
+        for name in dir(self):
+            if match("^_", name):
+                continue
+            text = text + "  " + name + ", " + `type(getattr(self, name))` + "\n"
+        return text
