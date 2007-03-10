91ef2415295fa52c6b0981b75dac71aa5b357efc
Edward d'Auvergne <edward@nmr-relax.com>
2007-03-10 01:31:23 +0000

    Updated the import statements, removing many unused imports.

--- test_suite/unit_tests/data/test_diff_tensor.py
+++ test_suite/unit_tests/data/test_diff_tensor.py
@@ -22,13 +22,12 @@
 #                                                                             #
 ###############################################################################
 
+# Python module imports.
 from math import cos, pi, sin
 from Numeric import array, dot, transpose
-from os.path import join, sep
-from string import split
-from sys import path
-from unittest import TestCase, main
+from unittest import TestCase
 
+# relax module imports.
 from data.diff_tensor import DiffTensorElement, DiffTensorSimList
 from relax_errors import RelaxError
 
