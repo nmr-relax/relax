5d76eb3e3d66ca704e49fe913ed67ea1dd7de413
Gary Thompson <garyt@nmr-relax.com>
2007-03-30 07:57:54 +0000

    applied GPL v2 licenses

--- multi/mpi4py_processor.py
+++ multi/mpi4py_processor.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python
+################################################################################
+#                                                                              #
+# Copyright (C) 2007  Gary S Thompson (see https://gna.org/users/varioustoxins #
+#                                      for contact details)                    #
+#                                                                              #
+#                                                                              #
+# This file is part of the program relax.                                      #
+#                                                                              #
+# relax is free software; you can redistribute it and/or modify                #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation; either version 2 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# relax is distributed in the hope that it will be useful,                     #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with relax; if not, write to the Free Software                         #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    #
+#                                                                              #
+################################################################################
 
 #TODO clone communicators & resize
 import sys
@@ -207,8 +229,3 @@ class Mpi4py_processor:
 
 
 
-
-if __name__ == '__main__':
-    test = Mpi4py_processor(None)
-    print test
-    print MPI.rank
