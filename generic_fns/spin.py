1a0911902c223b6336e173fe4cd8ed8fcb00b50c
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-04 19:51:15 +0000

    Updated the copyright notice.

--- generic_fns/spin.py
+++ generic_fns/spin.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2004, 2007 Edward d'Auvergne                             #
+# Copyright (C) 2007 Edward d'Auvergne                                        #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
