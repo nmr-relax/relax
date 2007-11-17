724190b99c30735b8ad9357f38229dd0eb86847f
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-17 10:25:18 +0000

    Seb's patch which fixes bug #10323 (http://gna.org/bugs/?10323) but for the consistency test code.
    The patch can be found at https://mail.gna.org/public/relax-devel/2007-11/msg00016.html.

--- specific_fns/consistency_tests.py
+++ specific_fns/consistency_tests.py
@@ -729,16 +729,18 @@ class Consistency_tests(Common_functions):
             ri_error = []
             if hasattr(self.relax.data, 'num_ri'):
                 for i in xrange(self.relax.data.num_ri[self.run]):
-                    # Find the residue specific data corresponding to i.
-                    index = None
-                    for j in xrange(data.num_ri):
-                        if data.ri_labels[j] == self.relax.data.ri_labels[self.run][i] and data.frq_labels[data.remap_table[j]] == self.relax.data.frq_labels[self.run][self.relax.data.remap_table[self.run][i]]:
-                            index = j
-
-                    # Data exists for this data type.
                     try:
+                        # Find the residue specific data corresponding to i.
+                        index = None
+                        for j in xrange(data.num_ri):
+                            if data.ri_labels[j] == self.relax.data.ri_labels[self.run][i] and data.frq_labels[data.remap_table[j]] == self.relax.data.frq_labels[self.run][self.relax.data.remap_table[self.run][i]]:
+                                index = j
+
+                        # Data exists for this data type.
                         ri.append(`data.relax_data[index]`)
                         ri_error.append(`data.relax_error[index]`)
+
+                    # No data exists for this data type.
                     except:
                         ri.append(None)
                         ri_error.append(None)
@@ -855,20 +857,23 @@ class Consistency_tests(Common_functions):
                 # Relaxation data and errors.
                 ri = []
                 ri_error = []
-                for k in xrange(self.relax.data.num_ri[self.run]):
-                    # Find the residue specific data corresponding to k.
-                    index = None
-                    for l in xrange(data.num_ri):
-                        if data.ri_labels[l] == self.relax.data.ri_labels[self.run][k] and data.frq_labels[data.remap_table[l]] == self.relax.data.frq_labels[self.run][self.relax.data.remap_table[self.run][k]]:
-                            index = l
+                if hasattr(self.relax.data, 'num_ri'):
+                    for k in xrange(self.relax.data.num_ri[self.run]):
+                        try:
+                            # Find the residue specific data corresponding to k.
+                            index = None
+                            for l in xrange(data.num_ri):
+                                if data.ri_labels[l] == self.relax.data.ri_labels[self.run][k] and data.frq_labels[data.remap_table[l]] == self.relax.data.frq_labels[self.run][self.relax.data.remap_table[self.run][k]]:
+                                    index = l
 
-                    # Data exists for this data type.
-                    try:
-                        ri.append(`data.relax_sim_data[i][index]`)
-                        ri_error.append(`data.relax_error[index]`)
-                    except:
-                        ri.append(None)
-                        ri_error.append(None)
+                            # Data exists for this data type.
+                            ri.append(`data.relax_sim_data[i][index]`)
+                            ri_error.append(`data.relax_error[index]`)
+
+                        # No data exists for this data type.
+                        except:
+                            ri.append(None)
+                            ri_error.append(None)
 
                 # Write the line.
                 self.write_columnar_line(file=file, num=data.num, name=data.name, select=data.select, data_set='sim_'+`i`, nucleus=nucleus, wH=`wH`, j0=`j0`, f_eta=`f_eta`, f_r2=`f_r2`, r=`r`, csa=`csa`, orientation=`orientation`, tc=`tc`, ri_labels=ri_labels, remap_table=remap_table, frq_labels=frq_labels, frq=frq, ri=ri, ri_error=ri_error)
