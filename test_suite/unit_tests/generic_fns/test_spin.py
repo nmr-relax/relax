0d94a73c8439ad91ba98add2d5bad758576a945e
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-18 19:05:19 +0000

    Fixed the unit test for renumbering spins.

--- test_suite/unit_tests/generic_fns/test_spin.py
+++ test_suite/unit_tests/generic_fns/test_spin.py
@@ -456,19 +456,17 @@ class Test_spin(TestCase):
         spin.renumber(spin_id='@9', new_number=5)
         spin.renumber(spin_id='@78', new_number=6)
         spin.renumber(spin_id='@239', new_number=7)
-        spin.renumber(spin_id='@None', new_number=8)
         spin.renumber(spin_id='@3239', new_number=9)
 
         # Test that the spins have been renumbered.
-        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[0].name, 1)
-        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[1].name, 2)
-        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[2].name, 3)
-        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[3].name, 4)
-        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[4].name, 5)
-        self.assertEqual(relax_data_store['orig'].mol[0].res[1].spin[0].name, 6)
-        self.assertEqual(relax_data_store['orig'].mol[1].res[0].spin[0].name, 7)
-        self.assertEqual(relax_data_store['orig'].mol[1].res[1].spin[0].name, 8)
-        self.assertEqual(relax_data_store['orig'].mol[1].res[1].spin[1].name, 9)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[0].num, 1)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[1].num, 2)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[2].num, 3)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[3].num, 4)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[0].spin[4].num, 5)
+        self.assertEqual(relax_data_store['orig'].mol[0].res[1].spin[0].num, 6)
+        self.assertEqual(relax_data_store['orig'].mol[1].res[0].spin[0].num, 7)
+        self.assertEqual(relax_data_store['orig'].mol[1].res[1].spin[1].num, 9)
 
 
 
