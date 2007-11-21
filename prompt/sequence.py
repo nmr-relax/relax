dfe221592e83f815b8dcacdc0e74b7eea7803ed9
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-21 23:49:57 +0000

    Bug fix for the prompt.sequence module, the correct RelaxErrors are now imported.

--- prompt/sequence.py
+++ prompt/sequence.py
@@ -26,7 +26,7 @@ import sys
 # relax module imports.
 import help
 from generic_fns import sequence
-from relax_errors import RelaxBinError, RelaxIntError, RelaxNoneStrError, RelaxStrError
+from relax_errors import RelaxBinError, RelaxNoneIntError, RelaxNoneStrError, RelaxStrError
 
 
 class Sequence:
