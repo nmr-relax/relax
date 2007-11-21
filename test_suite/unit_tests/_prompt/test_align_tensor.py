6b14d541c70427bb633b4ddbb8bf4ce43e7f2604
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-21 13:27:01 +0000

    Added the framework for the unit tests of the align_tensor.x() user functions.

---
+++ test_suite/unit_tests/_prompt/test_align_tensor.py
@@ -0,0 +1,50 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2007 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from unittest import TestCase
+import sys
+
+# relax module imports.
+from prompt.molecule import Molecule
+from test_suite.unit_tests.align_tensor_testing_base import Align_tensor_base_class
+
+# Set the variable sys.ps3 (this is required by the user functions).
+sys.ps3 = 'relax> '
+
+
+# A class to act as a container.
+class Container:
+    pass
+
+# Fake normal relax usage of the user function class.
+relax = Container()
+relax.interpreter = Container()
+relax.interpreter.intro = True
+
+
+class Test_align_tensor(Align_tensor_base_class, TestCase):
+    """Unit tests for the functions of the 'prompt.molecule' module."""
+
+    # Instantiate the user function class.
+    align_tensor_fns = Align_tensor(relax)
+ 
