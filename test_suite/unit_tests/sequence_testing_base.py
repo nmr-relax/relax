8584d6ada855fe5d3e851a6ddc02d4cb6e6a1d4e
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-21 22:57:08 +0000

    Added the base class for the unit tests of the prompt.sequence and generic_fns.sequence modules.

---
+++ test_suite/unit_tests/sequence_testing_base.py
@@ -0,0 +1,35 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2007 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# relax module imports.
+from data import Data as relax_data_store
+
+
+
+class Molecule_base_class:
+    """Base class for the tests of both the 'prompt.sequence' and 'generic_fns.sequence' modules.
+
+    This base class also contains many shared unit tests.
+    """
+
+
+
