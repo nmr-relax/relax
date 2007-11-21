237a1c50a10caa32c4534f9334cf743b63f4607f
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-21 13:22:26 +0000

    Added prototype user functions for loading the alignment tensor and its errors.

--- sample_scripts/N_state_model.py
+++ sample_scripts/N_state_model.py
@@ -9,3 +9,7 @@ The N-state model uses alignment tensors as determined by RDC data to determine 
 # Create the data pipe.
 name = 'CaM'
 pipe.create(name, 'N-state')
+
+# Load the alignment tensor.
+align_tensor.init(params=[-8.6322e-05, -5.5786e-04, -3.1732e-05, 2.2927e-05, 2.8599e-04], param_types=0)
+align_tensor.init(params=[3.5e-06, 1.8e-05, 9.2e-06, 1.7e-06, 6.4e-05], param_types=0, errors=1)
