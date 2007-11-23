ea812f23ec62cda59723576152b825590f229821
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-23 12:55:04 +0000

    Updated the copyright notice.

--- test_suite/system_tests/sequence.py
+++ test_suite/system_tests/sequence.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2006 Edward d'Auvergne                                        #
+# Copyright (C) 2006-2007 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
