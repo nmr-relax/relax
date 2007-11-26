460c7d76f9a638380a49d90d2f7ed07b0de42747
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-26 11:42:41 +0000

    Fixed a number of unit tests which were using 'd_scale' rather than 'scale' as an arg.

--- test_suite/unit_tests/align_tensor_testing_base.py
+++ test_suite/unit_tests/align_tensor_testing_base.py
@@ -63,7 +63,7 @@ class Align_tensor_base_class:
         """
 
         # Initialise the tensor.
-        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), d_scale=1.0, angle_units='rad', param_types=1)
+        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), scale=1.0, angle_units='rad', param_types=1)
 
         # Change the current data pipe.
         relax_data_store.current_pipe = 'test'
@@ -87,7 +87,7 @@ class Align_tensor_base_class:
         """
 
         # Initialise the tensor.
-        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), d_scale=1.0, angle_units='rad', param_types=0)
+        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), scale=1.0, angle_units='rad', param_types=0)
 
         # Copy the tensor to the test pipe.
         self.align_tensor_fns.copy(pipe_to='test')
@@ -150,7 +150,7 @@ class Align_tensor_base_class:
         """
 
         # Initialise the tensor.
-        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), d_scale=1.0, angle_units='rad', param_types=0)
+        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), scale=1.0, angle_units='rad', param_types=0)
 
         # Display the alignment tensor.
         self.align_tensor_fns.display()
@@ -200,7 +200,7 @@ class Align_tensor_base_class:
         """
 
         # Initialise the tensor.
-        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), d_scale=1.0, angle_units='rad', param_types=0)
+        self.align_tensor_fns.init(params=(-16.6278, 6.13037, 7.65639, -1.89157, 19.2561), scale=1.0, angle_units='rad', param_types=0)
 
         # Test the alignment tensor.
         self.assertEqual(relax_data_store['test'].align_tensor.Axx, -16.6278)
