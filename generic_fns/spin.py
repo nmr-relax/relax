1b85f2a9150254954faae14869c89c0a972c767a
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-28 09:59:58 +0000

    Bug fix for the generic_fns.spin.copy() function.

--- generic_fns/spin.py
+++ generic_fns/spin.py
@@ -23,7 +23,7 @@
 # relax module imports.
 from data import Data as relax_data_store
 from relax_errors import RelaxError, RelaxNoPipeError, RelaxSpinSelectDisallowError
-from selection import parse_token, residue_loop, return_residue, return_spin, return_single_spin_info, spin_loop, tokenise
+from selection import exists_spin_data, parse_token, residue_loop, return_residue, return_spin, return_single_spin_info, spin_loop, tokenise
 
 
 # Module doc.
@@ -65,8 +65,10 @@ def copy(pipe_from=None, spin_from=None, pipe_to=None, spin_to=None):
     mol_to_token, res_to_token, spin_to_token = tokenise(spin_to)
 
     # Test if the spin number already exists.
-    if return_spin(spin_to, pipe_to):
-        raise RelaxError, "The spin " + `spin_to` + " already exists in the " + `pipe_from` + " data pipe."
+    if spin_to_token:
+        spin_to_cont = return_spin(spin_to, pipe_to)
+        if spin_to_cont and exists_spin_data(spin_to_cont):
+            raise RelaxError, "The spin " + `spin_to` + " already exists in the " + `pipe_from` + " data pipe."
 
     # No residue to copy data from.
     if not return_residue(spin_from, pipe_from):
