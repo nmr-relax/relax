9ef44cd72218804a0dfd187b10f75dace12152d6
Edward d'Auvergne <edward@nmr-relax.com>
2007-12-11 16:21:57 +0000

    Change the import order.

--- generic_fns/nuclei.py
+++ generic_fns/nuclei.py
@@ -25,8 +25,8 @@ from re import match
 
 # relax module imports.
 from data import Data as relax_data_store
-from relax_errors import RelaxInvalidError
 from physical_constants import gC, gH, gN, gO, gP
+from relax_errors import RelaxInvalidError
 
 
 class Nuclei:
