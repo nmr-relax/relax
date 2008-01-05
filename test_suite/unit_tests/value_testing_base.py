c5cdf9cd2bb3df7a8842f5207471d6b3de238cad
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-05 23:37:50 +0000

    Removed all the setting of the homonucleus unit tests.

--- test_suite/unit_tests/value_testing_base.py
+++ test_suite/unit_tests/value_testing_base.py
@@ -2544,26 +2544,6 @@ class Value_base_class:
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'C')
 
 
-    def test_set_jw_all_spins_homonuc(self):
-        """Set the RSDM homonucleus type for all spins.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'jw'.
-        relax_data_store.current_pipe = 'jw'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc', val='H')
-
-        # Test the parameter.
-        self.assertEqual(cdp.mol[0].res[0].spin[0].homonuc, 'H')
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
-
-
     def test_set_jw_all_spins_diff_j0_jwx_jwh(self):
         """Set different RSDM parameters J(0), J(wX), J(wH) for all spins.
 
@@ -2720,26 +2700,6 @@ class Value_base_class:
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'N')
 
 
-    def test_set_jw_defaults_homonuc(self):
-        """Set the RSDM homonucleus type to the default value.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'jw'.
-        relax_data_store.current_pipe = 'jw'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc')
-
-        # Test the parameter.
-        self.assertEqual(cdp.mol[0].res[0].spin[0].homonuc, 'H')
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
-
-
     def test_set_jw_defaults_j0_jwx_jwh(self):
         """Set different RSDM parameters J(0), J(wX), J(wH) to the default values (there are none!).
 
@@ -2874,26 +2834,6 @@ class Value_base_class:
         # Test the parameter.
         self.assert_(not hasattr(cdp.mol[0].res[0].spin[0], 'heteronuc'))
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'C')
-
-
-    def test_set_jw_single_spin_homonuc(self):
-        """Set the RSDM homonucleus type for a single spin.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'jw'.
-        relax_data_store.current_pipe = 'jw'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc', val='H', spin_id='@112')
-
-        # Test the parameter.
-        self.assert_(not hasattr(cdp.mol[0].res[0].spin[0], 'homonuc'))
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
 
 
     def test_set_jw_single_spin_diff_j0_jwx_jwh(self):
@@ -3170,26 +3110,6 @@ class Value_base_class:
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'C')
 
 
-    def test_set_mf_all_spins_homonuc(self):
-        """Set the model-free homonucleus type for all spins.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'mf'.
-        relax_data_store.current_pipe = 'mf'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc', val='H')
-
-        # Test the parameter.
-        self.assertEqual(cdp.mol[0].res[0].spin[0].homonuc, 'H')
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
-
-
     def test_set_mf_all_spins_diff_s2f_s2s(self):
         """Set the model-free S2f and S2s parameters for all spins to different values.
 
@@ -3454,26 +3374,6 @@ class Value_base_class:
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'N')
 
 
-    def test_set_mf_defaults_homonuc(self):
-        """Set the model-free homonucleus type to the default value.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'mf'.
-        relax_data_store.current_pipe = 'mf'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc')
-
-        # Test the parameter.
-        self.assertEqual(cdp.mol[0].res[0].spin[0].homonuc, 'H')
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
-
-
     def test_set_mf_defaults_s2f_s2s(self):
         """Set the model-free S2f and S2s parameters to the default values.
 
@@ -3714,26 +3614,6 @@ class Value_base_class:
         # Test the parameter.
         self.assert_(not hasattr(cdp.mol[0].res[0].spin[0], 'heteronuc'))
         self.assertEqual(cdp.mol[0].res[1].spin[0].heteronuc, 'C')
-
-
-    def test_set_mf_single_spin_homonuc(self):
-        """Set the model-free homonucleus type for a single spin.
-
-        The functions tested are both generic_fns.value.set() and prompt.value.set().
-        """
-
-        # Set the current data pipe to 'mf'.
-        relax_data_store.current_pipe = 'mf'
-
-        # Alias the current data pipe.
-        cdp = relax_data_store[relax_data_store.current_pipe]
-
-        # Set the parameter.
-        self.value_fns.set(param='homonuc', val='H', spin_id='@112')
-
-        # Test the parameter.
-        self.assert_(not hasattr(cdp.mol[0].res[0].spin[0], 'homonuc'))
-        self.assertEqual(cdp.mol[0].res[1].spin[0].homonuc, 'H')
 
 
     def test_set_mf_single_spin_diff_s2f_s2s(self):
