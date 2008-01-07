5c81bb38dd8cdfa3ceea1138b29324d63c88a9df
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-07 10:05:40 +0000

    Created the base class for the unit tests of the prompt and generic_fns minimisation functions.

---
+++ test_suite/unit_tests/minimisation_testing_base.py
@@ -0,0 +1,48 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2008 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# relax module imports.
+from data import Data as relax_data_store
+
+
+class Minimisation_base_class:
+    """Base class for the tests of the minimisation modules.
+    
+    This includes both the 'prompt.minimisation' and 'generic_fns.minimise' modules.
+    The base class also contains many shared unit tests.
+    """
+
+
+    def setUp(self):
+        """Set up for all the minimisation unit tests."""
+
+        # Reset the relax data storage object.
+        relax_data_store.__reset__()
+
+        # Add a data pipe to the data store.
+        relax_data_store.add(pipe_name='orig', pipe_type='mf')
+
+
+    def tearDown(self):
+        """Reset the relax data storage object."""
+
+        relax_data_store.__reset__()
