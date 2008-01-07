3863f72a4ad4a2905cc6ef539fe01c56eb4462f6
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-07 08:45:50 +0000

    Added a missing import statement.

--- specific_fns/n_state_model.py
+++ specific_fns/n_state_model.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2007 Edward d'Auvergne                                        #
+# Copyright (C) 2007-2008 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -19,6 +19,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
 #                                                                             #
 ###############################################################################
+
+# Python module imports.
+from re import search
 
 # relax module imports.
 from data import Data as relax_data_store
