461aae702db06bf358d54d64b59aa076bee812fe
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-07 15:47:41 +0000

    Implemented the grid_search() and overfit_deselect() methods of the N_state_model class.
    grid_search() calls the non-existant minimise() and therefore still fails.

--- specific_fns/n_state_model.py
+++ specific_fns/n_state_model.py
@@ -33,6 +33,40 @@ class N_state_model(Common_functions):
         """Class containing functions for the N-state model."""
 
 
+    def grid_search(self, lower, upper, inc, constraints, print_flag, sim_index=None):
+        """The grid search function.
+
+        @param lower:       The lower bounds of the grid search which must be equal to the number of
+                            parameters in the model.
+        @type lower:        array of numbers
+        @param upper:       The upper bounds of the grid search which must be equal to the number of
+                            parameters in the model.
+        @type upper:        array of numbers
+        @param inc:         The increments for each dimension of the space for the grid search.  The
+                            number of elements in the array must equal to the number of parameters
+                            in the model.
+        @type inc:          array of int
+        @param constraints: If true, constraints are applied during the grid search (elinating parts
+                            of the grid).  If false, no constraints are used.
+        @type constraints:  bool
+        @param print_flag:  A flag specifying the amount of information to print.  The higher the
+                            value, the greater the verbosity.
+        @type print_flag:   int
+        """
+
+        # Arguments.
+        self.lower = lower
+        self.upper = upper
+        self.inc = inc
+
+        # Minimisation.
+        self.minimise(min_algor='grid', constraints=constraints, print_flag=print_flag, sim_index=sim_index)
+
+
+    def overfit_deselect(self):
+        """Dummy function nornally for deselecting spins with insufficient data for minimisation."""
+
+
     def return_data_name(self, name):
         """
         N-state model data type string matching patterns
