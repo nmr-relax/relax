f3aa1789be0546926d92a8580766f1ca2561c2b6
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-07 12:51:39 +0000

    Reverted r4185 and r4186 as these conflict with changes now in the 1.3 line.
    The command used was:
    svn merge -r4186:4184 .

--- generic_fns/nuclei.py
+++ generic_fns/nuclei.py
@@ -25,8 +25,11 @@ from re import match
 
 # relax module imports.
 from data import Data as relax_data_store
-from physical_constants import gC, gH, gN, gO, gP
 from relax_errors import RelaxInvalidError
+
+
+# The relax data storage object.
+
 
 
 class Nuclei:
@@ -47,7 +50,6 @@ class Nuclei:
         if relax_data_store.gx == self.gn():
             return 'N'
 
-
         # Carbon
         if relax_data_store.gx == self.gc():
             return 'C'
@@ -64,31 +66,34 @@ class Nuclei:
     def gc(self):
         """The 13C gyromagnetic ratio."""
 
-        return gC
+        return 6.728e7
 
 
     def gh(self):
         """The 1H gyromagnetic ratio."""
 
-        return gH
+        # Old, low precision gyromagnetic ratio.
+        #return 26.7522e7
+
+        return 26.7522212e7
 
 
     def gn(self):
         """The 15N gyromagnetic ratio."""
 
-        return gN
+        return -2.7126e7
 
 
     def go(self):
         """The 17O gyromagnetic ratio."""
 
-        return gO
+        return -3.628e7
 
 
     def gp(self):
         """The 31P gyromagnetic ratio."""
 
-        return gP
+        return 1.0841e8
 
 
     def set_values(self, heteronuc):
