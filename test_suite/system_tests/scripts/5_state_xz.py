6517740bfc9883c9be9de5627367a208a9226f8c
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-09 14:54:59 +0000

    Changed the 'label' arg back to the original 'tensor' arg.

--- test_suite/system_tests/scripts/5_state_xz.py
+++ test_suite/system_tests/scripts/5_state_xz.py
@@ -17,11 +17,11 @@ pipe.create('test', 'N-state')
 
 
 # Load the C-terminal alignment tensors..
-n_state_model.init(label='chi1', domain='C', red=False, params=(-1/2., -1/2.,  0.,   0.,     0.))
-n_state_model.init(label='chi2', domain='C', red=False, params=(-1/8., -7/8.,  0.,   0.,     0.))
-n_state_model.init(label='chi3', domain='C', red=False, params=(-1/8.,  1/16., 0.,   0.,    -15/16.))
-n_state_model.init(label='chi4', domain='C', red=False, params=(7/16., -7/8.,  0.,   9/16.,  0.))
-n_state_model.init(label='chi5', domain='C', red=False, params=(-1/2., -1/2.,  3/8., 0.,     0.))
+n_state_model.init(tensor='chi1', domain='C', red=False, params=(-1/2., -1/2.,  0.,   0.,     0.))
+n_state_model.init(tensor='chi2', domain='C', red=False, params=(-1/8., -7/8.,  0.,   0.,     0.))
+n_state_model.init(tensor='chi3', domain='C', red=False, params=(-1/8.,  1/16., 0.,   0.,    -15/16.))
+n_state_model.init(tensor='chi4', domain='C', red=False, params=(7/16., -7/8.,  0.,   9/16.,  0.))
+n_state_model.init(tensor='chi5', domain='C', red=False, params=(-1/2., -1/2.,  3/8., 0.,     0.))
 
 # Calculate the singular values.
 n_state_model.svd(basis_set=0, domain='C')
@@ -33,11 +33,11 @@ n_state_model.matrix_angles(basis_set=1, domain='C')
 
 
 # Load the N-terminal alignment tensors.
-n_state_model.init(label='chi1', domain='N', red=True, params=(1/4.,   -1/2.,   0.,              3/4.,   0.))
-n_state_model.init(label='chi2', domain='N', red=True, params=(7/16.,  -7/8.,   0.,              9/16.,  0.))
-n_state_model.init(label='chi3', domain='N', red=True, params=(-1/32.,  1/16., -15/(16*sqrt(2)), 3/32., -15/(16*sqrt(2))))
-n_state_model.init(label='chi4', domain='N', red=True, params=(1.,     -7/8.,   0.,              0.,     0.))
-n_state_model.init(label='chi5', domain='N', red=True, params=(1/4.,   -1/2.,   3/(8*sqrt(2)),   3/4.,  -3/(8*sqrt(2))))
+n_state_model.init(tensor='chi1', domain='N', red=True, params=(1/4.,   -1/2.,   0.,              3/4.,   0.))
+n_state_model.init(tensor='chi2', domain='N', red=True, params=(7/16.,  -7/8.,   0.,              9/16.,  0.))
+n_state_model.init(tensor='chi3', domain='N', red=True, params=(-1/32.,  1/16., -15/(16*sqrt(2)), 3/32., -15/(16*sqrt(2))))
+n_state_model.init(tensor='chi4', domain='N', red=True, params=(1.,     -7/8.,   0.,              0.,     0.))
+n_state_model.init(tensor='chi5', domain='N', red=True, params=(1/4.,   -1/2.,   3/(8*sqrt(2)),   3/4.,  -3/(8*sqrt(2))))
 
 # Calculate the singular values.
 n_state_model.svd(basis_set=0, domain='N')
