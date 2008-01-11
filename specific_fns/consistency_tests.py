e54984c5aa86f6d2864c7ef280b9ec888bc14090
Sébastien Morin <sebastien@nmr-relax.com>
2008-01-11 20:17:09 +0000

    Removed two unused imports.

--- specific_fns/consistency_tests.py
+++ specific_fns/consistency_tests.py
@@ -29,7 +29,7 @@ from string import replace
 from data import Data as relax_data_store
 from base_class import Common_functions
 from maths_fns.consistency_tests import Consistency
-from relax_errors import RelaxError, RelaxFuncSetupError, RelaxNoPipeError, RelaxNoSequenceError, RelaxNoValueError, RelaxNucleusError, RelaxParamSetError
+from relax_errors import RelaxError, RelaxFuncSetupError, RelaxNoPipeError, RelaxNoSequenceError, RelaxNoValueError
 from physical_constants import N15_CSA, NH_BOND_LENGTH
 
 
