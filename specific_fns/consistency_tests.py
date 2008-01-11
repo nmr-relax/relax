ccda42d5746019c210887df25148cdf1a103f12d
Sébastien Morin <sebastien@nmr-relax.com>
2008-01-11 20:45:34 +0000

    Reverted r4656 which was itself a reversion of r4653.
    The command used was:
    svn merge -r4656:4655 .
    
    This import uncovered a problem solved by r4659 and can now be committed again.

--- specific_fns/consistency_tests.py
+++ specific_fns/consistency_tests.py
@@ -28,6 +28,7 @@ from string import replace
 # relax module imports.
 from data import Data as relax_data_store
 from base_class import Common_functions
+from generic_fns.selection import exists_mol_res_spin_data, spin_loop
 from maths_fns.consistency_tests import Consistency
 from relax_errors import RelaxError, RelaxFuncSetupError, RelaxNoPipeError, RelaxNoSequenceError, RelaxNoValueError
 from physical_constants import N15_CSA, NH_BOND_LENGTH
