7cee8e3cf3b2263bd3598453d05b5edd395dc74d
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-13 11:31:20 +0000

    Added the framework for unit tests of the model-free user function args.

---
+++ test_suite/unit_tests/_prompt/test_model_free.py
@@ -0,0 +1,42 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2008 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from unittest import TestCase
+
+# relax module imports.
+from data import Data as relax_data_store
+from prompt.model_free import Model_free
+from test_suite.unit_tests.model_free_testing_base import Model_free_base_class
+
+# Unit test imports.
+from data_types import DATA_TYPES
+import fake_relax
+
+
+class Test_model_free(Model_free_base_class, TestCase):
+    """Unit tests for the functions of the 'prompt.model_free' module."""
+
+    # Instantiate the user function class.
+    model_free_fns = Model_free(fake_relax.fake_instance())
+
+
