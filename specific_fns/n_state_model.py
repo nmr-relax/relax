e3bbc101b69df7678f68d30124c91938a75d3fa3
Edward d'Auvergne <edward@nmr-relax.com>
2008-01-15 10:28:58 +0000

    Simplified the model_setup() method.  The data_init() call and pipe type checking was unnecessary.

--- specific_fns/n_state_model.py
+++ specific_fns/n_state_model.py
@@ -227,14 +227,6 @@ class N_state_model(Common_functions):
         # Alias the current data pipe.
         cdp = relax_data_store[relax_data_store.current_pipe]
 
-        # Test if the pipe type is 'N-state'.
-        function_type = cdp.pipe_type
-        if function_type != 'N-state':
-            raise RelaxFuncSetupError, specific_fns.get_string(function_type)
-
-        # Initialise the data structures (if needed).
-        self.data_init()
-
         # Set the value of N.
         cdp.N = N
         
