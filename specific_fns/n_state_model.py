68e757264a32b33663cc6fe32a5f6fd6aaebcbbd
Edward d'Auvergne <edward@nmr-relax.com>
2008-02-07 16:29:28 +0000

    Added a missing RelaxError import.

--- specific_fns/n_state_model.py
+++ specific_fns/n_state_model.py
@@ -30,7 +30,7 @@ from data import Data as relax_data_store
 from float import isNaN, isInf
 from maths_fns.n_state_model import N_state_opt
 from minfx.generic import generic_minimise
-from relax_errors import RelaxError, RelaxInfError, RelaxNaNError, RelaxNoModelError
+from relax_errors import RelaxError, RelaxInfError, RelaxNaNError, RelaxNoModelError, RelaxNoTensorError
 from specific_fns.base_class import Common_functions
 
 
