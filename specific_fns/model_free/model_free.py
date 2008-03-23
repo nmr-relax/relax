558d3d63b572b42201d59dd6412084268ea06605
Edward d'Auvergne <edward@nmr-relax.com>
2008-03-23 12:42:32 +0000

    Converted the num_params keyword arg in assemble_scaling_matrix() to a normal arg.

--- specific_fns/model_free/model_free.py
+++ specific_fns/model_free/model_free.py
@@ -317,12 +317,12 @@ class Model_free_main:
         return array(param_vector, float64)
 
 
-    def assemble_scaling_matrix(self, num_params=None, spin=None, spin_id=None, scaling=True):
+    def assemble_scaling_matrix(self, num_params, spin=None, spin_id=None, scaling=True):
         """Create and return the scaling matrix.
 
         If the spin argument is supplied, then the spin_id argument will be ignored.
 
-        @keyword num_params:    The number of parameters in the model.
+        @param num_params:      The number of parameters in the model.
         @type num_params:       int
         @keyword spin:          The spin data container.
         @type spin:             SpinContainer instance
