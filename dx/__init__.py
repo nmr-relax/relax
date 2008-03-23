ba0cdd1849b47ea8d722d4ad18c89b95526f3806
Edward d'Auvergne <edward@nmr-relax.com>
2008-03-23 15:53:05 +0000

    Added a package docstring for the OpenDX code.

--- dx/__init__.py
+++ dx/__init__.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003 Edward d'Auvergne                                        #
+# Copyright (C) 2003, 2008 Edward d'Auvergne                                  #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -19,6 +19,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
 #                                                                             #
 ###############################################################################
+
+# Package docstring.
+"""Package relating to the OpenDX data visualisation program."""
 
 
 __all__ = ['base_map',
