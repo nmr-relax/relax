a8a391aec655b147cb1f65f860b3b8abb768fa3e
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-04 09:25:38 +0000

    Started to create the structure package.
    The package is now initialised and has a docstring describing it.  The structure module has also
    been shifted from generic_fns to structure.

---
+++ structure/__init__.py
@@ -0,0 +1,39 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2008 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Package docstring.
+"""This package consists of modules used reading, writing, creating, etc. structural information.
+
+This package will eventually contain numerous structure related operations including:
+
+Numerous PDB parsers,
+A PDB writer,
+CIF parsers,
+Other structural data file parsers,
+Functions for creating geometric objects in structural format,
+Mass and inertia related functions.
+
+This package will eventually have a well defined API for accessing structural data independent of
+the structure parser used.
+"""
+
+__all__ = [ 'structure' ]
