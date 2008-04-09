7d071fae04ea78fdeb1d14ef07867d5f43ec53dc
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-09 15:16:40 +0000

    Added a package docstring.

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2004, 2006-2007 Edward d'Auvergne                        #
+# Copyright (C) 2003-2004, 2006-2008 Edward d'Auvergne                        #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -19,6 +19,10 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
 #                                                                             #
 ###############################################################################
+
+# Module docstring.
+"""Package containing the relax data storage object."""
+
 
 # Python module imports.
 from re import match
