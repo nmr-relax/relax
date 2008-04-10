61554907913ae5f67c33db1798079748e15a09ce
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-10 14:44:54 +0000

    Added a relax script to regenerate 'test_suite/shared_data/saved_states/basic_single_pipe.bz2'.
    This can be used when ever this saved state becomes incompatible with relax.

---
+++ test_suite/shared_data/saved_states/create_state_basic_single_pipe.py
@@ -0,0 +1,18 @@
+# relax script for regenerating the 'basic_single_pipe.bz2' saved state.  This is for when the saved
+# state becomes incompatible with relax.
+
+# The relax data store.
+from data import Data as ds
+
+
+# Add a data pipe to the data store.
+ds.add(pipe_name='orig', pipe_type='mf')
+
+# Add a single object to the 'orig' data pipe.
+ds['orig'].x = 1
+
+# Add a single object to the storage object.
+ds.y = 'Hello'
+
+# Save the state.
+state.save('basic_single_pipe', force=True)
