5fcd0f2fafe9d3c4566a5a3b698fa0d828574a66
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-10 09:28:00 +0000

    Added the 'atom_id' arg to the structural API atom_loop() generator method stub.

--- generic_fns/structure/api_base.py
+++ generic_fns/structure/api_base.py
@@ -88,7 +88,7 @@ class Str_object:
         raise RelaxImplementError
 
 
-    def atom_loop(self, mol_name_flag=False, res_num_flag=False, res_name_flag=False, atom_num_flag=False, atom_name_flag=False, element_flag=False, pos_flag=False):
+    def atom_loop(self, atom_id=None, mol_name_flag=False, res_num_flag=False, res_name_flag=False, atom_num_flag=False, atom_name_flag=False, element_flag=False, pos_flag=False):
         """Prototype generator method stub for looping over all atoms in the structural data object.
 
         This method should be designed as a generator (http://www.python.org/dev/peps/pep-0255/).
@@ -105,6 +105,9 @@ class Str_object:
             7.  The position of the atom in Euclidean space.
 
 
+        @keyword atom_id:           The molecule, residue, and atom identifier string.  Only atoms
+                                    matching this selection will be yielded.
+        @type atom_id:              str
         @keyword mol_name_flag:     A flag which if True will cause the molecule name to be yielded.
         @type mol_name_flag:        bool
         @keyword res_num_flag:      A flag which if True will cause the residue number to be
