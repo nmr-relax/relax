be1dc916c315c521b8f82311c3129ecd928f330a
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-10 09:09:03 +0000

    Made the call to the atom_loop() method in the centre_of_mass() function more robust.
    By setting all the flags, the returned tuple should always be the correct size, even if additional
    atomic data is specified in the API method.

--- generic_fns/structure/mass.py
+++ generic_fns/structure/mass.py
@@ -59,7 +59,7 @@ def centre_of_mass(return_mass=False):
     M = 0.0
 
     # Loop over all atoms.
-    for mol_name, res_num, res_name, atom_num, atom_name, element, pos in cdp.structure.atom_loop():
+    for mol_name, res_num, res_name, atom_num, atom_name, element, pos in cdp.structure.atom_loop(mol_name=True, res_num=True, res_name=True, atom_num=True, atom_name=True, element=True, pos=True):
         # Get the corresponding molecule container.
         if mol_name == None:
             mol_cont = cdp.mol[0]
