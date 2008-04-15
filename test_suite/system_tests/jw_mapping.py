f15f84118ce2a4360d820fbc7c359bc1ea98555f
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-15 21:17:20 +0000

    Fix for the spectral density calculation system test.
    The proton type is now set.

--- test_suite/system_tests/jw_mapping.py
+++ test_suite/system_tests/jw_mapping.py
@@ -75,10 +75,11 @@ class Jw(TestCase):
         for dataSet in xrange(len(dataPaths)):
             self.relax.interpreter._Relax_data.read(dataTypes[dataSet][0], dataTypes[dataSet][1], dataTypes[dataSet][2], dataPaths[dataSet])
 
-        # Set r, csa, and the heteronucleus type.
+        # Set r, csa, heteronucleus type, and proton type.
         self.relax.interpreter._Value.set(NH_BOND_LENGTH, 'bond_length')
         self.relax.interpreter._Value.set(N15_CSA, 'csa')
         self.relax.interpreter._Value.set('15N', 'heteronucleus')
+        self.relax.interpreter._Value.set('1H', 'proton')
 
         # Select the frequency.
         self.relax.interpreter._Jw_mapping.set_frq(frq=600.0 * 1e6)
