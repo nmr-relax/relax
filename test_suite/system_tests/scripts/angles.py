7fe5dd9c4d71f8ab34e64fc1f37fe892a96f678f
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-20 10:29:25 +0000

    Fixed the angles.py script used in the system tests.
    The heteronuc arg to structure.vectors() has been removed.

--- test_suite/system_tests/scripts/angles.py
+++ test_suite/system_tests/scripts/angles.py
@@ -11,7 +11,7 @@ structure.read_pdb(file='test.pdb', dir=sys.path[-1] + '/test_suite/system_tests
 structure.load_spins('@N')
 
 # Set the NH vector.
-structure.vectors(heteronuc='N', proton='H')
+structure.vectors(proton='H')
 
 # Initialise a diffusion tensor.
 diffusion_tensor.init((1.698e7, 1.417e7, 67.174, -83.718), param_types=3)
