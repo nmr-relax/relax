6ca6e8eac731af4b33c6f15395af8856006d44aa
Edward d'Auvergne <edward@nmr-relax.com>
2008-04-24 15:20:18 +0000

    Fixed the paths to the data files in the relaxation curve fitting script.

--- test_suite/system_tests/scripts/relax_fit.py
+++ test_suite/system_tests/scripts/relax_fit.py
@@ -1,28 +1,34 @@
 # Script for relaxation curve fitting.
 
+import sys
+
+
 # Create the data pipe.
 pipe.create('rx', 'relax_fit')
 
+# The path to the data files.
+data_path = sys.path[-1] + '/test_suite/shared_data/curve_fitting'
+
 # Load the sequence.
-sequence.read('Ap4Aase.seq', dir='..')
+sequence.read('Ap4Aase.seq', dir=sys.path[-1] + '/test_suite/system_tests/data')
 
 # Load the peak intensities.
-relax_fit.read(file='T2_ncyc1_ave.list', relax_time=0.0176)
-relax_fit.read(file='T2_ncyc1b_ave.list', relax_time=0.0176)
-relax_fit.read(file='T2_ncyc2_ave.list', relax_time=0.0352)
-relax_fit.read(file='T2_ncyc4_ave.list', relax_time=0.0704)
-relax_fit.read(file='T2_ncyc4b_ave.list', relax_time=0.0704)
-relax_fit.read(file='T2_ncyc6_ave.list', relax_time=0.1056)
-relax_fit.read(file='T2_ncyc9_ave.list', relax_time=0.1584)
-relax_fit.read(file='T2_ncyc9b_ave.list', relax_time=0.1584)
-relax_fit.read(file='T2_ncyc11_ave.list', relax_time=0.1936)
-relax_fit.read(file='T2_ncyc11b_ave.list', relax_time=0.1936)
+relax_fit.read(file='T2_ncyc1_ave.list', dir=data_path, relax_time=0.0176)
+relax_fit.read(file='T2_ncyc1b_ave.list', dir=data_path, relax_time=0.0176)
+relax_fit.read(file='T2_ncyc2_ave.list', dir=data_path, relax_time=0.0352)
+relax_fit.read(file='T2_ncyc4_ave.list', dir=data_path, relax_time=0.0704)
+relax_fit.read(file='T2_ncyc4b_ave.list', dir=data_path, relax_time=0.0704)
+relax_fit.read(file='T2_ncyc6_ave.list', dir=data_path, relax_time=0.1056)
+relax_fit.read(file='T2_ncyc9_ave.list', dir=data_path, relax_time=0.1584)
+relax_fit.read(file='T2_ncyc9b_ave.list', dir=data_path, relax_time=0.1584)
+relax_fit.read(file='T2_ncyc11_ave.list', dir=data_path, relax_time=0.1936)
+relax_fit.read(file='T2_ncyc11b_ave.list', dir=data_path, relax_time=0.1936)
 
 # Calculate the peak intensity averages and the standard deviation of all spectra.
 relax_fit.mean_and_error()
 
 # Unselect unresolved residues.
-unselect.read(file='unresolved')
+unselect.read(file='unresolved', dir=data_path)
 
 # Set the relaxation curve type.
 relax_fit.select_model('exp')
