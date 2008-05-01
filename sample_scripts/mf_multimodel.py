6c81a7c8dfcf97fd01aae4d95ad84baa222cef89
Edward d'Auvergne <edward@nmr-relax.com>
2008-05-01 14:51:48 +0000

    Added a copyright statement to the mf_multimodel.py sample script.
    The purpose of this is so the user knows who the author of the script was and how ancient the script
    is, as well as allowing the file to be redistributable.

--- sample_scripts/mf_multimodel.py
+++ sample_scripts/mf_multimodel.py
@@ -1,4 +1,27 @@
-# Script for model-free analysis.
+###############################################################################
+#                                                                             #
+# Copyright (C) 2003-2008 Edward d'Auvergne                                   #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# This script performs a model-free analysis for the models 'm0' to 'm9' (or 'tm0' to 'tm9').
+#############################################################################################
 
 # Set the run names (also the names of preset model-free models).
 #runs = ['tm0', 'tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8', 'tm9']
