37d09da292a64aa83096eedcad8073e0540c370e
Edward d'Auvergne <edward@nmr-relax.com>
2008-05-01 14:54:31 +0000

    Updated the mf_multimodel.py sample script to the new relax design.

--- sample_scripts/mf_multimodel.py
+++ sample_scripts/mf_multimodel.py
@@ -23,47 +23,46 @@
 # This script performs a model-free analysis for the models 'm0' to 'm9' (or 'tm0' to 'tm9').
 #############################################################################################
 
-# Set the run names (also the names of preset model-free models).
-#runs = ['tm0', 'tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8', 'tm9']
-runs = ['m0', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9']
+# Set the data pipe names (also the names of preset model-free models).
+#pipes = ['tm0', 'tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8', 'tm9']
+pipes = ['m0', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9']
 
-# Nuclei type
-nuclei('N')
-
-# Loop over the runs.
-for name in runs:
-    # Create the run.
+# Loop over the pipes.
+for name in pipes:
+    # Create the data pipe.
     pipe.create(name, 'mf')
 
     # Load the sequence.
-    sequence.read(name, 'noe.500.out')
+    sequence.read('noe.500.out')
 
     # Load a PDB file.
-    #structure.read_pdb(name, 'example.pdb')
+    #structure.read_pdb('example.pdb')
 
     # Load the relaxation data.
-    relax_data.read(name, 'R1', '600', 600.0 * 1e6, 'r1.600.out')
-    relax_data.read(name, 'R2', '600', 600.0 * 1e6, 'r2.600.out')
-    relax_data.read(name, 'NOE', '600', 600.0 * 1e6, 'noe.600.out')
-    relax_data.read(name, 'R1', '500', 500.0 * 1e6, 'r1.500.out')
-    relax_data.read(name, 'R2', '500', 500.0 * 1e6, 'r2.500.out')
-    relax_data.read(name, 'NOE', '500', 500.0 * 1e6, 'noe.500.out')
+    relax_data.read('R1', '600', 600.0 * 1e6, 'r1.600.out')
+    relax_data.read('R2', '600', 600.0 * 1e6, 'r2.600.out')
+    relax_data.read('NOE', '600', 600.0 * 1e6, 'noe.600.out')
+    relax_data.read('R1', '500', 500.0 * 1e6, 'r1.500.out')
+    relax_data.read('R2', '500', 500.0 * 1e6, 'r2.500.out')
+    relax_data.read('NOE', '500', 500.0 * 1e6, 'noe.500.out')
 
     # Setup other values.
-    diffusion_tensor.init(name, 1e-8, fixed=1)
-    #diffusion_tensor.init(name, (1e-8, 1.0, 60, 290), param_types=0, spheroid_type='oblate', fixed=0)
-    value.set(name, 1.02 * 1e-10, 'bond_length')
-    value.set(name, -172 * 1e-6, 'csa')
+    diffusion_tensor.init(1e-8, fixed=True)
+    #diffusion_tensor.init((1e-8, 1.0, 60, 290), param_types=0, spheroid_type='oblate', fixed=True)
+    value.set(1.02 * 1e-10, 'bond_length')
+    value.set(-172 * 1e-6, 'csa')
+    value.set('15N', 'heteronucleus')
+    value.set('1H', 'proton')
 
     # Select the model-free model.
-    model_free.select_model(run=name, model=name)
+    model_free.select_model(model=name)
 
     # Minimise.
-    grid_search(name, inc=11)
-    minimise('newton', run=name)
+    grid_search(inc=11)
+    minimise('newton')
 
     # Write the results.
-    results.write(run=name, file='results', force=1)
+    results.write(file='results', force=True)
 
 # Save the program state.
-state.save('save', force=1)
+state.save('save', force=True)
