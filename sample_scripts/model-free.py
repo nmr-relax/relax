94be87a0590c3c9e57b32959300d162c0cbe14b4
Edward d'Auvergne <edward@nmr-relax.com>
2008-05-01 14:43:01 +0000

    Updated the model-free.py script to the new relax design.

--- sample_scripts/model-free.py
+++ sample_scripts/model-free.py
@@ -31,54 +31,54 @@ pipe.create(name, 'mf')
 nuclei('N')
 
 # Load the sequence.
-sequence.read(name, 'noe.500.out')
+sequence.read('noe.500.out')
 
 # Load a PDB file.
-#structure.read_pdb(name, 'example.pdb')
+#structure.read_pdb('example.pdb')
 
 # Load the relaxation data.
-relax_data.read(name, 'R1', '600', 600.0 * 1e6, 'r1.600.out')
-relax_data.read(name, 'R2', '600', 600.0 * 1e6, 'r2.600.out')
-relax_data.read(name, 'NOE', '600', 600.0 * 1e6, 'noe.600.out')
-relax_data.read(name, 'R1', '500', 500.0 * 1e6, 'r1.500.out')
-relax_data.read(name, 'R2', '500', 500.0 * 1e6, 'r2.500.out')
-relax_data.read(name, 'NOE', '500', 500.0 * 1e6, 'noe.500.out')
+relax_data.read('R1', '600', 600.0 * 1e6, 'r1.600.out')
+relax_data.read('R2', '600', 600.0 * 1e6, 'r2.600.out')
+relax_data.read('NOE', '600', 600.0 * 1e6, 'noe.600.out')
+relax_data.read('R1', '500', 500.0 * 1e6, 'r1.500.out')
+relax_data.read('R2', '500', 500.0 * 1e6, 'r2.500.out')
+relax_data.read('NOE', '500', 500.0 * 1e6, 'noe.500.out')
 
 # Setup other values.
-diffusion_tensor.init(name, 10e-9, fixed=1)
-#diffusion_tensor.init(name, (2e-8, 1.3, 60, 290), param_types=0, spheroid_type='prolate', fixed=1)
-#diffusion_tensor.init(name, (9e-8, 0.5, 0.3, 60, 290, 100), fixed=0)
-value.set(name, 1.02 * 1e-10, 'bond_length')
-value.set(name, -172 * 1e-6, 'csa')
-#value.set(name, 1.0, 's2f')
-#value.set(name, 0.970, 's2')
-#value.set(name, 2048e-12, 'te')
-#value.set(name, 2048e-12, 'ts')
-#value.set(name, 2048e-12, 'tf')
-#value.set(name, 0.149/(2*pi*600e6)**2, 'rex')
+diffusion_tensor.init(10e-9, fixed=True)
+#diffusion_tensor.init((2e-8, 1.3, 60, 290), param_types=0, spheroid_type='prolate', fixed=True)
+#diffusion_tensor.init((9e-8, 0.5, 0.3, 60, 290, 100), fixed=False)
+value.set(1.02 * 1e-10, 'bond_length')
+value.set(-172 * 1e-6, 'csa')
+#value.set(1.0, 's2f')
+#value.set(0.970, 's2')
+#value.set(2048e-12, 'te')
+#value.set(2048e-12, 'ts')
+#value.set(2048e-12, 'tf')
+#value.set(0.149/(2*pi*600e6)**2, 'rex')
 
 # Select the model-free model.
-model_free.select_model(run=name, model=name)
-#model_free.create_model(run=name, model=name, equation='mf_ext2', params=['S2f', 'S2s', 'ts'])
+model_free.select_model(model=name)
+#model_free.create_model(model=name, equation='mf_ext2', params=['S2f', 'S2s', 'ts'])
 
 # Fixed value.
-#fix(name, 'all_res')
+#fix('all_res')
 
 # Grid search.
-grid_search(name, inc=11)
-#value.set(name)
+grid_search(inc=11)
+#value.set()
 
 # Minimise.
-minimise('newton', run=name)
+minimise('newton')
 
 # Monte Carlo simulations.
-#monte_carlo.setup(name, number=100)
-#monte_carlo.create_data(name)
-#monte_carlo.initial_values(name)
-#minimise('newton', run=name)
-#eliminate(run=name)
-#monte_carlo.error_analysis(name)
+#monte_carlo.setup(number=100)
+#monte_carlo.create_data()
+#monte_carlo.initial_values()
+#minimise('newton')
+#eliminate()
+#monte_carlo.error_analysis()
 
 # Finish.
-results.write(run=name, file='results', force=1)
-state.save('save', force=1)
+results.write(file='results', force=True)
+state.save('save', force=True)
