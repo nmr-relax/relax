1ff92719d32775d2904c8470adcdbf3464045109
Edward d'Auvergne <edward@nmr-relax.com>
2008-05-22 15:47:14 +0000

    Fixed the basic_heights_T2_ncyc1 saved state.

--- test_suite/shared_data/saved_states/create_peak_heights_T2_ncyc1.py
+++ test_suite/shared_data/saved_states/create_peak_heights_T2_ncyc1.py
@@ -14,9 +14,11 @@ ds.add(pipe_name='rx', pipe_type='relax_fit')
 # Load the Lupin Ap4Aase sequence.
 sequence.read(file="Ap4Aase.seq", dir=sys.path[-1] + "/test_suite/system_tests/data")
 
+# Name the spins so they can be matched to the assignments.
+spin.name(name='N')
+
 # Read the peak heights.
 relax_fit.read(file="T2_ncyc1_ave.list", dir=sys.path[-1] + "/test_suite/shared_data/curve_fitting", relax_time=0.0176)
-
 
 # Save the state.
 state.save('basic_heights_T2_ncyc1', force=True)
