71c6c7a85d95a22a493e6eea3c461034211ff937
Edward d'Auvergne <edward@nmr-relax.com>
2008-05-22 13:12:35 +0000

    Wrote a unit test for the resetting of an unpickled and restored relax data storage singleton.

--- test_suite/unit_tests/state_testing_base.py
+++ test_suite/unit_tests/state_testing_base.py
@@ -75,7 +75,7 @@ class State_base_class:
         # Load the state.
         self.state.load_state(state='basic_single_pipe', dir_name=path+'/test_suite/shared_data/saved_states')
 
-        # Test the contents of the restored singleton (with subsequent data added).
+        # Test the contents of the restored singleton.
         self.assertEqual(relax_data_store.keys(), ['orig'])
         self.assertEqual(relax_data_store.current_pipe, 'orig')
         self.assertEqual(relax_data_store['orig'].x, 1)
@@ -112,6 +112,34 @@ class State_base_class:
         self.assertEqual(relax_data_store['orig'].x, 1)
         self.assertEqual(relax_data_store.y, 'Hello')
         self.assertEqual(relax_data_store['new'].z, [None, None])
+
+
+    def test_load_and_reset(self):
+        """The resetting of an unpickled and restored relax data storage singleton.
+
+        This tests the normal operation of the generic_fns.state.load() function.
+        """
+
+        # Test the contents of the empty singleton.
+        self.assertEqual(relax_data_store.keys(), [])
+        self.assertEqual(relax_data_store.current_pipe, None)
+        self.assert_(not hasattr(relax_data_store, 'y'))
+
+        # Get the relative path of relax.
+        path = sys.path[0]
+        if path == '.':
+            path = sys.path[-1]
+
+        # Load the state.
+        self.state.load_state(state='basic_single_pipe', dir_name=path+'/test_suite/shared_data/saved_states')
+
+        # Reset.
+        relax_data_store.__reset__()
+
+        # Test that there are no contents in the reset singleton.
+        self.assertEqual(relax_data_store.keys(), [])
+        self.assertEqual(relax_data_store.current_pipe, None)
+        self.assert_(not hasattr(relax_data_store, 'y'))
 
 
     def test_save(self):
