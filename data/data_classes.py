c7e62c99dd1105021c406b842adce3887060c387
Edward d'Auvergne <edward@nmr-relax.com>
2008-06-22 12:57:56 +0000

    Deleted the orphaned Residue and ResidueList classes.

--- data/data_classes.py
+++ data/data_classes.py
@@ -22,8 +22,6 @@
 
 # Python module imports.
 from re import search
-from types import DictType, ListType
-
 
 
 # Empty data container.
@@ -75,52 +73,3 @@ class Element:
 
         # The Element container is empty.
         return True
-
-
-
-# Residue specific data.
-########################
-
-class Residue(DictType):
-    """Class containing all the residue specific data."""
-
-
-    def __repr__(self):
-        text = "Class containing all the residue specific data.\n\n"
-
-        # Empty.
-        if not len(self):
-            text = text + "The class contains no data.\n"
-
-        # Not empty.
-        else:
-            text = text + "The residue container contains the following keys:\n"
-            for key in self:
-                text = text + "    " + `key` + "\n"
-            text = text + "\nThese can be accessed by typing 'ds.res[key]'.\n"
-
-        return text
-
-
-    def add_list(self, key):
-        """Function for adding an empty container to the dictionary."""
-
-        self[key] = ResidueList()
-
-
-class ResidueList(ListType):
-    """Empty data container for residue specific data."""
-
-    def __repr__(self):
-        text = "Sequence data.\n\n"
-        text = text + "%-8s%-8s%-8s%-10s" % ("Index", "Number", "Name", "Selected") + "\n"
-        for i in xrange(len(self)):
-            text = text + "%-8i%-8i%-8s%-10i" % (i, self[i].num, self[i].name, self[i].select) + "\n"
-        text = text + "\nThese can be accessed by typing 'ds.res[key][index]'.\n"
-        return text
-
-
-    def add_item(self):
-        """Function for appending an empty container to the list."""
-
-        self.append(Element())
