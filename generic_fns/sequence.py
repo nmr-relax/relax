0d1ada75ab381c6416ef583d26364a42b28caaf5
Edward d'Auvergne <edward@nmr-relax.com>
2008-06-29 12:43:07 +0000

    Missing deepcopy import.

--- generic_fns/sequence.py
+++ generic_fns/sequence.py
@@ -20,6 +20,9 @@
 #                                                                             #
 ###############################################################################
 
+# Python module imports.
+from copy import deepcopy
+
 # relax module imports.
 from data import Relax_data_store; ds = Relax_data_store()
 from generic_fns.mol_res_spin import count_spins, exists_mol_res_spin_data, generate_spin_id, return_molecule, return_residue, return_spin, spin_loop
