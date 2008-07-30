da5b38aa30a2b4f5044780bf898e28f077bbce7f
Sébastien Morin <sebastien@nmr-relax.com>
2008-07-30 18:09:59 +0000

    Removed a deprecated run argument.

--- generic_fns/grace.py
+++ generic_fns/grace.py
@@ -186,11 +186,10 @@ class Grace:
         system(grace_exe + " " + self.file_path + " &")
 
 
-    def write(self, run=None, x_data_type='res', y_data_type=None, res_num=None, res_name=None, plot_data='value', norm=1, file=None, dir=None, force=False):
+    def write(self, x_data_type='res', y_data_type=None, res_num=None, res_name=None, plot_data='value', norm=1, file=None, dir=None, force=False):
         """Function for writing data to a file."""
 
         # Arguments.
-        self.run = run
         self.x_data_type = x_data_type
         self.y_data_type = y_data_type
         self.res_num = res_num
