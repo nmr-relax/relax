cb3a227831bff314a5fb16199670db4668748b48
Edward d'Auvergne <edward@nmr-relax.com>
2008-08-07 17:53:10 +0000

    Removed a useless function.

--- generic_fns/pcs.py
+++ generic_fns/pcs.py
@@ -713,30 +713,6 @@ def return_value(i, data_type):
     return value, error
 
 
-def test_labels(ri_label, frq_label):
-    """Test if data corresponding to 'ri_label' and 'frq_label' currently exists.
-
-    @return:        The answer to the question of whether relaxation data exists corresponding to
-                    the given labels.
-    @rtype:         bool
-    """
-
-    # Loop over the spins.
-    for spin in spin_loop():
-        # No ri data.
-        if not hasattr(spin, 'num_ri'):
-            continue
-
-        # Loop over the relaxation data.
-        for j in xrange(spin.num_ri):
-            # Test if the relaxation data matches 'ri_label' and 'frq_label'.
-            if ri_label == spin.ri_labels[j] and frq_label == spin.frq_labels[spin.remap_table[j]]:
-                return True
-
-    # No match.
-    return False
-
-
 def update_data_structures_pipe(ri_label=None, frq_label=None, frq=None):
     """Function for updating all relaxation data structures in the current data pipe.
 
