dedbf2571e57807f098e94f960055b6f12786cd9
Edward d'Auvergne <edward@nmr-relax.com>
2008-08-11 09:49:53 +0000

    Added a stub function generic_fns.temperature.set() to fix the missing import problem.

---
+++ generic_fns/temperature.py
@@ -0,0 +1,38 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2008 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Module docstring.
+"""Module for setting the experimental temperature."""
+
+# relax module imports.
+from data import Relax_data_store; ds = Relax_data_store()
+
+
+def set(id=None, temp=None):
+    """Set the experimental temperature.
+
+    @keyword id:    The experimental identification string (allowing for multiple experiments per
+                    data pipe).
+    @type id:       str
+    @keyword temp:  The temperature in kelvin.
+    @type temp:     float
+    """
