8c5c22022cbad1609316cce822c00c5a00e4c7c6
Edward d'Auvergne <edward@nmr-relax.com>
2008-08-12 12:34:12 +0000

    Fixes for all the N-state model unit tests.

--- test_suite/unit_tests/_specific_fns/test_n_state_model.py
+++ test_suite/unit_tests/_specific_fns/test_n_state_model.py
@@ -37,8 +37,8 @@ class Test_n_state_model(N_state_model_base_class, TestCase):
     n_state_model_fns = n_state_model.N_state_model()
 
 
-    def test_assemble_param_vector(self):
-        """Test the operation of the specific_fns.n_state_model.assemble_param_vector() method."""
+    def test___assemble_param_vector(self):
+        """Test the operation of the specific_fns.n_state_model.__assemble_param_vector() method."""
 
         # Alias the current data pipe.
         cdp = ds[ds.current_pipe]
@@ -51,7 +51,7 @@ class Test_n_state_model(N_state_model_base_class, TestCase):
         cdp.gamma = [1.0, 3*pi/2, 2*pi]
 
         # Get the parameter vector.
-        param_vector = self.n_state_model_fns.assemble_param_vector()
+        param_vector = self.n_state_model_fns._N_state_model__assemble_param_vector()
 
         # The correct result.
         vector_true = [0.1, 0.3, 0.0, pi/2, 1.0, pi/2, pi, 3*pi/2, pi, 3*pi/2, 2*pi]
@@ -62,8 +62,8 @@ class Test_n_state_model(N_state_model_base_class, TestCase):
             self.assertEqual(param_vector[i], vector_true[i])
 
 
-    def test_disassemble_param_vector(self):
-        """Test the operation of the specific_fns.n_state_model.disassemble_param_vector() method."""
+    def test___disassemble_param_vector(self):
+        """Test the operation of the specific_fns.n_state_model.__disassemble_param_vector() method."""
 
         # Alias the current data pipe.
         cdp = ds[ds.current_pipe]
@@ -79,7 +79,7 @@ class Test_n_state_model(N_state_model_base_class, TestCase):
         param_vector = [0.1, 0.3, 0.0, pi/2, 1.0, pi/2, pi, 3*pi/2, pi, 3*pi/2, 2*pi]
 
         # Disassemble the parameter vector.
-        self.n_state_model_fns.disassemble_param_vector(param_vector)
+        self.n_state_model_fns._N_state_model__disassemble_param_vector(param_vector)
 
         # Check the probabilities.
         self.assertEqual(cdp.probs, [0.1, 0.3, 0.6])
