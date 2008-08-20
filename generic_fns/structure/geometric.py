efc39226770a646fc7e84f70483deffe9bbcb53f
Edward d'Auvergne <edward@nmr-relax.com>
2008-08-20 18:27:46 +0000

    Fixed some more calls to the structural method atom_add().

--- generic_fns/structure/geometric.py
+++ generic_fns/structure/geometric.py
@@ -90,7 +90,7 @@ def cone_edge(structure=None, res_name='CON', res_num=None, apex=None, axis=None
     """
 
     # Add an atom for the cone apex.
-    structure.atom_add(pdb_record='HETATM', atom_num=apex, atom_name='APX', res_name=res_name_vect, chain_id=chain_id, res_num=res_num, pos=apex, segment_id=None, element='H', struct_index=None)
+    structure.atom_add(pdb_record='HETATM', atom_num=apex, atom_name='APX', res_name=res_name, res_num=res_num, pos=apex, segment_id=None, element='H', struct_index=None)
 
     # Initialise the rotation matrix, atom number, etc.
     if R == None:
@@ -128,7 +128,7 @@ def cone_edge(structure=None, res_name='CON', res_num=None, apex=None, axis=None
         pos = apex+vector*length
 
         # Add the vector as a H atom of the cone residue.
-        structure.atom_add(pdb_record='HETATM', atom_num=atom_id, atom_name='H'+`atom_num`, res_name=res_name, chain_id=chain_id, res_num=res_num, pos=pos, segment_id=None, element='H', struct_index=None)
+        structure.atom_add(pdb_record='HETATM', atom_num=atom_id, atom_name='H'+`atom_num`, res_name=res_name, res_num=res_num, pos=pos, segment_id=None, element='H', struct_index=None)
 
         # Connect across the radial array (to generate the circular cone edge).
         if i != 0:
