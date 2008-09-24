ebc054a77ff6a5d78574252f44938fb386d0798b
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-24 17:49:02 +0000

    Deleted the set_selected_sim() methods, as the base class method is sufficient.

--- specific_fns/jw_mapping.py
+++ specific_fns/jw_mapping.py
@@ -27,7 +27,7 @@ from string import replace
 # relax module imports.
 from base_class import Common_functions
 from data import Relax_data_store; ds = Relax_data_store()
-from generic_fns.mol_res_spin import exists_mol_res_spin_data, return_spin, return_spin_from_index, spin_loop
+from generic_fns.mol_res_spin import exists_mol_res_spin_data, return_spin, spin_loop
 from maths_fns.jw_mapping import Mapping
 from physical_constants import N15_CSA, NH_BOND_LENGTH, h_bar, mu0, return_gyromagnetic_ratio
 from relax_errors import RelaxError, RelaxFuncSetupError, RelaxNoPipeError, RelaxNoSequenceError, RelaxNoValueError, RelaxNucleusError, RelaxParamSetError, RelaxProtonTypeError, RelaxSpinTypeError
@@ -466,23 +466,6 @@ class Jw_mapping(Common_functions):
 
         # Multiple spins.
         return spin.select_sim
-
-
-    def set_selected_sim(self, model_index, select_sim):
-        """Set the array of selected simulation flags.
-
-        @param model_index: The global spin index, covering the molecule, residue, and spin
-                            indices).
-        @type model_index:  int
-        @param select_sim:  The selection flags.
-        @type select_sim:   bool
-        """
-
-        # Get the spin container.
-        spin = return_spin_from_index(model_index)
-
-        # Set the simulation flags.
-        spin.select_sim = select_sim
 
 
     def sim_pack_data(self, spin_id, sim_data):
