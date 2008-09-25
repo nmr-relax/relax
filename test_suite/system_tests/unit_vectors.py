dcce971b0ef43761b112cd820236263fe101fda1
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-25 14:42:37 +0000

    Added another system test for the calculation of unit XH vectors, but with spin numbers removed.

--- test_suite/system_tests/unit_vectors.py
+++ test_suite/system_tests/unit_vectors.py
@@ -65,3 +65,29 @@ class Unit_vectors(TestCase):
         self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[0], 0.40899187)
         self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[1], -0.80574458)
         self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[2], 0.42837054)
+
+
+    def test_calc_unit_vectors2(self):
+        """Load the PDB file and calculate the XH unit vectors from it (with spin numbers removed)."""
+
+        # Read the PDB file.
+        self.relax.interpreter._Structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir=sys.path[-1] + '/test_suite/shared_data/structures', model=1)
+
+        # Load the spins.
+        self.relax.interpreter._Structure.load_spins(spin_id='@N')
+        
+        # Remove the spin numbers.
+        self.relax.interpreter._Spin.number()
+
+        # Calculate the unit vectors.
+        self.relax.interpreter._Structure.vectors(attached='H')
+
+        # Alias the current data pipe.
+        cdp = ds[ds.current_pipe]
+
+        # Leu 3.
+        self.assert_(hasattr(cdp.mol[0].res[2].spin[0], 'xh_vect'))
+        self.assertNotEqual(cdp.mol[0].res[2].spin[0].xh_vect, None)
+        self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[0], 0.40899187)
+        self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[1], -0.80574458)
+        self.assertAlmostEqual(cdp.mol[0].res[2].spin[0].xh_vect[2], 0.42837054)
