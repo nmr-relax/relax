5a8caa5cb14ce6d7df3910459b822a01c4b46b07
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-25 16:19:41 +0000

    Bug fix for the loading of results files when they contain not alignment tensor data.
    The bug was in the PipeContainer.from_xml() method.

--- data/pipe_container.py
+++ data/pipe_container.py
@@ -125,8 +125,10 @@ class PipeContainer(Prototype):
 
         # Get the alignment tensor data nodes and, if they exist, fill the contents.
         align_tensor_super_node = relax_node.getElementsByTagName('align_tensors')
-        align_tensor_nodes = align_tensor_super_node[0].getElementsByTagName('align_tensor')
-        if align_tensor_nodes:
+        if align_tensor_super_node:
+            # Get the individual tensors.
+            align_tensor_nodes = align_tensor_super_node[0].getElementsByTagName('align_tensor')
+
             # Create the diffusion tensor object.
             self.align_tensors = AlignTensorList()
 
