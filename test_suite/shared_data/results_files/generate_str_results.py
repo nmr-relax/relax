911d722a60a6a99000704db19aa1b27314f6668d
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-26 15:20:21 +0000

    Changed the path to the PDB file to not be absolute.

--- test_suite/shared_data/results_files/generate_str_results.py
+++ test_suite/shared_data/results_files/generate_str_results.py
@@ -1,4 +1,4 @@
 import sys
 pipe.create('str', 'mf')
-structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir=sys.path[-1] + '/test_suite/shared_data/structures')
+structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir='../structures')
 results.write('str', dir=None, force=True)
