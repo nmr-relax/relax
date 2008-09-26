7c705816c693707f44ec49d94d3f8743018bcf62
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-26 13:31:50 +0000

    Added a script to generate a results file containing a single PDB structure.
    The results file has been added too.

---
+++ test_suite/shared_data/results_files/generate_str_results.py
@@ -0,0 +1,4 @@
+import sys
+pipe.create('str', 'mf')
+structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir=sys.path[-1] + '/test_suite/shared_data/structures')
+results.write('str', dir=None, force=True)
