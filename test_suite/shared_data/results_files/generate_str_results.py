d10243a330be4b8fde54b4ab7c587b2df53afadd
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-26 15:43:27 +0000

    Switched to the internal structural object.

--- test_suite/shared_data/results_files/generate_str_results.py
+++ test_suite/shared_data/results_files/generate_str_results.py
@@ -1,4 +1,4 @@
 import sys
 pipe.create('str', 'mf')
-structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir='../structures')
+structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir='../structures', parser='internal')
 results.write('str', dir=None, force=True)
