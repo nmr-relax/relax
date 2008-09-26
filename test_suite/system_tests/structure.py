b7ba5a4f59b30c017507207da322eee18bcdea68
Edward d'Auvergne <edward@nmr-relax.com>
2008-09-26 16:45:47 +0000

    Fixed the test_load_results() system test.

--- test_suite/system_tests/structure.py
+++ test_suite/system_tests/structure.py
@@ -58,6 +58,5 @@ class Structure(TestCase):
 
         # Test the structure.
         self.assert_(hasattr(cdp, 'structure'))
-        self.assertEqual(cdp.structure.file, '[Ap4Aase_res1-12.pdb]')
-        self.assert_(cdp.structure.path, '[../structures]')
-
+        self.assertEqual(cdp.structure.file, ['Ap4Aase_res1-12.pdb'])
+        self.assertEqual(cdp.structure.path, ['../structures'])
