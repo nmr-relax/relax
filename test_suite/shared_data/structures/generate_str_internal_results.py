48d941e40c6219c890dcdb6554febf2d971c1d84
Edward d'Auvergne <edward@nmr-relax.com>
2008-10-05 11:04:19 +0000

    Created the new test_load_scientific_results() system test.
    This test checks the loading of a PDB file using the Scientific python PDB internal object when
    reading a results file.  The test_load_results() system test has been renamed to
    test_load_internal_results().

--- test_suite/shared_data/structures/generate_str_internal_results.py
+++ test_suite/shared_data/structures/generate_str_internal_results.py
@@ -1,4 +1,4 @@
 import sys
 pipe.create('str', 'mf')
 structure.read_pdb(file='Ap4Aase_res1-12.pdb', parser='internal')
-results.write('str', dir=None, force=True)
+results.write('str_internal', dir=None, force=True)
---
+++ test_suite/shared_data/structures/generate_str_scientific_results.py
@@ -0,0 +1,4 @@
+import sys
+pipe.create('str', 'mf')
+structure.read_pdb(file='Ap4Aase_res1-12.pdb', parser='scientific')
+results.write('str_scientific', dir=None, force=True)
