1b7c0e7967ef29d5c35123ffcde482d7f4faca61
Edward d'Auvergne <edward@nmr-relax.com>
2008-10-05 10:19:51 +0000

    Fix for the test_tm_51ns() system test.
    The model 'tm4' is set rather than 'm4'.

--- test_suite/system_tests/scripts/local_tm_model_elimination.py
+++ test_suite/system_tests/scripts/local_tm_model_elimination.py
@@ -8,7 +8,7 @@ import sys
 sequence.read(file='Ap4Aase.Noe.600', dir=sys.path[-1] + '/test_suite/shared_data/relaxation_data/')
 
 # Select the model.
-model_free.select_model(model='m4')
+model_free.select_model(model='tm4')
 
 # Set a local tm value
 value.set(51 * 1e-9, 'local_tm', spin_id=":15")
