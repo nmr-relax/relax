9f06c9e2b09ef6a30c9727974ce32ceb31c8c398
Edward d'Auvergne <edward@nmr-relax.com>
2008-10-07 19:34:11 +0000

    Fixes for the system test NOE analysis script.

--- test_suite/system_tests/scripts/noe.py
+++ test_suite/system_tests/scripts/noe.py
@@ -1,4 +1,5 @@
 # Script for calculating NOEs.
+import sys
 
 # Create the NOE data pipe.
 pipe.create('NOE', 'noe')
@@ -7,16 +8,16 @@ pipe.create('NOE', 'noe')
 sequence.read(file='Ap4Aase.seq', dir=sys.path[-1] + '/test_suite/shared_data')
 
 # Load the reference spectrum and saturated spectrum peak intensities.
-noe.read(file='ref.list', spectrum_type='ref')
-noe.read(file='sat.list', spectrum_type='sat')
+noe.read(file='ref_ave.list', dir=sys.path[-1] + '/test_suite/shared_data/peak_lists', spectrum_type='ref')
+noe.read(file='sat_ave.list', dir=sys.path[-1] + '/test_suite/shared_data/peak_lists', spectrum_type='sat')
 
 # Set the errors.
 noe.error(error=3600, spectrum_type='ref')
 noe.error(error=3000, spectrum_type='sat')
 
 # Individual residue errors.
-noe.error(error=122000, spectrum_type='ref', res_num=114)
-noe.error(error=8500, spectrum_type='sat', res_num=114)
+noe.error(error=122000, spectrum_type='ref', res_num=5)
+noe.error(error=8500, spectrum_type='sat', res_num=5)
 
 # Deselect unresolved residues.
 deselect.read(file='unresolved', dir=sys.path[-1] + '/test_suite/shared_data/curve_fitting')
@@ -25,7 +26,7 @@ deselect.read(file='unresolved', dir=sys.path[-1] + '/test_suite/shared_data/cur
 calc()
 
 # Save the NOEs.
-value.write(param='noe', file='noe.out', force=True)
+value.write(param='noe', file='devnull', force=True)
 
 # Create grace files.
 grace.write(y_data_type='ref', file='devnull', force=True)
