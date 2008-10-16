753999b1bd21e210b1166422ca51355c71352b5f
Edward d'Auvergne <edward@nmr-relax.com>
2008-10-16 15:31:43 +0000

    Spacing fixes in the multi-processor import.

--- specific_fns/model_free/mf_minimise.py
+++ specific_fns/model_free/mf_minimise.py
@@ -33,7 +33,7 @@ from generic_fns.diffusion_tensor import diff_data_exists
 from generic_fns.mol_res_spin import count_spins, exists_mol_res_spin_data, return_spin_from_index, spin_loop
 from maths_fns.mf import Mf
 from minfx.generic import generic_minimise
-from multi.commands import  MF_grid_command,MF_super_grid_memo,MF_grid_memo
+from multi.commands import MF_grid_command, MF_super_grid_memo, MF_grid_memo
 from physical_constants import h_bar, mu0, return_gyromagnetic_ratio
 from relax_errors import RelaxError, RelaxInfError, RelaxLenError, RelaxNaNError, RelaxNoModelError, RelaxNoPdbError, RelaxNoResError, RelaxNoSequenceError, RelaxNoTensorError, RelaxNoValueError, RelaxNoVectorsError, RelaxNucleusError, RelaxProtonTypeError, RelaxSpinTypeError
 
