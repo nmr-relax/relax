36508d9b2329ddbd75599c38f8c2ce7ec2e97efa
Edward d'Auvergne <edward@nmr-relax.com>
2008-11-20 22:40:45 +0000

    Modified the noe.py sample script to use the new 'spectrum' user functions.
    This copies the changes of r7994 which were made to the equivalent system test.

--- sample_scripts/noe.py
+++ sample_scripts/noe.py
@@ -32,16 +32,16 @@ structure.read_pdb('Ap4Aase_new_3.pdb')
 structure.load_spins(spin_id='@N')
 
 # Load the reference spectrum and saturated spectrum peak intensities.
-noe.read(file='ref.list', spectrum_type='ref')
-noe.read(file='sat.list', spectrum_type='sat')
+spectrum.read_intensities(file='ref.list', spectrum_id='ref')
+spectrum.read_intensities(file='sat.list', spectrum_id='sat')
 
 # Set the errors.
-noe.error(error=3600, spectrum_type='ref')
-noe.error(error=3000, spectrum_type='sat')
+spectrum.baseplane_rmsd(error=3600, spectrum_id='ref')
+spectrum.baseplane_rmsd(error=3000, spectrum_id='sat')
 
 # Individual residue errors.
-noe.error(error=122000, spectrum_type='ref', spin_id=":114")
-noe.error(error=8500, spectrum_type='sat', spin_id=":114")
+spectrum.baseplane_rmsd(error=122000, spectrum_id='ref', spin_id=":114")
+spectrum.baseplane_rmsd(error=8500, spectrum_id='sat', spin_id=":114")
 
 # Deselect unresolved residues.
 deselect.read(file='unresolved')
