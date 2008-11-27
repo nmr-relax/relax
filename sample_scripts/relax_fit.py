a31031144aa544edacfc5beef41a43a7338f4550
Edward d'Auvergne <edward@nmr-relax.com>
2008-11-27 17:14:00 +0000

    Fixes for the relax_fit.py sample script.
    This includes fixes for spectrum.read_intensities() and the use of spectrum.replicated().

--- sample_scripts/relax_fit.py
+++ sample_scripts/relax_fit.py
@@ -62,10 +62,16 @@ times = [
 # Loop over the spectra.
 for i in xrange(len(names)):
     # Load the peak intensities.
-    spectrum.read_intensities(file=names[i]+'.list', dir=data_path, spectrum_id=names[i], int_method='heights')
+    spectrum.read_intensities(file=names[i]+'.list', dir=data_path, spectrum_id=names[i], int_method='height')
 
     # Set the relaxation times.
     relax_fit.relax_time(time=times[i], spectrum_id=names[i])
+
+# Specify the duplicated spectra.
+spectrum.replicated(spectrum_ids=['T2_ncyc1_ave', 'T2_ncyc1b_ave'])
+spectrum.replicated(spectrum_ids=['T2_ncyc4_ave', 'T2_ncyc4b_ave'])
+spectrum.replicated(spectrum_ids=['T2_ncyc9_ave', 'T2_ncyc9b_ave'])
+spectrum.replicated(spectrum_ids=['T2_ncyc11_ave', 'T2_ncyc11b_ave'])
 
 # Peak intensity error analysis.
 spectrum.error_analysis()
