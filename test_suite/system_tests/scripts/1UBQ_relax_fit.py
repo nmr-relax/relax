9bfda4498a88356ed73323aa2697559b51cfa8d6
Edward d'Auvergne <edward@nmr-relax.com>
2008-12-03 13:12:31 +0000

    Added all the files necessary to reproduce bug #12670 (https://gna.org/bugs/index.php?12670).
    This bug was reported by Michael Funk (mifu att nmr dot mpibpc dot mpg dot de).
    
    The script needs to be modified to point to the correct locations and needs to be activated as a
    system test.

---
+++ test_suite/system_tests/scripts/1UBQ_relax_fit.py
@@ -0,0 +1,89 @@
+# Script for catching bug #12670 (https://gna.org/bugs/index.php?12670) as submitted by Michael Funk (mifu att nmr dot mpibpc dot mpg dot de).
+
+# Create the 'rx' data pipe.
+pipe.create('rx', 'relax_fit')
+
+# Load the sequence.
+sequence.read('1UBQ.seq', mol_name_col=0, res_num_col=1, res_name_col=2, spin_num_col=3, spin_name_col=4)
+
+# Spectrum names.
+names = [
+    '700mhz-263k-1m-02',
+    '700mhz-263k-1m-04',
+    '700mhz-263k-1m-06',
+    '700mhz-263k-1m-08',
+    '700mhz-263k-1m-10',
+    '700mhz-263k-1m-12',
+    '700mhz-263k-1m-14',
+    '700mhz-263k-1m-16'
+    ]
+
+# Relaxation times (in seconds).
+times = [
+    0.004,
+    0.008,
+    0.012,
+    0.016,
+    0.020,
+    0.024,
+    0.028,
+    0.032
+]
+
+#maf# Definte data path
+data_path = '.'
+
+# Loop over the spectra.
+for i in xrange(len(names)):
+    # Load the peak intensities.
+    spectrum.read_intensities(file=names[i]+'.listb', dir=data_path, spectrum_id=names[i], int_method='height', proton='H')
+
+    # Set the relaxation times.
+    relax_fit.relax_time(time=times[i], spectrum_id=names[i])
+
+# Specify the duplicated spectra.
+
+#maf# Spectrum baseplane noise for non-duplicated spectra
+spectrum.baseplane_rmsd(error=20570, spectrum_id='700mhz-263k-1m-02', spin_id=None)
+spectrum.baseplane_rmsd(error=20570, spectrum_id='700mhz-263k-1m-04', spin_id=None)
+spectrum.baseplane_rmsd(error=18125, spectrum_id='700mhz-263k-1m-06', spin_id=None)
+spectrum.baseplane_rmsd(error=15560, spectrum_id='700mhz-263k-1m-08', spin_id=None)
+spectrum.baseplane_rmsd(error=15652, spectrum_id='700mhz-263k-1m-10', spin_id=None)
+spectrum.baseplane_rmsd(error=16500, spectrum_id='700mhz-263k-1m-12', spin_id=None)
+spectrum.baseplane_rmsd(error=16000, spectrum_id='700mhz-263k-1m-14', spin_id=None)
+spectrum.baseplane_rmsd(error=16700, spectrum_id='700mhz-263k-1m-16', spin_id=None)
+
+# Peak intensity error analysis.
+spectrum.error_analysis()
+
+# Set the relaxation curve type.
+relax_fit.select_model('exp')
+
+# Grid search.
+grid_search(inc=11)
+
+# Minimise.
+minimise('simplex', scaling=False, constraints=False)
+
+# Monte Carlo simulations.
+monte_carlo.setup(number=5)
+monte_carlo.create_data()
+monte_carlo.initial_values()
+minimise('simplex', scaling=False, constraints=False)
+monte_carlo.error_analysis()
+
+# Save the relaxation rates.
+value.write(param='rx', file='devnull', force=True)
+
+# Save the results.
+results.write(file='devnull', force=True)
+
+# Create Grace plots of the data.
+grace.write(y_data_type='chi2', file='devnull', force=True)    # Minimised chi-squared value.
+grace.write(y_data_type='i0', file='devnull', force=True)    # Initial peak intensity.
+grace.write(y_data_type='rx', file='devnull', force=True)    # Relaxation rate.
+grace.write(x_data_type='relax_times', y_data_type='int', file='devnull', force=True)    # Average peak intensities.
+grace.write(x_data_type='relax_times', y_data_type='int', norm=True, file='devnull', force=True)    # Average peak intensities (normalised).
+
+# Save the program state.
+state.save('devnull', force=True)
