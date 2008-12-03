8ec767fb86ea68d643e7a09f7742ff7c6ca303aa
Edward d'Auvergne <edward@nmr-relax.com>
2008-12-03 13:22:44 +0000

    Activated the system test catching bug #12670 (https://gna.org/bugs/index.php?12670).
    This bug was reported by Michael Funk (mifu att nmr dot mpibpc dot mpg dot de).

--- test_suite/system_tests/relax_fit.py
+++ test_suite/system_tests/relax_fit.py
@@ -46,6 +46,13 @@ class Relax_fit(TestCase):
         ds.__reset__()
 
 
+    def test_bug_12670(self):
+        """Test the relaxation curve fitting, replicating bug #12670 (https://gna.org/bugs/index.php?12670)."""
+
+        # Execute the script.
+        self.relax.interpreter.run(script_file=sys.path[-1] + '/test_suite/system_tests/scripts/1UBQ_relax_fit.py')
+
+
     def test_curve_fitting(self):
         """Test the relaxation curve fitting C modules."""
 
