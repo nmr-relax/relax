fadde35b51540ae92ffe7d01908dc42c68d392ac
Edward d'Auvergne <edward@nmr-relax.com>
2008-12-05 15:05:49 +0000

    Wrote 7 arg unit tests for the as of yet non-existent noe.read() user function.

--- test_suite/unit_tests/_prompt/test_noe.py
+++ test_suite/unit_tests/_prompt/test_noe.py
@@ -39,6 +39,97 @@ class Test_noe(TestCase):
     noe_fns = Noe(fake_relax.fake_instance())
 
 
+    def test_read_argfail_file(self):
+        """The file arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the str argument, and skip it.
+            if data[0] == 'str':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxStrError, self.noe_fns.read, file=data[1])
+
+
+    def test_read_argfail_dir(self):
+        """The dir arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the None and str arguments, and skip them.
+            if data[0] == 'None' or data[0] == 'str':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxNoneStrError, self.noe_fns.read, file='noes', dir=data[1])
+
+
+    def test_read_argfail_proton1_col(self):
+        """The proton1_col arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the None, int, and bin arguments, and skip them.
+            if data[0] == 'None' or data[0] == 'int' or data[0] == 'bin':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxNoneIntError, self.noe_fns.read, file='noes', proton1_col=data[1])
+
+
+    def test_read_argfail_proton2_col(self):
+        """The proton2_col arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the None, int, and bin arguments, and skip them.
+            if data[0] == 'None' or data[0] == 'int' or data[0] == 'bin':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxNoneIntError, self.noe_fns.read, file='noes', proton2_col=data[1])
+
+
+    def test_read_argfail_lower_col(self):
+        """The lower_col arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the int and bin arguments, and skip them.
+            if data[0] == 'int' or data[0] == 'bin':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxIntError, self.noe_fns.read, file='noes', lower_col=data[1])
+
+
+    def test_read_argfail_upper_col(self):
+        """The upper_col arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the int and bin arguments, and skip them.
+            if data[0] == 'int' or data[0] == 'bin':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxIntError, self.noe_fns.read, file='noes', upper_col=data[1])
+
+
+    def test_read_argfail_sep(self):
+        """The sep arg test of the noe.read() user function."""
+
+        # Loop over the data types.
+        for data in DATA_TYPES:
+            # Catch the None and str arguments, and skip them.
+            if data[0] == 'None' or data[0] == 'str':
+                continue
+
+            # The argument test.
+            self.assertRaises(RelaxNoneStrError, self.noe_fns.read, file='noes', sep=data[1])
+
+
     def test_spectrum_type_argfail_spectrum_type(self):
         """The spectrum_type arg test of the noe.spectrum_type() user function."""
 
