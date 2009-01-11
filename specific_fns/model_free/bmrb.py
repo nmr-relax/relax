238cfa898d7c234903705d2fb8dc7744af4cc46c
Edward d'Auvergne <edward@nmr-relax.com>
2009-01-11 13:01:28 +0000

    Added a stub bmrb_write() model-free method.

---
+++ specific_fns/model_free/bmrb.py
@@ -0,0 +1,32 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+
+class Bmrb:
+    """Class containing methods related to BMRB STAR file reading and writing."""
+
+    def bmrb_write(self, file):
+        """Write the model-free results to a BMRB NMR-STAR v3.1 formatted file.
+
+        @param file:    The writable file object.
+        @type file:     file object
+        """
