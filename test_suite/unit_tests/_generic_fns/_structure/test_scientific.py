f38849d014571927b5a9c53070e3930ef3bcb4f3
Edward d'Auvergne <edward@nmr-relax.com>
2009-01-15 10:32:19 +0000

    Fixes for the Scientific python structural object __residue_loop() unit tests.

--- test_suite/unit_tests/_generic_fns/_structure/test_scientific.py
+++ test_suite/unit_tests/_generic_fns/_structure/test_scientific.py
@@ -133,7 +133,7 @@ class Test_scientific(TestCase):
 
         # Loop over the residues.
         res_count = 0
-        for res, res_num, res_name in self.data._Scientific_data__residue_loop(self.data.structural_data[0].peptide_chains[0], None, 'protein'):
+        for res, res_num, res_name, res_index in self.data._Scientific_data__residue_loop(self.data.structural_data[0].peptide_chains[0]):
             res_count = res_count + 1
 
         # Test the number of residues looped over.
@@ -157,7 +157,7 @@ class Test_scientific(TestCase):
 
         # Loop over the residues.
         res_count = 0
-        for res, res_num, res_name in self.data._Scientific_data__residue_loop(self.data.structural_data[0].peptide_chains[0], 'Ap4Aase', 'protein', sel_obj):
+        for res, res_num, res_name, res_index in self.data._Scientific_data__residue_loop(self.data.structural_data[0].mol[0], sel_obj):
             res_count = res_count + 1
 
         # Test the number of residues looped over.
@@ -181,7 +181,7 @@ class Test_scientific(TestCase):
 
         # Loop over the residues.
         res_count = 0
-        for res, res_num, res_name in self.data._Scientific_data__residue_loop(self.data.structural_data[0].peptide_chains[0], None, 'protein', sel_obj):
+        for res, res_num, res_name, res_index in self.data._Scientific_data__residue_loop(self.data.structural_data[0].mol[0], sel_obj):
             res_count = res_count + 1
 
         # Test the number of residues looped over.
