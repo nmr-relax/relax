09badce2b3f897915354668a683813de38729b22
Edward d'Auvergne <edward@nmr-relax.com>
2009-01-15 10:53:55 +0000

    Fix for the test_load_pdb() unit test, in the check of the molecule name.

--- test_suite/unit_tests/_generic_fns/_structure/test_internal.py
+++ test_suite/unit_tests/_generic_fns/_structure/test_internal.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2008 Edward d'Auvergne                                        #
+# Copyright (C) 2008-2009 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -221,7 +221,7 @@ class Test_internal(TestCase):
         self.assertEqual(len(self.data.structural_data), 1)
         self.assertEqual(len(model.mol), 1)
         self.assertEqual(model.num, 1)
-        self.assertEqual(mol.mol_name, self.test_pdb_root+'_mol1')
+        self.assertEqual(mol.name, self.test_pdb_root+'_mol1')
         self.assertEqual(mol.file_name, self.test_pdb_file_name)
         self.assertEqual(mol.file_path, self.test_pdb_dir)
         self.assertEqual(mol.file_model, 1)
