016f5451aaa68ab2eeecbbc6529d56291dbf5af6
Edward d'Auvergne <edward@nmr-relax.com>
2009-01-19 14:45:26 +0000

    Added a missing import.

--- generic_fns/structure/api_base.py
+++ generic_fns/structure/api_base.py
@@ -35,7 +35,7 @@ from warnings import warn
 
 # relax module import.
 from data.relax_xml import fill_object_contents, xml_to_object
-from relax_errors import RelaxError, RelaxFileError, RelaxImplementError
+from relax_errors import RelaxError, RelaxFileError, RelaxFromXMLNotEmptyError, RelaxImplementError
 from relax_io import file_root
 from relax_warnings import RelaxWarning
 
