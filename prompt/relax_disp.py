1be3d2a50a1a1bc8e643ff40ef2de13557769381
Sébastien Morin <sebastien@nmr-relax.com>
2009-02-02 22:55:37 +0000

    Removed the obsolete function 'relax_disp.r2eff_read()'.
    R2eff values will be read directly by 'relax_data.read()' (see r8702 and r8703).

--- prompt/relax_disp.py
+++ prompt/relax_disp.py
@@ -251,22 +251,6 @@ class Relax_disp:
         relax_disp_obj.exp_type(exp_type=exp_type)
 
 
-    def r2eff_read(self):
-        """Function for reading 'R2eff' values directly, instead of calculating them within relax
-        starting from the intensities.
-
-        THIS FUNCTION IS NOT WRITTEN YET.
-
-        Keyword Arguments
-        ~~~~~~~~~~~~~~~~~
-
-
-        Examples
-        ~~~~~~~~
-
-        """
-
-
     def select_model(self, model='fast'):
         """Function for the selection of the relaxation dispersion curve type.
 
