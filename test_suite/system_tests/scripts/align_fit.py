b7a0f7c08c225ead68f8b350c7079e42cf86094d
Edward d'Auvergne <edward@nmr-relax.com>
2009-02-17 09:56:07 +0000

    Modified the NH bond length to be 1.041 Angstrom in the system test script.
    This is from Ottiger, M. and Bax A., J. Am. Chem. Soc. (1998), 120, 12334-12341.

--- test_suite/system_tests/scripts/align_fit.py
+++ test_suite/system_tests/scripts/align_fit.py
@@ -30,7 +30,7 @@ structure.load_spins()
 structure.vectors(spin_id='@N', attached='H', ave=False)
 
 # Set the values needed to calculate the dipolar constant.
-value.set(1.02 * 1e-10, 'bond_length', spin_id="@N")
+value.set(1.041 * 1e-10, 'bond_length', spin_id="@N")
 value.set('15N', 'heteronucleus', spin_id="@N")
 value.set('1H', 'proton', spin_id="@N")
 
