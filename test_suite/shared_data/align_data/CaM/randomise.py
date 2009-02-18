7d492324c643879de8d0744bea2725ed47000ef8
Edward d'Auvergne <edward@nmr-relax.com>
2009-02-18 13:12:54 +0000

    Created a Pales input file for the randomised RDCs.
    This randomised the data again!

--- test_suite/shared_data/align_data/CaM/randomise.py
+++ test_suite/shared_data/align_data/CaM/randomise.py
@@ -16,6 +16,16 @@ pcs_file = open('synth_pcs')
 rdc_out = open('synth_rdc_rand', 'w')
 pcs_out = open('synth_pcs_rand', 'w')
 
+# Open the Pales input file.
+pales_file = open('pales_rand.in', 'w')
+
+# The Pales header.
+pales_file.write("DATA SEQUENCE ADQLTEEQIAEFKEAFSLFDKDGDGTITTKELGTVMRSLGQNPTEAELQDMINEVDADGNGTIDFPEFLT\n")
+pales_file.write("DATA SEQUENCE MMARKMKDTDSEEEIREAFRVFDKDGNGYISAAELRHVMTNLGEKLTDEEVDEMIREANIDGDGQVNYEE\n")
+pales_file.write("DATA SEQUENCE FVQMMTAK\n\n")
+pales_file.write("VARS   RESID_I RESNAME_I ATOMNAME_I RESID_J RESNAME_J ATOMNAME_J D      DD    W\n")
+pales_file.write("FORMAT %5d     %6s       %6s        %5d     %6s       %6s    %9.3f   %9.3f %.2f\n\n")
+
 # Loop over the RDC data.
 for line in rdc_file.readlines():
     # Split the line up.
@@ -26,6 +36,10 @@ for line in rdc_file.readlines():
 
     # Write the line out.
     rdc_out.write("%20s%10s%10s%10s%10s%30.11f\n" % (row[0], row[1], row[2], row[3], row[4], val))
+
+    # The Pales data line (equal weight, no errors).
+    pales_file.write("%5s     %6s       %6s        %5s     %6s       %6s    %9.3f   %9.3f %.2f\n" % (row[1], row[2], row[4], row[1], row[2], 'H', val, 0.0, 1.0))
+
 
 # Loop over the PCS data.
 for line in pcs_file.readlines():
