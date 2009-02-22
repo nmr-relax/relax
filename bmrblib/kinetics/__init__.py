f1ce81117933a37deafe3233feadea91ea03a883
Edward d'Auvergne <edward@nmr-relax.com>
2009-02-22 22:28:23 +0000

    Updated the __all__ list.

--- bmrblib/kinetics/__init__.py
+++ bmrblib/kinetics/__init__.py
@@ -27,6 +27,9 @@ This is a collection of NMR-STAR dictionary saveframes as defined at, for exampl
 http://www.bmrb.wisc.edu/dictionary/3.1html/SuperGroupPage.html.
 """
 
-__all__ = ['heteronucl_T1_relaxation',
+__all__ = ['heteronucl_NOE_relaxation',
+           'heteronucl_NOE_relaxation_v3_1']
+           'heteronucl_T1_relaxation',
+           'heteronucl_T1_relaxation_v3_1',
            'heteronucl_T2_relaxation',
-           'heteronucl_NOE_relaxation']
+           'heteronucl_T2_relaxation_v3_1',
