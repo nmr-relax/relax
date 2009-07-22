9296a0f739bbb392f1478c27adae59d8e9029318
Edward d'Auvergne <edward@nmr-relax.com>
2009-07-22 09:23:10 +0000

    Added comprehensive checks for the creation of the diffusion tensor PDB representations.
    The sphere and spheroid PDB files are now checked, line for line.

--- test_suite/system_tests/diffusion_tensor.py
+++ test_suite/system_tests/diffusion_tensor.py
@@ -55,7 +55,7 @@ class Diffusion_tensor(TestCase):
         self.relax.interpreter._Pipe.switch('spheroid')
         self.relax.interpreter._Structure.read_pdb(file='Ap4Aase_res1-12.pdb', dir=sys.path[-1] + '/test_suite/shared_data/structures', read_model=1)
         self.relax.interpreter._Sequence.read(file='Ap4Aase.seq', dir=sys.path[-1] + '/test_suite/shared_data/')
-        self.relax.interpreter._Diffusion_tensor.init((3.5457744201817697e-09, -10027622.7828162, 1.6721590102675075, 2.7163466955672457), angle_units='rad', spheroid_type='oblate', fixed=True)
+        self.relax.interpreter._Diffusion_tensor.init((5e-09, -10000000., 1.6, 2.7), angle_units='rad', spheroid_type='oblate', fixed=True)
         self.tmpfile_spheroid = mktemp()
 
         # Ellipsoid tensor initialization.
@@ -65,14 +65,18 @@ class Diffusion_tensor(TestCase):
         self.relax.interpreter._Diffusion_tensor.init((9e-8, 5e6, 0.3, 60+360, 290, 100), fixed=False)
         self.tmpfile_ellipsoid = mktemp()
 
+
         # Some fake MC simulations (for the sphere).
         self.relax.interpreter._Pipe.switch('sphere')
         cdp = get_pipe()
         cdp.diff_tensor.tm_err = 10e-11
         cdp.diff_tensor.tm_sim = DiffTensorSimList('tm', cdp.diff_tensor, elements=5)
-        tm_sim = [8.98e-8, 8.99e-8, 9.00e-8, 9.01e-8, 9.02e-8]
+        tm_sim = [8.98e-8, 8.99e-8, 9.00e-7, 9.01e-8, 9.02e-8]
         for i in range(5):
             cdp.diff_tensor.tm_sim[i] = tm_sim[i]
+
+        # Reset some values.
+        cdp.diff_tensor.tm_sim[0] = 9.00e-8
 
 
         # Some fake MC simulations (for the spheroid).
@@ -86,21 +90,25 @@ class Diffusion_tensor(TestCase):
         cdp.diff_tensor.phi_sim = DiffTensorSimList('phi', cdp.diff_tensor, elements=5)
 
         # Set some errors.
-        cdp.diff_tensor.Da_err = 765986.42128728074
-        cdp.diff_tensor.theta_err = 0.00068021155978203692
-        cdp.diff_tensor.tm_err = 1.7695846045376088e-11
-        cdp.diff_tensor.phi_err = 0.00078453847526795194
+        cdp.diff_tensor.Da_err = 1000000
+        cdp.diff_tensor.theta_err = 0.01
+        cdp.diff_tensor.tm_err = 1e-11
+        cdp.diff_tensor.phi_err = 0.01
 
         # The sim data.
-        Da_sim = [-10090112.175, -9922709.9518, -10118143.631, -10016815.789, -10009684.028]
-        theta_sim = [1.67256128318, 1.67099947873, 1.67255937685, 1.67210656881, 1.6715284024]
-        tm_sim = [3.5314969337e-09, 3.56919594167e-09, 3.52348845053e-09, 3.54505145128e-09, 3.5502948504e-09]
-        phi_sim = [2.71811219172, 2.7177223339, 2.71817620501, 2.71783544462, 2.71625856632]
+        Da_sim = [-12000000., -11000000., -10000000., -9000000., -8000000.]
+        theta_sim = [1.70, 1.65, 1.6, 1.55, 1.50]
+        tm_sim = [5.4e-09, 4.8e-09, 5e-09, 5.4e-09, 5.8e-09]
+        phi_sim = [2.5, 2.6, 2.7, 2.8, 100]
         for i in range(5):
             cdp.diff_tensor.Da_sim[i] = Da_sim[i]
             cdp.diff_tensor.theta_sim[i] = theta_sim[i]
             cdp.diff_tensor.tm_sim[i] = tm_sim[i]
             cdp.diff_tensor.phi_sim[i] = phi_sim[i]
+
+        # Reset some values.
+        cdp.diff_tensor.tm_sim[0] = 4.4e-9
+        cdp.diff_tensor.phi_sim[4] = 2.9
 
 
         # Some fake MC simulations (for the ellipsoid).
@@ -204,23 +212,65 @@ class Diffusion_tensor(TestCase):
     def test_create_diff_tensor_pdb_sphere(self):
         """Test the user function structure.create_diff_tensor_pdb() for the sphere."""
 
+        # First copy the data (a more vigorous copy test!).
+        self.relax.interpreter._Pipe.copy('sphere', 'sphere2')
+        self.relax.interpreter._Pipe.switch('sphere2')
+        self.relax.interpreter._Diffusion_tensor.delete()
+        self.relax.interpreter._Diffusion_tensor.copy('sphere', 'sphere2')
+
         # Create the diffusion tensor objects.
-        self.relax.interpreter._Pipe.switch('sphere')
         self.relax.interpreter._Structure.create_diff_tensor_pdb(file=self.tmpfile_sphere)
 
-        # Open the temp files.
-        file_sphere = open(self.tmpfile_sphere)
+        # Open the temp file.
+        file = open(self.tmpfile_sphere)
+        new_data = file.readlines()
+        file.close()
+
+        # Open the real file.
+        file = open(sys.path[-1] + '/test_suite/shared_data/structures/diff_tensors/sphere.pdb')
+        real_data = file.readlines()
+        file.close()
+
+        # Check the data.
+        self.assertEqual(len(real_data), len(new_data))
+        for i in range(len(real_data)):
+            # Print the PDB line, for debugging.
+            print real_data[i][0:-1]
+
+            # Check the line.
+            self.assertEqual(real_data[i], new_data[i])
 
 
     def test_create_diff_tensor_pdb_spheroid(self):
         """Test the user function structure.create_diff_tensor_pdb() for the spheroid."""
 
+        # First copy the data (a more vigorous copy test!).
+        self.relax.interpreter._Pipe.copy('spheroid', 'spheroid2')
+        self.relax.interpreter._Pipe.switch('spheroid2')
+        self.relax.interpreter._Diffusion_tensor.delete()
+        self.relax.interpreter._Diffusion_tensor.copy('spheroid', 'spheroid2')
+
         # Create the diffusion tensor objects.
-        self.relax.interpreter._Pipe.switch('spheroid')
         self.relax.interpreter._Structure.create_diff_tensor_pdb(file=self.tmpfile_spheroid)
 
-        # Open the temp files.
-        file_spheroid = open(self.tmpfile_spheroid)
+        # Open the temp file.
+        file = open(self.tmpfile_spheroid)
+        new_data = file.readlines()
+        file.close()
+
+        # Open the real file.
+        file = open(sys.path[-1] + '/test_suite/shared_data/structures/diff_tensors/spheroid.pdb')
+        real_data = file.readlines()
+        file.close()
+
+        # Check the data.
+        self.assertEqual(len(real_data), len(new_data))
+        for i in range(len(real_data)):
+            # Print the PDB line, for debugging.
+            print real_data[i][0:-1]
+
+            # Check the line.
+            self.assertEqual(real_data[i], new_data[i])
 
 
     def test_create_diff_tensor_pdb_ellipsoid(self):
