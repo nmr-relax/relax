1da41d43d1eda2adbbb516d262313cacbc8581d6
Edward d'Auvergne <edward@nmr-relax.com>
2009-09-03 17:25:01 +0000

    The beta angle for the tensor rotations is now folded to be between 0 and pi.

--- specific_fns/frame_order.py
+++ specific_fns/frame_order.py
@@ -241,6 +241,17 @@ class Frame_order(Common_functions):
         beta  = wrap_angles(beta, 0.0, 2.0*pi)
         gamma = wrap_angles(gamma, 0.0, 2.0*pi)
 
+        # Fold beta to be between 0 and pi.
+        if beta >= pi:
+            alpha = alpha - pi
+            beta = 2*pi - beta
+            gamma = gamma - pi
+
+        # Wrap again.
+        alpha = wrap_angles(alpha, 0.0, 2.0*pi)
+        beta  = wrap_angles(beta, 0.0, 2.0*pi)
+        gamma = wrap_angles(gamma, 0.0, 2.0*pi)
+
         # Monte Carlo simulation data structures.
         if sim_index != None:
             # Tensor rotation.
