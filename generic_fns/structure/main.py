0ddbd3db051243f4e9cdc7a9004144a927bf1a8d
Edward d'Auvergne <edward@nmr-relax.com>
2009-10-07 12:34:52 +0000

    Import fixes by shifting to relax_io.write_spin_data_file().

--- generic_fns/structure/main.py
+++ generic_fns/structure/main.py
@@ -33,11 +33,10 @@ from warnings import warn
 from generic_fns import molmol, relax_re
 from generic_fns.mol_res_spin import exists_mol_res_spin_data, generate_spin_id, return_molecule, return_residue, return_spin, spin_loop
 from generic_fns import pipes
-from generic_fns.sequence import write_header, write_line
 from generic_fns.structure.internal import Internal
 from generic_fns.structure.scientific import Scientific_data
 from relax_errors import RelaxError, RelaxFileError, RelaxNoPdbError, RelaxNoSequenceError, RelaxPdbError
-from relax_io import get_file_path, open_write_file
+from relax_io import get_file_path, open_write_file, write_spin_data_file
 from relax_warnings import RelaxWarning, RelaxNoPDBFileWarning, RelaxZeroVectorWarning
 
 
