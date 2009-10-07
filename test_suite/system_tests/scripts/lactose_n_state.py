ccebf8ddd2e642840fbaf9717c5541764aebdb30
Edward d'Auvergne <edward@nmr-relax.com>
2009-10-07 08:08:32 +0000

    Fixes for the system tests to use the new 'align_id' args.

--- test_suite/system_tests/scripts/lactose_n_state.py
+++ test_suite/system_tests/scripts/lactose_n_state.py
@@ -45,12 +45,12 @@ align_list = ['Dy', 'Tb', 'Tm', 'Er']
 # Load the RDCs and PCSs.
 for i in xrange(len(align_list)):
     # The RDC.
-    rdc.read(id=align_list[i], file='rdc.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=i+3, error_col=None)
-    rdc.read(id=align_list[i], file='rdc_err.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=None, error_col=i+3)
+    rdc.read(align_id=align_list[i], file='rdc.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=i+3, error_col=None)
+    rdc.read(align_id=align_list[i], file='rdc_err.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=None, error_col=i+3)
 
     # The PCS.
-    pcs.read(id=align_list[i], file='pcs.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=i+2, error_col=None)
-    pcs.read(id=align_list[i], file='pcs_err.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=None, error_col=i+2)
+    pcs.read(align_id=align_list[i], file='pcs.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=i+2, error_col=None)
+    pcs.read(align_id=align_list[i], file='pcs_err.txt', dir=data_path, mol_name_col=None, res_num_col=None, res_name_col=None, spin_num_col=None, spin_name_col=1, data_col=None, error_col=i+2)
 
     # The temperature.
     temperature(id=align_list[i], temp=298)
