beef97db8bad0869eaaff514e697399c62e8af99
Edward d'Auvergne <edward@nmr-relax.com>
2009-10-09 18:24:11 +0000

    Created a data container for storing experimental details.
    This will be used extensively for the BMRB file generation.

--- data/data_classes.py
+++ data/data_classes.py
@@ -23,6 +23,9 @@
 # Python module imports.
 from re import search
 
+# relax module imports.
+from relax_xml import fill_object_contents, xml_to_object
+
 
 # Empty data container.
 #######################
@@ -51,6 +54,17 @@ class Element(object):
         return text
 
 
+    def from_xml(self, exp_info_node):
+        """Recreate the container data structure from the XML container node.
+
+        @param exp_info_node:   The container XML node.
+        @type exp_info_node:    xml.dom.minicompat.Element instance
+        """
+
+        # Recreate all the data structures.
+        xml_to_object(exp_info_node, self)
+
+
     def is_empty(self):
         """Method for testing if the Element container is empty.
 
@@ -73,3 +87,26 @@ class Element(object):
 
         # The Element container is empty.
         return True
+
+
+    def to_xml(self, doc, element):
+        """Create an XML element for the container.
+
+        The variables self.name and self.desc must exist.
+
+
+        @param doc:     The XML document object.
+        @type doc:      xml.dom.minidom.Document instance
+        @param element: The element to add the container element to.
+        @type element:  XML element object
+        """
+
+        # Create the container element and add it to the higher level element.
+        tensor_element = doc.createElement(self.name)
+        element.appendChild(tensor_element)
+
+        # Set the container attributes.
+        tensor_element.setAttribute('desc', self.desc)
+
+        # Add all simple python objects within the PipeContainer to the pipe element.
+        fill_object_contents(doc, tensor_element, object=self, blacklist=['type'] + list(self.__class__.__dict__.keys()))
