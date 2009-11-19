e23f78529e2501dc20c75e33c17d58aabeaf7147
Edward d'Auvergne <edward@nmr-relax.com>
2009-11-19 10:18:50 +0000

    Converted the relaxGUI code into a Python package.

---
+++ gui_bieri/__init__.py
@@ -0,0 +1,30 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+
+__all__ = ['relaxGUI']
+
+__doc__ = \
+"""Package for the Bieri GUI interface for relax.
+
+This GUI was announced in the post at https://mail.gna.org/public/relax-devel/2009-11/msg00005.html.
+"""
