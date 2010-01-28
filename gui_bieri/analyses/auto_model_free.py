b5a5323e39a9bc4077350608bf47b7e6de0fde00
Edward d'Auvergne <edward@nmr-relax.com>
2010-01-28 12:34:52 +0000

    Added the missing RedirectText import.

--- gui_bieri/analyses/auto_model_free.py
+++ gui_bieri/analyses/auto_model_free.py
@@ -36,7 +36,7 @@ from data import Relax_data_store; ds = Relax_data_store()
 # relax GUI module imports.
 from gui_bieri.analyses.results_analysis import model_free_results, see_results
 from gui_bieri.analyses.select_model_calc import Select_tensor
-from gui_bieri.base_classes import Container
+from gui_bieri.base_classes import Container, RedirectText
 from gui_bieri.controller import Thread_container
 from gui_bieri.derived_wx_classes import StructureTextCtrl
 from gui_bieri.filedialog import opendir, openfile
