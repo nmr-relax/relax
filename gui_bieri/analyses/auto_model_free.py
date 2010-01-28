d7f128799c5859c87f57ea561c72c250abc42368
Edward d'Auvergne <edward@nmr-relax.com>
2010-01-28 12:39:50 +0000

    Temporarily disabled IO redirection to the controller for debugging.

--- gui_bieri/analyses/auto_model_free.py
+++ gui_bieri/analyses/auto_model_free.py
@@ -805,8 +805,8 @@ class Auto_model_free:
 
         # Redirect relax output and errors to the controller.
         redir = Redirect_text(self.gui.controller)
-        sys.stdout = redir
-        sys.stderr = redir
+        #sys.stdout = redir
+        #sys.stderr = redir
 
         # Print a header in the controller.
         wx.CallAfter(self.gui.controller.log_panel.AppendText, ('Starting Model-free calculation\n------------------------------------------\n\n') )
