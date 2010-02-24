71376bf1b3d8ce2763e301c97ba1f108876e85d4
Michael Bieri <michael.bieri@nmr-relax.com>
2010-02-24 04:51:17 +0000

    The title of the NOE analysis frame was changed.

--- gui_bieri/analyses/auto_noe.py
+++ gui_bieri/analyses/auto_noe.py
@@ -116,7 +116,7 @@ class Auto_noe:
         """
 
         # The title.
-        label = wx.StaticText(self.parent, -1, "Set-up for %s relaxation analysis:" % self.label)
+        label = wx.StaticText(self.parent, -1, "Set-up for steady-state NOE analysis:")
 
         # The font properties.
         label.SetFont(wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Sans"))
