3036de93f8e0dad5bbef72eee1bc3bb1fc4636a2
Edward d'Auvergne <edward@nmr-relax.com>
2010-04-28 16:53:08 +0000

    Another fix for the huge bug identified in r11163.
    This is for the PCS part - this was also using equal weights for all states set to pN!

--- maths_fns/rdc.py
+++ maths_fns/rdc.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2008 Edward d'Auvergne                                        #
+# Copyright (C) 2008, 2010 Edward d'Auvergne                                  #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
