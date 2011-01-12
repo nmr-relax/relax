74b08a865e8cf5160b0ce89b3c5474bbdaeef71b
Edward d'Auvergne <edward@nmr-relax.com>
2011-01-12 13:23:16 +0000

    Modified the model-free system test method mesg_opt_debug() to handle all model-free models.

--- test_suite/system_tests/model_free.py
+++ test_suite/system_tests/model_free.py
@@ -106,9 +106,20 @@ class Mf(SystemTestCase):
 
 
         # Minimisation info.
-        string = string + "\n%-15s %30.16g\n" % ('s2:',      spin.s2)
-        string = string +   "%-15s %30.13g\n" % ('te:',      spin.te * 1e12)
-        string = string +   "%-15s %30.17g\n" % ('rex:',     spin.rex * (2.0 * pi * spin.frq[0])**2)
+        if spin.s2 != None:
+            string = string + "\n%-15s %30.16g\n" % ('s2:',      spin.s2)
+        if spin.s2f != None:
+            string = string + "\n%-15s %30.16g\n" % ('s2f:',      spin.s2f)
+        if spin.s2s != None:
+            string = string + "\n%-15s %30.16g\n" % ('s2s:',      spin.s2s)
+        if spin.te != None:
+            string = string +   "%-15s %30.13g\n" % ('te:',      spin.te * 1e12)
+        if spin.tf != None:
+            string = string +   "%-15s %30.13g\n" % ('tf:',      spin.tf * 1e12)
+        if spin.ts != None:
+            string = string +   "%-15s %30.13g\n" % ('ts:',      spin.ts * 1e12)
+        if spin.rex != None:
+            string = string +   "%-15s %30.17g\n" % ('rex:',     spin.rex * (2.0 * pi * spin.frq[0])**2)
         string = string +   "%-15s %30.17g\n" % ('chi2:',    spin.chi2)
         string = string +   "%-15s %30i\n"   % ('iter:',    spin.iter)
         string = string +   "%-15s %30i\n"   % ('f_count:', spin.f_count)
