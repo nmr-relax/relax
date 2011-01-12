def0489ca7d1ad976eb3d7197c5618b14c51614e
Edward d'Auvergne <edward@nmr-relax.com>
2011-01-12 15:31:07 +0000

    Created a system test for checking model-free model m2 against the tm2 grid.

--- test_suite/system_tests/model_free.py
+++ test_suite/system_tests/model_free.py
@@ -337,6 +337,55 @@ class Mf(SystemTestCase):
         self.value_test(spin, local_tm=10, s2=0.8, te=40, chi2=0.0)
 
 
+    def test_m2_grid(self):
+        """Test the optimisation of the m2 model-free parameter grid."""
+
+        # Setup the data pipe for optimisation.
+        self.interpreter.run(script_file=status.install_path + sep+'test_suite'+sep+'system_tests'+sep+'scripts'+sep+'model_free'+sep+'opt_setup_m2_grid.py')
+
+        # The model-free parameters.
+        tm = [2e-9, 10e-9, 21e-9]
+        s2 = [0.2, 0.8, 0.95]
+        te = [2e-12, 40e-12, 1e-9]
+
+        # Deselect all spins.
+        self.interpreter.deselect.spin()
+
+        # Residue index.
+        res_index = 0
+
+        # Loop over te.
+        for te_index in range(3):
+            # Loop over s2.
+            for s2_index in range(3):
+                # Loop over tm.
+                for tm_index in range(3):
+                    # Alias the relevent spin container.
+                    spin = cdp.mol[0].res[res_index].spin[0]
+
+                    # Select the spin.
+                    spin.select = True
+
+                    # Set up the diffusion tensor.
+                    if res_index:
+                        self.interpreter.diffusion_tensor.delete()
+                    self.interpreter.diffusion_tensor.init(tm[tm_index])
+
+                    # Set up the initial model-free parameter values (bypass the grid search for speed).
+                    spin.s2 = s2[s2_index]
+                    spin.te = te[te_index]
+
+                    # Minimise.
+                    self.interpreter.minimise('newton', 'gmw', 'back')
+
+                    # Check the values.
+                    self.value_test(spin, s2=s2[s2_index], te=te[te_index]*1e12, chi2=0.0)
+
+                    # Increment the residue index and deselect the spin.
+                    res_index += 1
+                    spin.select = False
+
+
     def test_omp_analysis(self):
         """Try a very minimal model-free analysis on the OMP relaxation data."""
 
