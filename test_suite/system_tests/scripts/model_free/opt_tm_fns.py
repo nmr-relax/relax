f0b21ae0b3c05c45106ebbae7ef788297db7e77f
Edward d'Auvergne <edward@nmr-relax.com>
2011-01-13 08:39:47 +0000

    Fix for opt_and_check(), the diffusion tensor object is called cdp.diff_tensor!

--- test_suite/system_tests/scripts/model_free/opt_tm_fns.py
+++ test_suite/system_tests/scripts/model_free/opt_tm_fns.py
@@ -70,7 +70,7 @@ def opt_and_check(spin=None, tm=None, tm_index=None):
 
     # Set up the diffusion tensor.
     if search('^m', cdp._model):
-        if hasattr(cdp, 'diffusion_tensor'):
+        if hasattr(cdp, 'diff_tensor'):
             interpreter.diffusion_tensor.delete()
         interpreter.diffusion_tensor.init(tm[tm_index])
 
