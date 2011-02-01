efcb7261091fe376ea93dfba5b187ff1dbb7e420
Edward d'Auvergne <edward@nmr-relax.com>
2011-02-01 19:29:43 +0000

    A fix for BMRB files with no relaxation data - Rex cannot be scaled to the correct value!!!

--- specific_fns/model_free/bmrb.py
+++ specific_fns/model_free/bmrb.py
@@ -161,7 +161,9 @@ class Bmrb:
                 ts_scale = te_scale
 
             # Rex scaling.
-            rex_scale = 1.0 / (2.0*pi*cdp.frq[0]**2)
+            rex_scale = 1.0
+            if hasattr(cdp, 'frq') and len(cdp.frq):
+                rex_scale = 1.0 / (2.0*pi*cdp.frq[0]**2)
 
             # Loop over the spins.
             for i in range(N):
