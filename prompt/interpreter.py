489de2c5708605e694679aa81affa176c4d3ded5
Edward d'Auvergne <edward@nmr-relax.com>
2011-02-25 13:32:27 +0000

    The runpy module is only imported if it exists.

--- prompt/interpreter.py
+++ prompt/interpreter.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2010 Edward d'Auvergne                                   #
+# Copyright (C) 2003-2011 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -33,7 +33,8 @@ import platform
 from re import search
 if dep_check.readline_module:
     import readline
-import runpy
+if dep_check.runpy_module:
+    import runpy
 import sys
 
 # Python modules accessible on the command prompt.
