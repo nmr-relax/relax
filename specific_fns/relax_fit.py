c3fc3acb70368fe7541d8083d12008343ef67dff
Edward d'Auvergne <edward@nmr-relax.com>
2011-07-08 10:04:13 +0000

    Converted all of the relaxation curve fitting documentation used in the user functions to the new design.

--- specific_fns/relax_fit.py
+++ specific_fns/relax_fit.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2004-2009 Edward d'Auvergne                                   #
+# Copyright (C) 2004-2011 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -630,12 +630,8 @@ class Relax_fit(API_base, API_common):
         return names
 
 
-    default_value_doc = """
-        Relaxation curve fitting default values
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        These values are completely arbitrary as peak heights (or volumes) are extremely variable
-        and the Rx value is a compensation for both the R1 and R2 values.
+    default_value_doc = ["Relaxation curve fitting default values", """
+        These values are completely arbitrary as peak heights (or volumes) are extremely variable and the Rx value is a compensation for both the R1 and R2 values.
         ___________________________________________________________________
         |                        |               |                        |
         | Data type              | Object name   | Value                  |
@@ -649,7 +645,7 @@ class Relax_fit(API_base, API_common):
         |                        |               |                        |
         |________________________|_______________|________________________|
 
-        """
+        """]
 
     def default_value(self, param):
         """Return the default relaxation curve-fitting parameter values.
@@ -924,10 +920,7 @@ class Relax_fit(API_base, API_common):
         return spin.intensities
 
 
-    return_data_name_doc = """
-        Relaxation curve fitting data type string matching patterns
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    return_data_name_doc = ["Relaxation curve fitting data type string matching patterns", """
         __________________________________________________________________________________________
         |                                   |                      |                             |
         | Data type                         | Object name          | Patterns                    |
@@ -944,7 +937,7 @@ class Relax_fit(API_base, API_common):
         | Relaxation period times (series)  | 'relax_times'        | '^[Rr]elax[ -_][Tt]imes$'   |
         |___________________________________|______________________|_____________________________|
 
-        """
+        """]
 
     def return_data_name(self, param):
         """Return a unique identifying string for the relaxation curve-fitting parameter.
@@ -1048,14 +1041,11 @@ class Relax_fit(API_base, API_common):
         return None
 
 
-    set_doc = """
-        Relaxation curve fitting set details
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    set_doc = ["Relaxation curve fitting set details", """
         Only three parameters can be set, the relaxation rate (Rx), the initial intensity (I0), and
         the intensity at infinity (Iinf).  Setting the parameter Iinf has no effect if the chosen
         model is that of the exponential curve which decays to zero.
-        """
+        """]
 
 
     def sim_pack_data(self, data_id, sim_data):
