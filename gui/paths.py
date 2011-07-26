92812bb1ddaa4b6986b86d702ff56757ead54dea
Edward d'Auvergne <edward@nmr-relax.com>
2011-07-26 10:25:30 +0000

    Added a red flag to use if the user changes the model-free models!

--- gui/paths.py
+++ gui/paths.py
@@ -83,6 +83,7 @@ class I16x16:
         self.edit_delete =          path + 'actions'    + sep + 'edit-delete.png'
         self.exit =                 path + 'actions'    + sep + 'system-shutdown.png'
         self.flag_blue =            path + 'actions'    + sep + 'flag-blue.png'
+        self.flag_red =             path + 'actions'    + sep + 'flag-red.png'
         self.list_remove =          path + 'actions'    + sep + 'list-remove.png'
         self.load =                 path + 'actions'    + sep + 'arrow-right.png'
         self.manual =               path + 'mimetypes'  + sep + 'application-pdf.png'
