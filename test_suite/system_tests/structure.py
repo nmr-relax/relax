a5ca1377dbd365b538c39b8a3005ba727ad1c08a
Han Sun <hasu@nmr.mpibpc.mpg.de>
2011-07-26 15:38:38 +0000

    Changing the code in the test_read_xyz_internal2
    Test to load a spin of the already loaded model of the molecule.

--- test_suite/system_tests/structure.py
+++ test_suite/system_tests/structure.py
@@ -678,7 +678,7 @@ class Structure(SystemTestCase):
         self.interpreter.structure.load_spins()
 
         # Extract a vector between first two spins.
-        self.interpreter.structure.vectors(attached='@10', spin_id='#SSS-cluster4-new-test_mol1@2')
+        self.interpreter.structure.vectors(attached='@10', spin_id='@2')
         self.assertAlmostEqual(a.bond_vect[0], -2.162)
         self.assertAlmostEqual(a.bond_vect[1], 3.274)
         self.assertAlmostEqual(a.bond_vect[2], -3.518)
