31d18fe7138997101ee3cdc1927affb040074d69
Edward d'Auvergne <edward@nmr-relax.com>
2011-07-28 15:08:31 +0000

    Created a temporary script for forcing relax's GUI mode.

---
+++ relax_gui_mode.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+# This is a transient solution to forcing the GUI - a better solution would be an environmental variable.
+
+# Import the relax module.
+import relax
+
+# Start relax.
+relax.start(mode='gui')
