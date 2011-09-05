7569d60e1ce2411eb56b3a1b8d0dc66ebe4fa8df
Edward d'Auvergne <edward@nmr-relax.com>
2011-09-05 16:49:21 +0000

    Fix for the relax_fit.relax_time GUI user function.
    The combobox GetValue() call has been replaced by GetStringSelection() for the spectrum ID.

--- gui/user_functions/relax_fit.py
+++ gui/user_functions/relax_fit.py
@@ -99,7 +99,7 @@ class Relax_time_page(UF_page):
         time = gui_to_float(self.time.GetValue())
 
         # The spectrum ID.
-        spectrum_id = gui_to_str(self.spectrum_id.GetValue())
+        spectrum_id = gui_to_str(self.spectrum_id.GetStringSelection())
 
         # Read the relaxation data.
         self.gui.interpreter.queue('relax_fit.relax_time', time=time, spectrum_id=spectrum_id)
