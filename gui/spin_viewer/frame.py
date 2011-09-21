8e7f34b53b7ea6e09da46c594183328a9cb42200
Edward d'Auvergne <edward@nmr-relax.com>
2011-09-21 12:46:41 +0000

    Bug fix for MS Windows - the spin viewer window was being hidden by the main window when switching pipes.
    The window is now being re-raised to the front.

--- gui/spin_viewer/frame.py
+++ gui/spin_viewer/frame.py
@@ -340,6 +340,9 @@ class Spin_view_window(wx.Frame):
         # Set the pipe name to the cdp.
         self.pipe_name.SetValue(str_to_gui(pipe))
 
+        # Bug fix for MS Windows.
+        wx.CallAfter(self.Raise)
+
         # Reset the cursor.
         wx.EndBusyCursor()
 
