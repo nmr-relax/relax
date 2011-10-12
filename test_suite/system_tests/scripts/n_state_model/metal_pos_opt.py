606a43704417625a98ec8707bcf6d8e29064af35
Edward d'Auvergne <edward@nmr-relax.com>
2011-10-12 13:18:25 +0000

    Created a system test script for checking one type of algorithm for Ln3+ position optimisation.

---
+++ test_suite/system_tests/scripts/n_state_model/metal_pos_opt.py
@@ -0,0 +1,95 @@
+"""Script for testing the lanthanide position optimisation using RDCs and PCSs."""
+
+# Python module imports.
+from os import sep
+import sys
+
+# relax module imports.
+from data import Relax_data_store; ds = Relax_data_store()
+from generic_fns import pipes
+from status import Status; status = Status()
+
+
+# Path of the alignment data and structure.
+DATA_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'align_data'+sep+'population_model'
+STRUCT_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'structures'+sep+'lactose'
+
+# Create the data pipe.
+pipe.create('Ln3+ opt', 'N-state')
+
+# Load the structures.
+for i in range(1, 4):
+    structure.read_pdb(file='lactose_MCMM4_S1_%i.pdb' % i, dir=STRUCT_PATH, set_model_num=i, set_mol_name='LE')
+
+# Load the spins.
+structure.load_spins(spin_id=':UNK@C*', ave_pos=False)
+structure.load_spins(spin_id=':UNK@H*', ave_pos=False)
+
+# Deselect the CH2 protons (the rotation of these doesn't work in the model, but the carbon doesn't move).
+deselect.spin(spin_id=':UNK@H6')
+deselect.spin(spin_id=':UNK@H7')
+deselect.spin(spin_id=':UNK@H17')
+deselect.spin(spin_id=':UNK@H18')
+
+# Load the CH vectors.
+structure.vectors(spin_id='@C*', attached='H*', ave=False)
+
+# Set the values needed to calculate the dipolar constant.
+value.set(1.10 * 1e-10, 'bond_length', spin_id="@C*")
+value.set('13C', 'heteronucleus', spin_id="@C*")
+value.set('1H', 'proton', spin_id="@C*")
+
+# Set the paramagnetic centre.
+paramag.centre([ -14.8,    0.9,    0.2])
+
+# File list.
+align_list = ['Dy', 'Tb', 'Tm', 'Er']
+
+# Load the RDCs and PCSs.
+for i in xrange(len(align_list)):
+    # The RDC.
+    rdc.read(align_id=align_list[i], file='missing_rdc_%i' % i, dir=DATA_PATH, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
+
+    # The PCS.
+    pcs.read(align_id=align_list[i], file='missing_pcs_%i' % i, dir=DATA_PATH, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
+
+    # The temperature.
+    temperature(id=align_list[i], temp=298)
+
+    # The frequency.
+    frq.set(id=align_list[i], frq=799.75376122 * 1e6)
+
+    # Set up the model.
+    n_state_model.select_model('fixed')
+
+    # Minimisation.
+    grid_search(inc=3)
+    minimise('newton', constraints=False)
+
+    # Fix the tensor.
+    align_tensor.fix(id=align_list[i])
+
+# Optimise the Ln3+ position.
+paramag.centre(fix=False)
+minimise('simplex', constraints=False)
+
+# Optimise everything.
+align_tensor.fix(fixed=False)
+minimise('simplex', constraints=False)
+
+# Monte Carlo simulations.
+monte_carlo.setup(3)
+monte_carlo.create_data()
+monte_carlo.initial_values()
+minimise('simplex', constraints=False, max_iter=500)
+monte_carlo.error_analysis()
+
+# Write out a results file.
+results.write('devnull', force=True)
+
+# Show the tensors.
+align_tensor.display()
+
+# Print the contents of the current data pipe (for debugging Q-values).
+print(cdp)
+print((cdp.align_tensors[0]))
