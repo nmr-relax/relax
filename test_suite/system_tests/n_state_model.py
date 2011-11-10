70cdc82c32ccad072d33979a5772a763c9d43de1
Edward d'Auvergne <edward@nmr-relax.com>
2011-11-10 10:29:58 +0000

    Fix for the precision of the chi2 value check in the N_state_model.test_monte_carlo_sims system test.
    This affects 32-bit GNU/Linux and not 64-bit GNU/Linux.  Other operating systems are unknown if this
    test previously passed or not.

--- test_suite/system_tests/n_state_model.py
+++ test_suite/system_tests/n_state_model.py
@@ -431,7 +431,7 @@ class N_state_model(SystemTestCase):
         self.assertAlmostEqual(cdp.align_tensors[0].Axy, -0.506392/2000)
         self.assertAlmostEqual(cdp.align_tensors[0].Axz, 0.560544/2000)
         self.assertAlmostEqual(cdp.align_tensors[0].Ayz, -0.286367/2000)
-        self.assertAlmostEqual(cdp.chi2, 1745860.0485368515)
+        self.assertAlmostEqual(cdp.chi2 / 1e6, 1745860.0485368515 / 1e6)
         self.assertAlmostEqual(cdp.q_rdc, 0.0)
         self.assertAlmostEqual(cdp.q_pcs, 0.0)
 
@@ -463,7 +463,7 @@ class N_state_model(SystemTestCase):
             self.assertAlmostEqual(cdp.align_tensors[0].Axy, -0.506392/2000)
             self.assertAlmostEqual(cdp.align_tensors[0].Axz, 0.560544/2000)
             self.assertAlmostEqual(cdp.align_tensors[0].Ayz, -0.286367/2000)
-            self.assertAlmostEqual(cdp.chi2, 1745860.0485368515)
+            self.assertAlmostEqual(cdp.chi2 / 1e6, 1745860.0485368515 / 1e6)
 
         # Test the tensor error values.
         self.assertAlmostEqual(cdp.align_tensors[0].Axx_err, 0.0)
