a0218591e1c88c727c18f347671c03677d95e14c
Edward d'Auvergne <edward@nmr-relax.com>
2012-01-10 14:10:26 +0000

    Updates to the free rotor pseudo-ellipse test model data.

--- test_suite/shared_data/frame_order/pseudo_ellipse_free_rotor/frame_order.py
+++ test_suite/shared_data/frame_order/pseudo_ellipse_free_rotor/frame_order.py
@@ -57,7 +57,7 @@ class Analysis:
             rdc.read(align_id=ln[i], file='rdc_%s.txt'%ln[i], res_num_col=2, spin_name_col=5, data_col=6, error_col=7)
 
             # The PCS.
-            #pcs.read(align_id=ln[i], file='pcs_%s.txt'%ln[i], res_num_col=2, spin_name_col=5, data_col=6, error_col=7)
+            pcs.read(align_id=ln[i], file='pcs_%s.txt'%ln[i], res_num_col=2, spin_name_col=5, data_col=6, error_col=7)
 
             # The temperature and field strength.
             temperature(id=ln[i], temp=303)
