459ccb8f7d5c93c42decc623e6e48e0e6c7dc4e5
Edward d'Auvergne <edward@nmr-relax.com>
2012-01-27 10:59:24 +0000

    Converted the system test scripts directory into a Python package.
    This will be used to import the base frame order CaM optimisation script.

---
+++ test_suite/system_tests/scripts/__init__.py
@@ -0,0 +1,47 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2012 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Package docstring.
+"""The system test scripts."""
+
+
+__all__ = ['1UBQ_relax_fit',
+           'angles',
+           'bmrb_rw',
+           'bug_17276_peak_lists',
+           'consistency_tests',
+           'curve_fitting',
+           'dasha',
+           'diff_tensor',
+           'frame_order',
+           'jw_mapping',
+           'load_spins_from_small_molecule',
+           'local_tm_model_elimination',
+           'model_free',
+           'nested_scripting',
+           'noe',
+           'n_state_model',
+           'palmer_omp',
+           'palmer',
+           'peak_lists',
+           'relax_fit',
+           'xh_vector_dist']
