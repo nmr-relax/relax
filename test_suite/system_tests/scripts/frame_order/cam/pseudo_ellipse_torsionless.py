83546cb6d089fe5bffee3149cfc907d2389c196a
Edward d'Auvergne <edward@nmr-relax.com>
2012-01-27 17:12:40 +0000

    Created the rest of the frame order system test scripts.

---
+++ test_suite/system_tests/scripts/frame_order/cam/pseudo_ellipse_torsionless.py
@@ -0,0 +1,46 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2012 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Module docstring.
+"""Script for optimising the torsionless pseudo-ellipse frame order test model of CaM."""
+
+# relax module imports.
+from base_script import Base_script
+
+
+class Analysis(Base_script):
+
+    # Set up some class variables.
+    directory = 'pseudo_ellipse_torsionless'
+    model = 'pseudo-ellipse, torsionless'
+    ave_pos_alpha = 4.3434999280669997
+    ave_pos_beta = 0.43544332764249905
+    ave_pos_gamma = 3.8013235235956007
+    eigen_alpha = 3.1415926535897931
+    eigen_beta = 0.96007997859534311
+    eigen_gamma = 4.0322755062196229
+    cone_theta_x = 0.1
+    cone_theta_y = 0.5
+
+
+# Execute the analysis.
+Analysis(self)
