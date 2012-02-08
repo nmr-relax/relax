aff045c2fe1662546a2115dfa5c2998256a8f86d
Edward d'Auvergne <edward@nmr-relax.com>
2012-02-08 10:41:39 +0000

    Switched the isotropic cone frame order system test to use the save file.
    This significantly speeds up the system test, as all the sequence generation and RDC and PCS data
    reading is skipped.

--- test_suite/system_tests/scripts/frame_order/cam/iso_cone.py
+++ test_suite/system_tests/scripts/frame_order/cam/iso_cone.py
@@ -23,25 +23,31 @@
 # Module docstring.
 """Script for optimising the isotropic cone frame order test model of CaM."""
 
+# Python module imports.
+from os import sep
+
 # relax module imports.
-from base_script import Base_script
+from status import Status; status = Status()
 
 
-class Analysis(Base_script):
+# Path of the save file.
+DATA_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'frame_order'+sep+'cam'+sep+'iso_cone'
 
-    # Set up some class variables.
-    directory = 'iso_cone'
-    model = 'iso cone'
-    ave_pos_alpha = 4.3434999280669997
-    ave_pos_beta = 0.43544332764249905
-    ave_pos_gamma = 3.8013235235956007
-    axis_theta = 2.1815126749944502
-    axis_phi = 0.89068285262982982
-    cone_theta = 10.0 * 2.0 * pi / 360.0
-    cone_sigma_max = 20.0 * 2.0 * pi / 360.0
-    cone = True
-    num_int_pts = 100
+# Load the save file.
+reset()
+state.load('frame_order', dir=DATA_PATH)
 
+# Set up some variables.
+value.set(param='ave_pos_alpha', val=4.3434999280669997)
+value.set(param='ave_pos_beta', val=0.43544332764249905)
+value.set(param='ave_pos_gamma', val=3.8013235235956007)
+value.set(param='axis_theta', val=2.1815126749944502)
+value.set(param='axis_phi', val=0.89068285262982982)
+value.set(param='cone_theta', val=10.0 * 2.0 * pi / 360.0)
+value.set(param='cone_sigma_max', val=20.0 * 2.0 * pi / 360.0)
 
-# Execute the analysis.
-Analysis(self)
+# Increase the number of Sobol points so the cone is sampled.
+cdp.num_int_pts = 100
+
+# Calculate the chi-squared value.
+calc()
