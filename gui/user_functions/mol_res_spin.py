a38abfb39dcd402aaf3c15b63ad158e6cee7ebdf
Edward d'Auvergne <edward@nmr-relax.com>
2012-04-17 13:27:49 +0000

    Copyright updates.

--- gui/user_functions/mol_res_spin.py
+++ gui/user_functions/mol_res_spin.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2010 Edward d'Auvergne                                        #
+# Copyright (C) 2010-2012 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
--- gui/user_functions/spin.py
+++ gui/user_functions/spin.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2011-2012 Edward d'Auvergne                                   
+# Copyright (C) 2011-2012 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
