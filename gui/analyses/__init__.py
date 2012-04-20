795a694f458351bf9e6f084152f741add85fdd5c
Edward d'Auvergne <edward@nmr-relax.com>
2012-04-20 20:42:44 +0000

    Missing RelaxError import!

--- gui/analyses/__init__.py
+++ gui/analyses/__init__.py
@@ -34,6 +34,7 @@ from data import Relax_data_store; ds = Relax_data_store()
 from data.gui import Gui
 import dep_check
 from generic_fns import pipes
+from relax_errors import RelaxError
 from status import Status; status = Status()
 
 # relax GUI module imports.
