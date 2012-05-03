1bc8279410fdbbb88d9a1e95d9c534cb00010dbf
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-03 20:12:28 +0000

    Added the prompt.objects module which contains the user function auto-generation objects.

---
+++ prompt/objects.py
@@ -0,0 +1,38 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2012 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax.                                     #
+#                                                                             #
+# relax is free software; you can redistribute it and/or modify               #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation; either version 2 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# relax is distributed in the hope that it will be useful,                    #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with relax; if not, write to the Free Software                        #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                             #
+###############################################################################
+
+# Module docstring.
+"""Module containing the special objects for auto-generating the user functions and classes."""
+
+
+class Class_container(object):
+    """The container for created the user function class objects."""
+
+    def __init__(self, name):
+        """Set up the container.
+
+        @param name:    The name of the user function class.
+        @type name:     str
+        """
+
+        # Store the args.
+        self._name = name
