c7c28540c19c92a77a07d3f42f54daafa507a6ac
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-07 08:12:26 +0000

    The Bmrb.test_bmrb_rw GUI test is now complete and fully uses the new auto-generated user functions.
    This test will only pass once all the relevant user functions are converted to the auto-generation
    design!

--- test_suite/gui_tests/bmrb.py
+++ test_suite/gui_tests/bmrb.py
@@ -54,93 +54,48 @@ class Bmrb(GuiTestCase):
         self.execute_uf(name='pipe.create', pipe_name='results', pipe_type='mf')
 
         # Read the results.
-        results_read = uf_pages.results.Read_page(self._wizard)
-        results_read.file.SetValue(str_to_gui(status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'model_free'+sep+'OMP'+sep+'final_results_trunc_1.3'))
-        results_read.on_execute()
+        self.execute_uf(name='results.read', file='final_results_trunc_1.3', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'model_free'+sep+'OMP')
 
         # Play with the data.
-        deselect_all = uf_pages.deselect.All_page(self._wizard)
-        deselect_all.on_execute()
-
-        spin_copy = uf_pages.spin.Copy_page(self._wizard)
-        spin_copy.res_from.SetValue(str_to_gui('9 None'))
-        spin_copy.res_num_to.SetValue(str_to_gui('9'))
-        spin_copy.spin_name_to.SetValue(str_to_gui('NE'))
-        spin_copy.on_execute()
-
-        select_spin = uf_pages.select.Spin_page(self._wizard)
-        select_spin.spin_id.SetValue(str_to_gui(':9'))
-        select_spin.on_execute()
-        select_spin.spin_id.SetValue(str_to_gui(':10'))
-        select_spin.on_execute()
-        select_spin.spin_id.SetValue(str_to_gui(':11'))
-        select_spin.on_execute()
-
-        spin_name = uf_pages.spin.Name_page(self._wizard)
-        spin_name.name.SetValue(str_to_gui('N'))
-        spin_name.on_execute()
-
-        spin_element = uf_pages.spin.Element_page(self._wizard)
-        spin_element.spin_id.SetValue(str_to_gui('N'))
-        spin_element.on_execute()
-
-        #molecule_name = uf_pages.molecule.Name_page(self._wizard)
-        #molecule_name.name.SetValue(str_to_gui('OMP'))
-        #molecule_name.type.SetValue(str_to_gui('protein'))
-        #molecule_name.on_execute()
-
-        #bmrb_thiol_state = uf_pages.bmrb.Thiol_state_page(self._wizard)
-        #bmrb_thiol_state.state.SetValue(str_to_gui('reduced'))
-        #bmrb_thiol_state.on_execute()
+        self.execute_uf(name='deselect.all')
+        self.execute_uf(name='spin.copy', spin_from=':9', spin_to=':9@NE')
+        self.execute_uf(name='select.spin', ':9')
+        self.execute_uf(name='select.spin', ':10')
+        self.execute_uf(name='select.spin', ':11')
+        self.execute_uf(name='spin.name', name='N')
+        self.execute_uf(name='spin.element', element='N')
+        self.execute_uf(name='molecule.name', name='OMP')
+        self.execute_uf(name='molecule.type', type='protein')
+        self.execute_uf(name='bmrb.thiol_state', state='reduced')
 
         # Display the data (as a test).
-        #relax_data_display = uf_pages.relax_data.Display_page(self._wizard)
-        #relax_data_display.ri_id.SetValue(str_to_gui('R1_800'))
-        #relax_data_display.on_execute()
+        self.execute_uf(name='relax_data.display', ri_id='R1_800')
 
         # Temperature control and peak intensity type.
         ri_ids = ['R1_600', 'R2_600', 'NOE_600', 'R1_800', 'R2_800', 'NOE_800']
         for i in range(6):
-            pass
-            #relax_data_temp_calibration = uf_pages.relax_data.Temp_calibration_page(self._wizard)
-            #relax_data_temp_calibration.ri_id.SetValue(str_to_gui(ri_ids[i]))
-            #relax_data_temp_calibration.method.SetValue(str_to_gui('methanol'))
-            #relax_data_temp_calibration.on_execute()
-
-            #relax_data_temp_control = uf_pages.relax_data.Temp_control_page(self._wizard)
-            #relax_data_temp_control.ri_id.SetValue(str_to_gui(ri_ids[i]))
-            #relax_data_temp_control.method.SetValue(str_to_gui('single fid interleaving'))
-            #relax_data_temp_control.on_execute()
-
-            #relax_data_peak_intensity_type = uf_pages.relax_data.Peak_intensity_type_page(self._wizard)
-            #relax_data_peak_intensity_type.ri_id.SetValue(str_to_gui(ri_ids[i]))
-            #relax_data_peak_intensity_type.type.SetValue(str_to_gui('height'))
-            #relax_data_peak_intensity_type.on_execute()
+            self.execute_uf(name='relax_data.temp_calibration', ri_id=ri_ids[i], method='methanol')
+            self.execute_uf(name='relax_data.temp_control', ri_id=ri_ids[i], method='single fid interleaving')
+            self.execute_uf(name='relax_data.peak_intensity_type', ri_id=ri_ids[i], type='height')
 
         # Set up some BMRB information.
-        #bmrb_software_select = uf_pages.bmrb.Software_select_page(self._wizard)
-        #bmrb_software_select.name.SetValue(str_to_gui('NMRPipe'))
-        #bmrb_software_select.on_execute()
-        #bmrb_software_select.name.SetValue(str_to_gui('Sparky'))
-        #bmrb_software_select.version.SetValue(str_to_gui('3.106'))
-        #bmrb_software_select.on_execute()
+        self.execute_uf(name='bmrb.software_select', 'NMRPipe')
+        self.execute_uf(name='bmrb.software_select', 'Sparky', version='3.106')
 
-        self.execute_uf(page=uf_pages.bmrb.Citation_page, cite_id='test', authors=[["Edward", "d'Auvergne", "E.", "J."], ["Paul", "Gooley", "P.", "R."]], doi="10.1039/b702202f", pubmed_id="17579774", full_citation="d'Auvergne E. J., Gooley P. R. (2007). Set theory formulation of the model-free problem and the diffusion seeded model-free paradigm. Mol. Biosyst., 3(7), 483-494.", title="Set theory formulation of the model-free problem and the diffusion seeded model-free paradigm.", status="published", type="journal", journal_abbrev="Mol. Biosyst.", journal_full="Molecular Biosystems", volume=3, issue=7, page_first=483, page_last=498, year=2007)
-
-        #bmrb.software(name='X', url='http://www.nmr-relax.com', vendor_name='me', cite_ids=['test'], tasks=['procrastinating', 'nothing much', 'wasting time'])
-
-        #bmrb.script(file='noe.py', dir=status.install_path+sep+'sample_scripts', analysis_type='noe', engine='relax')
-        #bmrb.script(file='relax_fit.py', dir=status.install_path+sep+'sample_scripts', analysis_type='relax_fit', engine='relax')
-        #bmrb.script(file='dauvergne_protocol.py', dir=status.install_path+sep+'sample_scripts'+sep+'model_free', analysis_type='mf', model_selection='AIC', engine='relax', model_elim=True, universal_solution=True)
+        self.execute_uf(name='bmrb.citation', cite_id='test', authors=[["Edward", "d'Auvergne", "E.", "J."], ["Paul", "Gooley", "P.", "R."]], doi="10.1039/b702202f", pubmed_id="17579774", full_citation="d'Auvergne E. J., Gooley P. R. (2007). Set theory formulation of the model-free problem and the diffusion seeded model-free paradigm. Mol. Biosyst., 3(7), 483-494.", title="Set theory formulation of the model-free problem and the diffusion seeded model-free paradigm.", status="published", type="journal", journal_abbrev="Mol. Biosyst.", journal_full="Molecular Biosystems", volume=3, issue=7, page_first=483, page_last=498, year=2007)
+        self.execute_uf(name='bmrb.software', name='X', url='http://www.nmr-relax.com', vendor_name='me', cite_ids=['test'], tasks=['procrastinating', 'nothing much', 'wasting time'])
+        self.execute_uf(name='bmrb.script', file='noe.py', dir=status.install_path+sep+'sample_scripts', analysis_type='noe', engine='relax')
+        self.execute_uf(name='bmrb.script', file='relax_fit.py', dir=status.install_path+sep+'sample_scripts', analysis_type='relax_fit', engine='relax')
+        self.execute_uf(name='bmrb.script', file='dauvergne_protocol.py', dir=status.install_path+sep+'sample_scripts'+sep+'model_free', analysis_type='mf', model_selection='AIC', engine='relax', model_elim=True, universal_solution=True)
 
         # Write, then read the data to a new data pipe.
-        #bmrb.write(file=ds.tmpfile, dir=None, version=ds.version, force=True)
-        #pipe.create(pipe_name='new', pipe_type='mf')
-        #bmrb.read(file=ds.tmpfile, version=ds.version)
+        self.execute_uf(name='bmrb.write', file=ds.tmpfile, dir=None, version=ds.version, force=True)
+        self.execute_uf(name='pipe.create', pipe_name='new', pipe_type='mf')
+        self.execute_uf(name='bmrb.read', file=ds.tmpfile, version=ds.version)
 
         # Display tests.
-        #sequence.display()
-        #relax_data.display(ri_id='R1_800')
+        self.execute_uf(name='sequence.display')
+        self.execute_uf(name='relax_data.display', ri_id='R1_800')
 
         # Save the program state.
-        #state.save('devnull', force=True)
+        self.execute_uf(name='state.save', 'devnull', force=True)
