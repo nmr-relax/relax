89b7aae14235912a66328b9ff5f5f724753a0a3a
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-09 07:20:42 +0000

    Updated the copyright statement.

--- sample_scripts/relax_fit.py
+++ sample_scripts/relax_fit.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2004-2008 Edward d'Auvergne                                   #
+# Copyright (C) 2004-2012 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
