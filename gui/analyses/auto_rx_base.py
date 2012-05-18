504f6bc646fdd0a61f6fe664e0cbe7b05f6cef90
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-18 08:33:57 +0000

    The Rx auto-analysis peak loading wizard has been converted to the new auto-generated uf design.

--- gui/analyses/auto_rx_base.py
+++ gui/analyses/auto_rx_base.py
@@ -304,7 +304,7 @@ class Auto_rx(Base_analysis):
 
 
     def peak_wizard(self, event):
-        """Launch the NOE peak loading wizard.
+        """Launch the Rx peak loading wizard.
 
         @param event:   The wx event.
         @type event:    wx event
@@ -452,7 +452,7 @@ class Auto_rx(Base_analysis):
         """
 
         # Go to the spectrum.integration_points page.
-        int_method = gui_to_str(self.page_intensity.int_method.GetValue())
+        int_method = gui_to_str(self.page_intensity.uf_args['int_method'].GetValue())
         if int_method != 'height':
             return self.page_indices['pts']
 
@@ -469,7 +469,7 @@ class Auto_rx(Base_analysis):
         """
 
         # Go to the spectrum.integration_points page.
-        int_method = gui_to_str(self.page_intensity.int_method.GetValue())
+        int_method = gui_to_str(self.page_intensity.uf_args['int_method'].GetValue())
         if int_method != 'height':
             return self.page_indices['pts']
 
@@ -485,11 +485,11 @@ class Auto_rx(Base_analysis):
         page = self.wizard.get_page(self.page_indices['read'])
 
         # Set the spectrum ID.
-        id = page.spectrum_id.GetValue()
+        id = page.uf_args['spectrum_id'].GetValue()
 
         # Set the ID in the spectrum.replicated page.
         page = self.wizard.get_page(self.page_indices['pts'])
-        page.spectrum_id.SetStringSelection(str_to_gui(id))
+        page.uf_args['spectrum_id'].SetValue(id)
 
 
     def wizard_update_repl(self):
@@ -499,11 +499,11 @@ class Auto_rx(Base_analysis):
         page = self.wizard.get_page(self.page_indices['read'])
 
         # Set the spectrum ID.
-        id = page.spectrum_id.GetValue()
+        id = page.uf_args['spectrum_id'].GetValue()
 
         # Set the ID in the spectrum.replicated page.
         page = self.wizard.get_page(self.page_indices['repl'])
-        page.spectrum_id_boxes[0].SetStringSelection(str_to_gui(id))
+        page.uf_args['spectrum_ids'].SetValue(value=id, index=0)
 
 
     def wizard_update_rmsd(self):
@@ -513,11 +513,11 @@ class Auto_rx(Base_analysis):
         page = self.wizard.get_page(self.page_indices['read'])
 
         # Set the spectrum ID.
-        id = page.spectrum_id.GetValue()
+        id = page.uf_args['spectrum_id'].GetValue()
 
         # Set the ID in the spectrum.baseplane_rmsd page.
         page = self.wizard.get_page(self.page_indices['rmsd'])
-        page.spectrum_id.SetStringSelection(str_to_gui(id))
+        page.uf_args['spectrum_id'].SetValue(id)
 
 
     def wizard_update_relax_time(self):
@@ -527,11 +527,11 @@ class Auto_rx(Base_analysis):
         page = self.wizard.get_page(self.page_indices['read'])
 
         # Set the spectrum ID.
-        id = page.spectrum_id.GetValue()
+        id = page.uf_args['spectrum_id'].GetValue()
 
         # Set the ID in the relax_fit.relax_time page.
         page = self.wizard.get_page(self.page_indices['relax_time'])
-        page.spectrum_id.SetStringSelection(str_to_gui(id))
+        page.uf_args['spectrum_id'].SetValue(id)
 
 
 
