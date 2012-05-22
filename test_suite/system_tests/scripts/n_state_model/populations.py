ff440cceee17b5dd27916a39f1b90f1341b212c3
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-22 11:55:56 +0000

    Converted all of the N-state model scripts to use the new self._execute_uf() interface.

--- test_suite/system_tests/scripts/n_state_model/populations.py
+++ test_suite/system_tests/scripts/n_state_model/populations.py
@@ -13,32 +13,32 @@ str_path = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'structu
 data_path = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'align_data'+sep+'population_model'
 
 # Create the data pipe.
-pipe.create('populations', 'N-state')
+self._execute_uf(uf_name='pipe.create', pipe_name='populations', pipe_type='N-state')
 
 # Load the structures.
 NUM_STR = 3
 i = 1
 for model in [1, 3, 2]:
-    structure.read_pdb(file='lactose_MCMM4_S1_%i.pdb' % i, dir=str_path, set_model_num=model, set_mol_name='LE')
+    self._execute_uf(uf_name='structure.read_pdb', file='lactose_MCMM4_S1_%i.pdb' % i, dir=str_path, set_model_num=model, set_mol_name='LE')
     i += 1
 
 # Load the sequence information.
-structure.load_spins(spin_id=':UNK@C*', ave_pos=False)
-structure.load_spins(spin_id=':UNK@H*', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id=':UNK@C*', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id=':UNK@H*', ave_pos=False)
 
 # Deselect the CH2 protons (the rotation of these doesn't work in the model, but the carbon doesn't move).
-deselect.spin(spin_id=':UNK@H6')
-deselect.spin(spin_id=':UNK@H7')
-deselect.spin(spin_id=':UNK@H17')
-deselect.spin(spin_id=':UNK@H18')
+self._execute_uf(uf_name='deselect.spin', spin_id=':UNK@H6')
+self._execute_uf(uf_name='deselect.spin', spin_id=':UNK@H7')
+self._execute_uf(uf_name='deselect.spin', spin_id=':UNK@H17')
+self._execute_uf(uf_name='deselect.spin', spin_id=':UNK@H18')
 
 # Load the CH vectors for the C atoms.
-structure.vectors(spin_id='@C*', attached='H*', ave=False)
+self._execute_uf(uf_name='structure.vectors', spin_id='@C*', attached='H*', ave=False)
 
 # Set the values needed to calculate the dipolar constant.
-value.set(1.10 * 1e-10, 'r', spin_id="@C*")
-value.set('13C', 'heteronuc_type', spin_id="@C*")
-value.set('1H', 'proton_type', spin_id="@C*")
+self._execute_uf(uf_name='value.set', val=1.10 * 1e-10, param='r', spin_id="@C*")
+self._execute_uf(uf_name='value.set', val='13C', param='heteronuc_type', spin_id="@C*")
+self._execute_uf(uf_name='value.set', val='1H', param='proton_type', spin_id="@C*")
 
 # File list.
 align_list = ['Dy', 'Tb', 'Tm', 'Er']
@@ -46,44 +46,44 @@ align_list = ['Dy', 'Tb', 'Tm', 'Er']
 # Load the RDCs and PCSs.
 for i in xrange(len(align_list)):
     # The RDC.
-    rdc.read(align_id=align_list[i], file='missing_rdc_%i' % i, dir=data_path, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
+    self._execute_uf(uf_name='rdc.read', align_id=align_list[i], file='missing_rdc_%i' % i, dir=data_path, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
 
     # The PCS.
-    pcs.read(align_id=align_list[i], file='missing_pcs_%i' % i, dir=data_path, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
+    self._execute_uf(uf_name='pcs.read', align_id=align_list[i], file='missing_pcs_%i' % i, dir=data_path, mol_name_col=1, res_num_col=2, res_name_col=3, spin_num_col=None, spin_name_col=5, data_col=6, error_col=None)
 
     # The temperature.
-    temperature(id=align_list[i], temp=298)
+    self._execute_uf(uf_name='temperature', id=align_list[i], temp=298)
 
     # The frequency.
-    frq.set(id=align_list[i], frq=799.75376122 * 1e6)
+    self._execute_uf(uf_name='frq.set', id=align_list[i], frq=799.75376122 * 1e6)
 
 # Set the paramagnetic centre.
-paramag.centre([ -14.845,    0.969,    0.265])
+self._execute_uf(uf_name='paramag.centre', pos=[ -14.845,    0.969,    0.265])
 
 
 # The solution.
 ###############
 
 # Set up the model.
-n_state_model.select_model(model='population')
+self._execute_uf(uf_name='n_state_model.select_model', model='population')
 print n_state_model_obj._assemble_param_vector()
 
 # Set pc to the exact values.
-value.set(0.3, 'p0')
-value.set(0.1, 'p2')
-value.set(0.6, 'p1')
+self._execute_uf(uf_name='value.set', val=0.3, param='p0')
+self._execute_uf(uf_name='value.set', val=0.1, param='p2')
+self._execute_uf(uf_name='value.set', val=0.6, param='p1')
 
 # Set the tensors.
-align_tensor.init(tensor=align_list[0], params=( 1.42219822168827662867e-04, -1.44543001566521341940e-04, -7.07796211648713973798e-04, -6.01619494082773244303e-04,  2.02008007072950861996e-04), param_types=2)
-align_tensor.init(tensor=align_list[1], params=( 3.56720663040924505435e-04, -2.68385787902088840916e-04, -1.69361406642305853832e-04,  1.71873715515064501074e-04, -3.05790155096090983822e-04), param_types=2)
-align_tensor.init(tensor=align_list[2], params=( 2.32088908680377300801e-07,  2.08076808579168379617e-06, -2.21735465435989729223e-06, -3.74311563209448033818e-06, -2.40784858070560310370e-06), param_types=2)
-align_tensor.init(tensor=align_list[3], params=(-2.62495279588228071048e-04,  7.35617367964106275147e-04,  6.39754192258981332648e-05,  6.27880171180572523460e-05,  2.01197582457700226708e-04), param_types=2)
+self._execute_uf(uf_name='align_tensor.init', tensor=align_list[0], params=( 1.42219822168827662867e-04, -1.44543001566521341940e-04, -7.07796211648713973798e-04, -6.01619494082773244303e-04,  2.02008007072950861996e-04), param_types=2)
+self._execute_uf(uf_name='align_tensor.init', tensor=align_list[1], params=( 3.56720663040924505435e-04, -2.68385787902088840916e-04, -1.69361406642305853832e-04,  1.71873715515064501074e-04, -3.05790155096090983822e-04), param_types=2)
+self._execute_uf(uf_name='align_tensor.init', tensor=align_list[2], params=( 2.32088908680377300801e-07,  2.08076808579168379617e-06, -2.21735465435989729223e-06, -3.74311563209448033818e-06, -2.40784858070560310370e-06), param_types=2)
+self._execute_uf(uf_name='align_tensor.init', tensor=align_list[3], params=(-2.62495279588228071048e-04,  7.35617367964106275147e-04,  6.39754192258981332648e-05,  6.27880171180572523460e-05,  2.01197582457700226708e-04), param_types=2)
 print n_state_model_obj._assemble_param_vector()
 
 # Calculation.
 print cdp
 print cdp.mol[0].res[0].spin[0]
-calc()
+self._execute_uf(uf_name='calc')
 print("Chi2: %s" % cdp.chi2)
 if abs(cdp.chi2) > 1e-15:
     raise RelaxError, "The chi2 at the solution is not zero!"
@@ -93,8 +93,8 @@ if abs(cdp.chi2) > 1e-15:
 ###########################
 
 # Minimisation.
-minimise('sd', func_tol=1e-2)
+self._execute_uf(uf_name='minimise', min_algor='sd', func_tol=1e-2)
 print cdp
 
 # Write out a results file.
-results.write('devnull', force=True)
+self._execute_uf(uf_name='results.write', file='devnull', force=True)
--- test_suite/system_tests/scripts/n_state_model/rdc_tensor.py
+++ test_suite/system_tests/scripts/n_state_model/rdc_tensor.py
@@ -14,48 +14,48 @@ STRUCT_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'stru
 tag = "Dy C-dom"
 
 # Create the data pipe.
-pipe.create(tag, 'N-state')
+self._execute_uf(uf_name='pipe.create', pipe_name=tag, pipe_type='N-state')
 
 # Load the CaM structures.
-structure.read_pdb('bax_C_1J7P_N_H_Ca.pdb', dir=STRUCT_PATH)
+self._execute_uf(uf_name='structure.read_pdb', file='bax_C_1J7P_N_H_Ca.pdb', dir=STRUCT_PATH)
 
 # Load the spins.
-structure.load_spins('@N', ave_pos=False)
-structure.load_spins('@H', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id='@N', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id='@H', ave_pos=False)
 
 # Load the NH vectors.
-structure.vectors('H', '@N', ave=False)
+self._execute_uf('H', '@N', ave=False, uf_name='structure.vectors')
 
 # Set the values needed to calculate the dipolar constant.
-value.set(NH_BOND_LENGTH_RDC, 'r')
-value.set('15N', 'heteronuc_type')
-value.set('1H', 'proton_type')
+self._execute_uf(NH_BOND_LENGTH_RDC, 'r', uf_name='value.set')
+self._execute_uf('15N', 'heteronuc_type', uf_name='value.set')
+self._execute_uf('1H', 'proton_type', uf_name='value.set')
 
 # RDCs.
-rdc.read(tag, file='rdc_dy', dir=DATA_PATH, res_num_col=1, spin_name_col=2, data_col=3, error_col=4, neg_g_corr=True)
+self._execute_uf(uf_name='rdc.read', align_id=tag, file='rdc_dy', dir=DATA_PATH, res_num_col=1, spin_name_col=2, data_col=3, error_col=4, neg_g_corr=True)
 
 # Set up the model.
-n_state_model.select_model('fixed')
+self._execute_uf(uf_name='n_state_model.select_model', model='fixed')
 
 # Minimisation.
-grid_search(inc=3)
-minimise('newton', constraints=False)
+self._execute_uf(uf_name='grid_search', inc=3)
+self._execute_uf(uf_name='minimise', 'newton', constraints=False)
 
 # Monte Carlo simulations.
-monte_carlo.setup(number=3)
-monte_carlo.create_data()
-monte_carlo.initial_values()
-minimise('newton', constraints=False)
-monte_carlo.error_analysis()
+self._execute_uf(uf_name='monte_carlo.setup', number=3)
+self._execute_uf(uf_name='monte_carlo.create_data')
+self._execute_uf(uf_name='monte_carlo.initial_values')
+self._execute_uf(uf_name='minimise', min_algor='newton', constraints=False)
+self._execute_uf(uf_name='monte_carlo.error_analysis')
 
 # Show the tensors.
-align_tensor.display(tag)
+self._execute_uf(uf_name='align_tensor.display', align_id=tag)
 
 # Back calc.
-rdc.back_calc()
+self._execute_uf(uf_name='rdc.back_calc')
 
 # Q-factors.
-rdc.calc_q_factors()
+self._execute_uf(uf_name='rdc.calc_q_factors')
 
 # Correlation plots.
-rdc.corr_plot(file="devnull", force=True)
+self._execute_uf(uf_name='rdc.corr_plot', file="devnull", force=True)
