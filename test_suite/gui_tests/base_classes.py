55bd30f233438208ee249015460dafb119c8d938
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-22 13:08:08 +0000

    Fix for the file selection and directory handling in the GUI base test _execute_uf() method.
    This now handles the situation of when no directory argument is supplied.

--- test_suite/gui_tests/base_classes.py
+++ test_suite/gui_tests/base_classes.py
@@ -95,8 +95,8 @@ class GuiTestCase(TestCase):
                     if uf_data.kargs[j]['arg_type'] == 'file sel':
                         file_sel_name = uf_data.kargs[j]['name']
 
-                # Prepend the directory to the file, if needed.
-                if file_sel_name in kargs:
+                # Prepend the directory to the file, if needed and supplied.
+                if file_sel_name in kargs and kargs[arg['name']]:
                     kargs[file_sel_name] = kargs[arg['name']] + sep + kargs[file_sel_name]
 
                 # Remove the directory argument.
