3b5b6a68fa8d0531b4c34660346a612fb066c26b
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-25 16:19:05 +0000

    This should have been part of r16460!

--- test_suite/gui_tests/__init__.py
+++ test_suite/gui_tests/__init__.py
@@ -33,6 +33,7 @@ from relax_errors import RelaxError
 
 # relax GUI test module imports.
 from bmrb import Bmrb
+from consistency_tests import Ct
 from jw_mapping import Jw_mapping
 from model_free import Mf
 from n_state_model import N_state_model
@@ -43,6 +44,7 @@ from test_suite.relax_test_loader import RelaxTestLoader as TestLoader
 
 
 __all__ = ['bmrb',
+           'consistency_tests',
            'jw_mapping',
            'model_free',
            'n_state_model',
@@ -104,6 +106,7 @@ class GUI_test_runner:
         # All tests.
         if not tests:
             suite_array.append(TestLoader().loadTestsFromTestCase(Bmrb))
+            suite_array.append(TestLoader().loadTestsFromTestCase(Ct))
             suite_array.append(TestLoader().loadTestsFromTestCase(Jw_mapping))
             suite_array.append(TestLoader().loadTestsFromTestCase(Mf))
             suite_array.append(TestLoader().loadTestsFromTestCase(N_state_model))
