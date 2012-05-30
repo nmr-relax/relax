5950b06f33b8a86b9e5c8f33d539fdd56902bcb7
Edward d'Auvergne <edward@nmr-relax.com>
2012-05-30 19:40:21 +0000

    Increased the size of the unit vector loading wizard in the model-free analysis.
    This now matches the structure.read_pdb user function size and allows it to fit!

--- gui/analyses/auto_model_free.py
+++ gui/analyses/auto_model_free.py
@@ -583,7 +583,7 @@ class Auto_model_free(Base_analysis):
         wx.BeginBusyCursor()
 
         # Create the wizard.
-        self.vect_wizard = Wiz_window(parent=self.gui, size_x=800, size_y=600, title="Load unit vectors from file")
+        self.vect_wizard = Wiz_window(parent=self.gui, size_x=1000, size_y=800, title="Load unit vectors from file")
 
         # Create the PDB reading page.
         page = uf_store['structure.read_pdb'].create_page(self.vect_wizard)
