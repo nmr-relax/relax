7416d546712e308662d4710e795007d14d5d15ea
Edward d'Auvergne <edward@nmr-relax.com>
2012-06-05 07:33:29 +0000

    The GUI user function window titles are now set to the 'title_short' value.
    This is because the long title can be far too long for a GUI window title!

--- gui/uf_objects.py
+++ gui/uf_objects.py
@@ -1063,7 +1063,7 @@ class Uf_storage(dict):
             # Generate the user functions.
             for name, data in uf_info.uf_loop():
                 # Generate a new container.
-                obj = Uf_object(name, title=data.title, size=data.wizard_size, height_desc=data.wizard_height_desc, apply_button=data.wizard_apply_button, sync=data.gui_sync)
+                obj = Uf_object(name, title=data.title_short, size=data.wizard_size, height_desc=data.wizard_height_desc, apply_button=data.wizard_apply_button, sync=data.gui_sync)
 
                 # Store it.
                 self._instance[name] = obj
