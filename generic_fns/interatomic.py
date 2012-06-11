c5c9b6e01de1772e3edb8359754098eebf82527e
Edward d'Auvergne <edward@nmr-relax.com>
2012-06-11 18:47:41 +0000

    Created the generic_fns.interatomic module and added the return_container() method.

--- generic_fns/interatomic.py
+++ generic_fns/interatomic.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2004-2012 Edward d'Auvergne                                   #
+# Copyright (C) 2012 Edward d'Auvergne                                        #
 #                                                                             #
 # This file is part of the program relax.                                     #
 #                                                                             #
@@ -20,45 +20,41 @@
 #                                                                             #
 ###############################################################################
 
+# Module docstring.
+"""Module for the manipulation of the interatomic data structures in the relax data store."""
 
-__all__ = [ 'align_tensor',
-            'angles',
-            'bmrb',
-            'bruker',
-            'dasha',
-            'diffusion_tensor',
-            'eliminate',
-            'exp_info',
-            'fix',
-            'frame_order',
-            'frq',
-            'grace',
-            'interatomic',
-            'minimise',
-            'model_selection',
-            'mol_res_spin',
-            'molmol',
-            'monte_carlo',
-            'noesy',
-            'palmer',
-            'paramag',
-            'pcs',
-            'pipes',
-            'pymol_control',
-            'rdc',
-            'relax_data',
-            'relax_re',
-            'reset',
-            'results',
-            'script',
-            'selection',
-            'sequence',
-            'spectrum',
-            'state',
-            'structure',
-            'sys_info',
-            'temperature',
-            'value',
-            'vmd',
-            'xplor'
-]
+# relax module imports.
+from generic_fns import pipes
+
+
+def return_container(spin_id1=None, spin_id2=None, pipe=None):
+    """Return the interatomic data container for the two spins.
+
+    @keyword spin_id1:  The spin ID string of the first atom.
+    @type spin_id1:     str
+    @keyword spin_id2:  The spin ID string of the first atom.
+    @type spin_id2:     str
+    @keyword pipe:      The data pipe holding the container.  Defaults to the current data pipe.
+    @type pipe:         str or None
+    @return:            The interatomic data container, if it exists.
+    @rtype:             data.interatomic.InteratomContainer instance or None
+    """
+
+    # The data pipe.
+    if pipe == None:
+        pipe = pipes.cdp_name()
+
+    # Get the data pipe.
+    dp = pipes.get_pipe(pipe)
+
+    # No interatomic data.
+    if not hasattr(dp, 'interatomic'):
+        return None
+
+    # Loop over the data.
+    for i in range(len(dp.interatomic)):
+        if dp.interatomic[i].id_match(spin_id1, spin_id2):
+            return dp.interatomic[i]
+
+    # No container found.
+    return None
