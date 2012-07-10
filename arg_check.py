258521205fab1aefb6afb9d307fa4ff53ab8e21a
Edward d'Auvergne <edward@nmr-relax.com>
2012-07-10 18:43:53 +0000

    More GPL v3 license updates.

--- arg_check.py
+++ arg_check.py
@@ -2,20 +2,20 @@
 #                                                                             #
 # Copyright (C) 2009-2012 Edward d'Auvergne                                   #
 #                                                                             #
-# This file is part of the program relax.                                     #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
-# relax is free software; you can redistribute it and/or modify               #
+# This program is free software: you can redistribute it and/or modify        #
 # it under the terms of the GNU General Public License as published by        #
-# the Free Software Foundation; either version 3 of the License, or           #
+# the Free Software Foundation, either version 3 of the License, or           #
 # (at your option) any later version.                                         #
 #                                                                             #
-# relax is distributed in the hope that it will be useful,                    #
+# This program is distributed in the hope that it will be useful,             #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of              #
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
 # GNU General Public License for more details.                                #
 #                                                                             #
 # You should have received a copy of the GNU General Public License           #
-# along with relax.  If not, see <http://www.gnu.org/licenses/>.              #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #                                                                             #
 ###############################################################################
 
--- maths_fns/exponential.h
+++ maths_fns/exponential.h
@@ -2,20 +2,20 @@
  * Copyright (C) 2006 Gary S Thompson (see https://gna.org/users for contact
  *                                     details)
  *
- * This file is part of the program relax.
+ * This file is part of the program relax (http://www.nmr-relax.com).
  *
- * relax is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * relax is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with relax.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef RELAX_EXPONENTIAL 
 #define RELAX_EXPONENTIAL
