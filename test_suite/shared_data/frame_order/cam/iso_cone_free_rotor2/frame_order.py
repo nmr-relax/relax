1c2a1ad1754b13bba5508ab9ae4b9d81e5cd6f03
Edward d'Auvergne <edward@nmr-relax.com>
2012-07-20 07:57:35 +0000

    Updated the 2nd free-rotor isotropic cone frame order optimisation script to match the test suite.

--- test_suite/shared_data/frame_order/cam/iso_cone_free_rotor2/frame_order.py
+++ test_suite/shared_data/frame_order/cam/iso_cone_free_rotor2/frame_order.py
@@ -98,11 +98,12 @@ class Analysis:
         paramag.centre(pos=[35.934, 12.194, -4.206])
 
         # Check the minimum.
-        value.set(param='ave_pos_beta', val=1.1321377968123119)
-        value.set(param='ave_pos_gamma', val=0.029867114268410672)
-        value.set(param='axis_theta', val=0.69828059268540488)
-        value.set(param='axis_phi', val=-2.2509097944247132)
-        value.set(param='cone_s1', val=iso_cone_theta_to_S(20.0 * 2.0 * pi / 360.0))
+        value.set(param='ave_pos_beta', val=0.77485244603476433)
+        value.set(param='ave_pos_gamma', val=0.1420704587263735)
+        value.set(param='axis_theta', val=0.69828059079619353433)
+        value.set(param='axis_phi', val=4.03227550621962294031)
+        value.set(param='cone_s1', val=iso_cone_theta_to_S(1.2))
+        cdp.num_int_pts = 1000000
         calc()
         print("\nchi2: %s" % cdp.chi2)
 
