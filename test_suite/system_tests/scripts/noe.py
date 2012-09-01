8260106137dc919231a028b4ae02ed8df04a7c6f
Edward d'Auvergne <edward@nmr-relax.com>
2012-09-01 08:17:30 +0000

    Modified the NOE system test to test the usage of Trp indole 15N data.
    This is to catch bug #20119 (https://gna.org/bugs/index.php?20119).

--- test_suite/system_tests/scripts/noe.py
+++ test_suite/system_tests/scripts/noe.py
@@ -15,9 +15,14 @@ sequence.read(file='Ap4Aase.seq', dir=status.install_path + sep+'test_suite'+sep
 # Name the spins so they can be matched to the assignments.
 spin.name(name='N')
 
+# Add the Trp 40 indole spin.
+spin.create(res_num=40, spin_name='NE1')
+
 # Load the reference spectrum and saturated spectrum peak intensities.
-spectrum.read_intensities(file='ref_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='ref_ave')
-spectrum.read_intensities(file='sat_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='sat_ave')
+spectrum.read_intensities(file='ref_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='ref_ave', heteronuc='N', proton='HN')
+spectrum.read_intensities(file='ref_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='ref_ave', heteronuc='NE1', proton='HE1')
+spectrum.read_intensities(file='sat_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='sat_ave', heteronuc='N', proton='HN')
+spectrum.read_intensities(file='sat_ave.list', dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', spectrum_id='sat_ave', heteronuc='NE1', proton='HE1')
 
 # Set the spectrum types.
 noe.spectrum_type('ref', 'ref_ave')
