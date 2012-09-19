7e2b96d20c25f300cddc90dc598371eec172b162
Edward d'Auvergne <edward@nmr-relax.com>
2012-09-19 07:35:49 +0000

    Improvements to the descriptions of the structure.read_pdb user function arguments.

--- user_functions/structure.py
+++ user_functions/structure.py
@@ -485,28 +485,28 @@ uf.add_keyarg(
 uf.add_keyarg(
     name = "read_mol",
     py_type = "int_or_int_list",
-    desc_short = "read molecule number",
+    desc_short = "molecule number to read",
     desc = "If set, only the given molecule(s) will be read.  The molecules are determined differently by the different parsers, but are numbered consecutively from 1.  If unset, then all molecules will be loaded.  By providing a list of numbers such as [1, 2], multiple molecules will be read.",
     can_be_none = True
 )
 uf.add_keyarg(
     name = "set_mol_name",
     py_type = "str_or_str_list",
-    desc_short = "set molecule names",
+    desc_short = "setting of molecule names",
     desc = "Set the names of the read molecules.  If unset, then the molecules will be automatically labelled based on the file name or other information.  This can either be a single name or a list of names.",
     can_be_none = True
 )
 uf.add_keyarg(
     name = "read_model",
     py_type = "int_or_int_list",
-    desc_short = "read model",
-    desc = "If set, only the given model number(s) from the PDB file will be read.  This can be a single number or list of numbers.",
+    desc_short = "model to read",
+    desc = "If set, only the given model number(s) from the PDB file will be read.  Otherwise all models will be read.  This can be a single number or list of numbers.",
     can_be_none = True
 )
 uf.add_keyarg(
     name = "set_model_num",
     py_type = "int_or_int_list",
-    desc_short = "set model numbers",
+    desc_short = "setting of model numbers",
     desc = "Set the model numbers of the loaded molecules.  If unset, then the PDB model numbers will be preserved if they exist.  This can be a single number or list of numbers.",
     can_be_none = True
 )
