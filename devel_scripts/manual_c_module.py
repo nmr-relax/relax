91e7108a5a628188fe390f67421cd67593f46939
Edward d'Auvergne <edward@nmr-relax.com>
2012-10-07 13:56:44 +0000

    The C module compilation testing script now accepts the Python version as a first argument.

--- devel_scripts/manual_c_module.py
+++ devel_scripts/manual_c_module.py
@@ -3,24 +3,25 @@
 # Script for manually compiling the C modules for different Python targets.
 
 from os import system
+import sys
 
 
 # The target.
-target = 'python3.2'
+target = sys.argv[1]
 path = '/data/python/'
 
 # The list of build commands to run.
 cmd = []
 
 # Python.h.
-include = '%s/include/%s/' % (path, target)
-if target == 'python3.2':
+include = '%s/include/python%s/' % (path, target)
+if target == '3.2':
     include = '%s/include/python3.2m/' % path
-elif target == 'python3.3':
+elif target == '3.3':
     include = '%s/include/python3.3m/' % path
 
 # numpy includes.
-numpy_include = '%s/lib/%s/site-packages/numpy/core/include/' % (path, target)
+numpy_include = '%s/lib/python%s/site-packages/numpy/core/include/' % (path, target)
 
 # Python 3.2 installed in the home directory.
 cmd.append("gcc -o maths_fns/c_chi2.os -c -I%s -I%s -fPIC maths_fns/c_chi2.c" % (include, numpy_include))
