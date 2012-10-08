03b3a8bc2f89f007e1dd7a5663eaa046e86c1d27
Edward d'Auvergne <edward@nmr-relax.com>
2012-10-08 16:44:12 +0000

    Fix for the diffusion tensor to_xml() method for the new design.
    For some reason the methods of the Element class are no longer blacklisted.

--- data/diff_tensor.py
+++ data/diff_tensor.py
@@ -1116,7 +1116,7 @@ class DiffTensorData(Element):
         tensor_element.setAttribute('type', self.type)
 
         # Add all simple python objects within the PipeContainer to the pipe element.
-        fill_object_contents(doc, tensor_element, object=self, blacklist=['type'] + list(self.__class__.__dict__.keys()))
+        fill_object_contents(doc, tensor_element, object=self, blacklist=['type', 'is_empty'] + list(self.__class__.__dict__.keys()))
 
 
 
