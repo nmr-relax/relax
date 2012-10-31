e18cf618030f6801dd764abd841fae05799b4764
Edward d'Auvergne <edward@nmr-relax.com>
2012-10-31 17:52:32 +0000

    Converted the RelaxError introduced in r17944 to a RelaxWarning.
    There are some cases where deselection of all spins is ok, for example an analysis using the local
    tm models where some models are run but skipped when they have too many parameters.

--- specific_fns/model_free/main.py
+++ specific_fns/model_free/main.py
@@ -33,7 +33,7 @@ from generic_fns import diffusion_tensor, interatomic, pipes, sequence
 from generic_fns.mol_res_spin import count_spins, exists_mol_res_spin_data, find_index, return_spin, return_spin_from_index, return_spin_indices, spin_loop
 import specific_fns
 from relax_errors import RelaxError, RelaxFault, RelaxFuncSetupError, RelaxNoModelError, RelaxNoSequenceError, RelaxNoTensorError, RelaxTensorError
-from relax_warnings import RelaxDeselectWarning
+from relax_warnings import RelaxDeselectWarning, RelaxWarning
 from user_functions.data import Uf_tables; uf_tables = Uf_tables()
 from user_functions.objects import Desc_container
 
@@ -2032,7 +2032,7 @@ class Model_free_main:
 
         # No spins selected, so fail hard to prevent the user from going any further.
         if spin_count == 0:
-            raise RelaxError("No spins are selected, therefore the optimisation or calculation cannot proceed.")
+            warn(RelaxWarning("No spins are selected therefore the optimisation or calculation cannot proceed."))
 
         # Final printout.
         if verbose and not deselect_flag:
