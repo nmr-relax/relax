9cd950114136924377a68abf21e856f144a897b4
Edward d'Auvergne <edward@nmr-relax.com>
2012-12-17 17:50:15 +0000

    Created the Selection.test_select_domain_bool_and system test to check the select.domain user function.
    This user function does not exist yet.

--- test_suite/system_tests/selection.py
+++ test_suite/system_tests/selection.py
@@ -78,3 +78,25 @@ class Selection(SystemTestCase):
 
         # Check the data.
         self.check_spin_selection([False]*9)
+
+
+    def test_select_domain_bool_and(self):
+        """Check the operation of the select.domain user function using the AND boolean."""
+
+        # First deselect some spins.
+        self.interpreter.deselect.spin(":1@C")
+        self.interpreter.deselect.spin(":2@H")
+        self.interpreter.deselect.spin(":2@C")
+
+        # Display the sequence.
+        self.interpreter.sequence.display()
+
+        # Define the domain.
+        self.interpreter.domain(id='N', spin_id=":1-2")
+
+        # Select the domain.
+        self.interpreter.select.domain(domain_id='N', boolean="AND")
+
+        # Check the selections.
+        selection = [False, True, True, False, True, False] + [False]*3
+        self.check_spin_selection(selection)
