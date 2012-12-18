e14a2f38e54f0011b4dec536ae8e1f7328a953de
Edward d'Auvergne <edward@nmr-relax.com>
2012-12-18 09:49:21 +0000

    Created the Selection.test_select_domain_bool_or system test.
    This is to better check the operation of the select.domain user function.

--- test_suite/system_tests/selection.py
+++ test_suite/system_tests/selection.py
@@ -87,6 +87,7 @@ class Selection(SystemTestCase):
         self.interpreter.deselect.spin(":1@C")
         self.interpreter.deselect.spin(":2@H")
         self.interpreter.deselect.spin(":2@C")
+        self.interpreter.deselect.spin(":3@H")
 
         # Display the sequence.
         self.interpreter.sequence.display()
@@ -99,4 +100,27 @@ class Selection(SystemTestCase):
 
         # Check the selections.
         selection = [False, True, True, False, True, False] + [False]*3
+        self.check_spin_selection(selection)
+
+
+    def test_select_domain_bool_or(self):
+        """Check the operation of the select.domain user function using the OR boolean."""
+
+        # First deselect some spins.
+        self.interpreter.deselect.spin(":1@C")
+        self.interpreter.deselect.spin(":2@H")
+        self.interpreter.deselect.spin(":2@C")
+        self.interpreter.deselect.spin(":3@H")
+
+        # Display the sequence.
+        self.interpreter.sequence.display()
+
+        # Define the domain.
+        self.interpreter.domain(id='N', spin_id=":1-2")
+
+        # Select the domain.
+        self.interpreter.select.domain(domain_id='N', boolean="OR", change_all=False)
+
+        # Check the selections.
+        selection = [True]*6 + [True, True, False]
         self.check_spin_selection(selection)
