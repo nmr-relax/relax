b28456db27eaf84dc110739ba2605ff816ea97e8
Edward d'Auvergne <edward@nmr-relax.com>
2013-01-18 15:10:14 +0000

    Created the Pcs.test_load_multi_col_data system test to demonstrate a failure of PCS data loading.
    This is a problem when 15N data is in one column and 1H data is in another, and the spin_id argument
    is used to specify which is which.

--- test_suite/system_tests/pcs.py
+++ test_suite/system_tests/pcs.py
@@ -35,6 +35,41 @@ from test_suite.system_tests.base_classes import SystemTestCase
 class Pcs(SystemTestCase):
     """Class for testing PCS operations."""
 
+    def test_load_multi_col_data(self):
+        """Test the loading of PCS data from a file with different columns for each spin type."""
+
+        # Create a data pipe for all the data.
+        self.interpreter.pipe.create('CaM N-dom', 'N-state')
+
+        # Create some spins.
+        self.interpreter.spin.create(spin_name='N', spin_num=1, res_name='Gly', res_num=3)
+        self.interpreter.spin.create(spin_name='H', spin_num=2, res_name='Gly', res_num=3)
+        self.interpreter.spin.create(spin_name='N', spin_num=3, res_name='Gly', res_num=4)
+        self.interpreter.spin.create(spin_name='H', spin_num=4, res_name='Gly', res_num=4)
+        self.interpreter.sequence.display()
+
+        # Data directory.
+        dir = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'align_data'+sep
+
+        # PCSs.
+        self.interpreter.pcs.read(align_id='dy', file='pcs_dy_200911.txt', dir=dir, res_num_col=1, data_col=2, error_col=4, spin_id='@N')
+        self.interpreter.pcs.read(align_id='dy', file='pcs_dy_200911.txt', dir=dir, res_num_col=1, data_col=3, error_col=4, spin_id='@H')
+
+        # The data.
+        pcs_data = {
+            ':3@N': 0.917,
+            ':3@H': 0.843,
+            ':4@N': 1.131,
+            ':4@H': 1.279,
+        }
+
+        # Check the PCS data.
+        for spin, spin_id in spin_loop(return_id=True):
+            self.assert_(hasattr(spin, 'pcs'))
+            self.assertEqual(spin.pcs['dy'], pcs_data[spin_id])
+            self.assertEqual(spin.pcs_err['dy'], 0.1)
+
+
     def test_pcs_copy(self):
         """Test the operation of the pcs.copy user function."""
 
