10e901a10b5d4b36ed4696832a68d0cb70f82546
Edward d'Auvergne <edward@nmr-relax.com>
2013-02-01 10:28:43 +0000

    Clean up of the Mf.test_bug_20464_missing_ri_data system test.
    The directories and files created are now placed in a temporary directory to be removed by the
    tearDown() method, and the optimisation is now much faster.

--- test_suite/system_tests/scripts/model_free/bug_20464_missing_ri_data.py
+++ test_suite/system_tests/scripts/model_free/bug_20464_missing_ri_data.py
@@ -3,9 +3,11 @@
 
 # Python module imports.
 from os import sep
+from tempfile import mkdtemp
 
 # relax module imports.
 from auto_analyses.dauvergne_protocol import dAuvergne_protocol
+from data import Relax_data_store; ds = Relax_data_store()
 from status import Status; status = Status()
 
 
@@ -14,10 +16,10 @@ DIFF_MODEL = 'local_tm'
 
 # The model-free models.  Do not change these unless absolutely necessary, the protocol is likely to fail if these are changed.
 MF_MODELS = ['m0', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9']
-LOCAL_TM_MODELS = ['tm0', 'tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8', 'tm9']
+LOCAL_TM_MODELS = ['tm0', 'tm4', 'tm9']
 
 # The grid search size (the number of increments per dimension).
-GRID_INC = 11
+GRID_INC = 3
 
 # The optimisation technique.
 MIN_ALGOR = 'newton'
@@ -35,5 +37,12 @@ data_path = status.install_path + sep + 'test_suite' + sep + 'shared_data' + sep
 # Load the state.
 state.load('bug_20464_mf_missing_ri_data', dir=data_path)
 
+# The results dir.
+ds.tmpdir = mkdtemp()
+
+# Change some opt params.
+dAuvergne_protocol.opt_func_tol = 1e-2
+dAuvergne_protocol.opt_max_iterations = 20
+
 # Do not change!
-dAuvergne_protocol(pipe_name='origin - mf (Thu Jan 31 10:06:25 2013)', pipe_bundle='mf (Thu Jan 31 10:06:25 2013)', diff_model=DIFF_MODEL, mf_models=MF_MODELS, local_tm_models=LOCAL_TM_MODELS, grid_inc=GRID_INC, min_algor=MIN_ALGOR, mc_sim_num=MC_NUM, conv_loop=CONV_LOOP)
+dAuvergne_protocol(pipe_name='origin - mf (Thu Jan 31 10:06:25 2013)', pipe_bundle='mf (Thu Jan 31 10:06:25 2013)', results_dir=ds.tmpdir, diff_model=DIFF_MODEL, mf_models=MF_MODELS, local_tm_models=LOCAL_TM_MODELS, grid_inc=GRID_INC, min_algor=MIN_ALGOR, mc_sim_num=MC_NUM, conv_loop=CONV_LOOP)
