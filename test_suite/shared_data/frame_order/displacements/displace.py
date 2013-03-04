1a76de9df6b341e4783fac95a9542f2a460a7495
Edward d'Auvergne <edward@nmr-relax.com>
2013-03-04 14:52:58 +0000

    The displacement script now prints out the inverted Euler angles (the solution to the problem).

--- test_suite/shared_data/frame_order/displacements/displace.py
+++ test_suite/shared_data/frame_order/displacements/displace.py
@@ -1,10 +1,10 @@
 # Rotate and translate the pseudo-molecule.
 
 # Python module imports.
-from numpy import array, float64, zeros
+from numpy import array, float64, transpose, zeros
 
 # relax module imports.
-from maths_fns.rotation_matrix import euler_to_R_zyz
+from maths_fns.rotation_matrix import euler_to_R_zyz, R_to_euler_zyz
 
 
 # Create a data pipe for the data.
@@ -25,3 +25,9 @@ structure.translate(T=T)
 
 # Write out the new structure.
 structure.write_pdb('displaced.pdb', force=True)
+
+# Printout of the inverted Euler angles of rotation (the solution).
+a, b, g = R_to_euler_zyz(transpose(R))
+print("alpha: %s" % a)
+print("beta:  %s" % b)
+print("gamma: %s" % g)
