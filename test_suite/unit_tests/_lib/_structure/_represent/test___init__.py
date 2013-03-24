2e24d8233dfbdcd8b6bfdf0756429df53a439742
Edward d'Auvergne <edward@nmr-relax.com>
2013-03-24 15:56:29 +0000

    Created the empty lib.structure.represent package.
    This will be used to hold modules which generate 3D structures as geometric representations of
    abstract ideas such as tensors, cones, frames, etc.

---
+++ test_suite/unit_tests/_lib/_structure/_represent/test___init__.py
@@ -0,0 +1,39 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009-2013 Edward d'Auvergne                                   #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from os import sep
+import sys
+
+# relax module imports.
+from test_suite.unit_tests.package_checking import PackageTestCase
+import lib.structure.represent
+
+
+class Test___init__(PackageTestCase):
+    """Unit tests for the lib.structure.represent package."""
+
+    def setUp(self):
+        """Set up for the package checking."""
+
+        self.package = lib.structure.represent
+        self.package_name = 'lib.structure.represent'
+        self.package_path = sys.path[0] + sep + 'lib' + sep + 'structure' + sep + 'represent'
