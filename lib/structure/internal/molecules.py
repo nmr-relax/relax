1707a8f26c94c275c1fe9a6226c1591d95c08ddc
Edward d'Auvergne <edward@nmr-relax.com>
2013-03-24 15:48:54 +0000

    Shifted the MolContainer class from pipe_control.structure.internal into lib.structure.internal.molecules.
    This is in preparation for shifting the internal structural object to lib.structure.internal and for
    the elimination of the unused and no longer useful ScientificPython structural object.

--- lib/structure/internal/molecules.py
+++ lib/structure/internal/molecules.py
@@ -26,6 +26,430 @@
 from lib.errors import RelaxError, RelaxFromXMLNotEmptyError
 
 
+class MolContainer:
+    """The container for the molecular information.
+
+    The structural data object for this class is a container possessing a number of different arrays
+    corresponding to different structural information.  These objects include:
+
+        - atom_num:  The atom name.
+        - atom_name:  The atom name.
+        - bonded:  Each element an array of bonded atom indices.
+        - chain_id:  The chain ID.
+        - element:  The element symbol.
+        - pdb_record:  The optional PDB record name (one of ATOM, HETATM, or TER).
+        - res_name:  The residue name.
+        - res_num:  The residue number.
+        - seg_id:  The segment ID.
+        - x:  The x coordinate of the atom.
+        - y:  The y coordinate of the atom.
+        - z:  The z coordinate of the atom.
+
+    All arrays should be of equal length so that an atom index can retrieve all the corresponding
+    data.  Only the atom identification string is compulsory, all other arrays can contain None.
+    """
+
+
+    def __init__(self):
+        """Initialise the molecular container."""
+
+        # The atom num (array of int).
+        self.atom_num = []
+
+        # The atom name (array of str).
+        self.atom_name = []
+
+        # The bonded atom indices (array of arrays of int).
+        self.bonded = []
+
+        # The chain ID (array of str).
+        self.chain_id = []
+
+        # The element symbol (array of str).
+        self.element = []
+
+        # The optional PDB record name (array of str).
+        self.pdb_record = []
+
+        # The residue name (array of str).
+        self.res_name = []
+
+        # The residue number (array of int).
+        self.res_num = []
+
+        # The segment ID (array of int).
+        self.seg_id = []
+
+        # The x coordinate (array of float).
+        self.x = []
+
+        # The y coordinate (array of float).
+        self.y = []
+
+        # The z coordinate (array of float).
+        self.z = []
+
+
+    def _atom_index(self, atom_num):
+        """Find the atom index corresponding to the given atom number.
+
+        @param atom_num:        The atom number to find the index of.
+        @type atom_num:         int
+        @return:                The atom index corresponding to the atom.
+        @rtype:                 int
+        """
+
+        # Loop over the atoms.
+        for j in range(len(self.atom_num)):
+            # Return the index.
+            if self.atom_num[j] == atom_num:
+                return j
+
+        # Should not be here, the PDB connect records are incorrect.
+        warn(RelaxWarning("The atom number " + repr(atom_num) + " from the CONECT record cannot be found within the ATOM and HETATM records."))
+
+
+    def _det_pdb_element(self, atom_name):
+        """Try to determine the element from the PDB atom name.
+
+        @param atom_name:   The PDB atom name.
+        @type atom_name:    str
+        @return:            The element name, or None if unsuccessful.
+        @rtype:             str or None
+        """
+
+        # Strip away the "'" character (for RNA, etc.).
+        element = atom_name.strip("'")
+
+        # Strip away atom numbering, from the front and end.
+        element = element.strip(digits)
+
+        # Amino acid atom translation table (note, numbers have been stripped already!).
+        table = {'C': ['CA', 'CB', 'CG', 'CD', 'CE', 'CH', 'CZ'],
+                 'N': ['ND', 'NE', 'NH', 'NZ'],
+                 'H': ['HA', 'HB', 'HG', 'HD', 'HE', 'HH', 'HT', 'HZ'],
+                 'O': ['OG', 'OD', 'OE', 'OH', 'OT'],
+                 'S': ['SD', 'SG']
+        }
+
+        # Translate amino acids.
+        for key in list(table.keys()):
+            if element in table[key]:
+                element = key
+                break
+
+        # Allowed element list.
+        elements = ['H', 'C', 'N', 'O', 'F', 'P', 'S']
+
+        # Return the element, if in the list.
+        if element in elements:
+            return element
+
+        # Else, throw a warning.
+        warn(RelaxWarning("Cannot determine the element associated with atom '%s'." % atom_name))
+
+
+    def _parse_xyz_record(self, record):
+        """Parse the XYZ record string and return an array of the corresponding atomic information.
+
+        The format of the XYZ records is::
+         __________________________________________________________________________________________
+         |         |              |              |                                                |
+         | Columns | Data type    | Field        | Definition                                     |
+         |_________|______________|______________|________________________________________________|
+         |         |              |              |                                                |
+         |  1      | String       | element      |                                                |
+         |  2      | Real         | x            | Orthogonal coordinates for X in Angstroms      |
+         |  3      | Real         | y            | Orthogonal coordinates for Y in Angstroms      |
+         |  4      | Real         | z            | Orthogonal coordinates for Z in Angstroms      |
+         |_________|______________|______________|________________________________________________|
+
+
+        @param record:  The single line PDB record.
+        @type record:   str
+        @return:        The list of atomic information
+        @rtype:         list of str
+        """
+
+        # Initialise.
+        fields = []
+        word = record.split()
+
+        # ATOM and HETATM records.
+        if len(word)==4:
+            # Split up the record.
+            fields.append(word[0])
+            fields.append(word[1])
+            fields.append(word[2])
+            fields.append(word[3])
+
+            # Loop over the fields.
+            for i in range(len(fields)):
+                # Strip all whitespace.
+                fields[i] = fields[i].strip()
+
+                # Replace nothingness with None.
+                if fields[i] == '':
+                    fields[i] = None
+
+            # Convert strings to numbers.
+            if fields[1]:
+                fields[1] = float(fields[1])
+            if fields[2]:
+                fields[2] = float(fields[2])
+            if fields[3]:
+                fields[3] = float(fields[3])
+
+        # Return the atomic info.
+        return fields
+
+
+    def atom_add(self, atom_name=None, res_name=None, res_num=None, pos=[None, None, None], element=None, atom_num=None, chain_id=None, segment_id=None, pdb_record=None):
+        """Method for adding an atom to the structural data object.
+
+        This method will create the key-value pair for the given atom.
+
+
+        @keyword atom_name:     The atom name, e.g. 'H1'.
+        @type atom_name:        str or None
+        @keyword res_name:      The residue name.
+        @type res_name:         str or None
+        @keyword res_num:       The residue number.
+        @type res_num:          int or None
+        @keyword pos:           The position vector of coordinates.
+        @type pos:              list (length = 3)
+        @keyword element:       The element symbol.
+        @type element:          str or None
+        @keyword atom_num:      The atom number.
+        @type atom_num:         int or None
+        @keyword chain_id:      The chain identifier.
+        @type chain_id:         str or None
+        @keyword segment_id:    The segment identifier.
+        @type segment_id:       str or None
+        @keyword pdb_record:    The optional PDB record name, e.g. 'ATOM' or 'HETATM'.
+        @type pdb_record:       str or None
+        @return:                The index of the added atom.
+        @rtype:                 int
+        """
+
+        # Append to all the arrays.
+        self.atom_num.append(atom_num)
+        self.atom_name.append(atom_name)
+        self.bonded.append([])
+        self.chain_id.append(chain_id)
+        self.element.append(element)
+        self.pdb_record.append(pdb_record)
+        self.res_name.append(res_name)
+        self.res_num.append(res_num)
+        self.seg_id.append(segment_id)
+        self.x.append(pos[0])
+        self.y.append(pos[1])
+        self.z.append(pos[2])
+
+        # Return the index.
+        return len(self.atom_num) - 1
+
+
+    def atom_connect(self, index1=None, index2=None):
+        """Method for connecting two atoms within the data structure object.
+
+        This method will append index2 to the array at bonded[index1] and vice versa.
+
+
+        @keyword index1:        The index of the first atom.
+        @type index1:           int
+        @keyword index2:        The index of the second atom.
+        @type index2:           int
+        """
+
+        # Update the bonded array structure, if necessary.
+        if index2 not in self.bonded[index1]:
+            self.bonded[index1].append(index2)
+        if index1 not in self.bonded[index2]:
+            self.bonded[index2].append(index1)
+
+
+    def fill_object_from_pdb(self, records, alt_loc_select=None):
+        """Method for generating a complete Structure_container object from the given PDB records.
+
+        @param records:             A list of structural PDB records.
+        @type records:              list of str
+        @keyword alt_loc_select:    The PDB ATOM record 'Alternate location indicator' field value to select which coordinates to use.
+        @type alt_loc_select:       str or None
+        """
+
+        # Loop over the records.
+        for record in records:
+            # Nothing to do.
+            if not record or record == '\n':
+                continue
+
+            # Add the atom.
+            if record[:4] == 'ATOM' or record[:6] == 'HETATM':
+                # Parse the record.
+                if record[:4] == 'ATOM':
+                    record_type, serial, name, alt_loc, res_name, chain_id, res_seq, icode, x, y, z, occupancy, temp_factor, element, charge = pdb_read.atom(record)
+                if record[:6] == 'HETATM':
+                    record_type, serial, name, alt_loc, res_name, chain_id, res_seq, icode, x, y, z, occupancy, temp_factor, element, charge = pdb_read.hetatm(record)
+
+                # Handle the alternate locations.
+                if alt_loc != None:
+                    # Don't know what to do.
+                    if alt_loc_select == None:
+                        raise RelaxError("Multiple alternate location indicators are present in the PDB file, but the desired coordinate set has not been specified.")
+
+                    # Skip non-matching locations.
+                    if alt_loc != alt_loc_select:
+                        continue
+
+                # Attempt at determining the element, if missing.
+                if not element:
+                    element = self._det_pdb_element(name)
+
+                # Add.
+                self.atom_add(pdb_record=record_type, atom_num=serial, atom_name=name, res_name=res_name, chain_id=chain_id, res_num=res_seq, pos=[x, y, z], element=element)
+
+            # Connect atoms.
+            if record[:6] == 'CONECT':
+                # Parse the record.
+                record_type, serial, bonded1, bonded2, bonded3, bonded4 = pdb_read.conect(record)
+
+                # Loop over the atoms of the record.
+                for bonded in [bonded1, bonded2, bonded3, bonded4]:
+                    # Skip if there is no record.
+                    if not bonded:
+                        continue
+
+                    # Skip broken CONECT records (for when the record points to a non-existent atom).
+                    if self._atom_index(serial) == None or self._atom_index(bonded) == None:
+                        continue
+
+                    # Make the connection.
+                    self.atom_connect(index1=self._atom_index(serial), index2=self._atom_index(bonded))
+
+
+    def fill_object_from_xyz(self, records):
+        """Method for generating a complete Structure_container object from the given xyz records.
+
+        @param records:         A list of structural xyz records.
+        @type records:          list of str
+        """
+
+        # initialisation for atom number
+        atom_number = 1
+
+        # Loop over the records.
+        for record in records:
+            # Parse the record.
+            record = self._parse_xyz_record(record)
+
+            # Nothing to do.
+            if not record:
+                continue
+
+            # Add the atom.
+            if len(record) == 4:
+                # Add.
+                self.atom_add(atom_name=record[0], atom_num=atom_number, pos=[record[1], record[2], record[3]], element=record[0])
+
+                # Increment of atom number
+                atom_number = atom_number + 1
+
+
+    def from_xml(self, mol_node, file_version=1):
+        """Recreate the MolContainer from the XML molecule node.
+
+        @param mol_node:        The molecule XML node.
+        @type mol_node:         xml.dom.minicompat.NodeList instance
+        @keyword file_version:  The relax XML version of the XML file.
+        @type file_version:     int
+        """
+
+        # Recreate the current molecule container.
+        xml_to_object(mol_node, self, file_version=file_version)
+
+
+    def is_empty(self):
+        """Check if the container is empty."""
+
+        # Set attributes.
+        if hasattr(self, 'mol_name'): return False
+        if hasattr(self, 'file_name'): return False
+        if hasattr(self, 'file_path'): return False
+        if hasattr(self, 'file_mol_num'): return False
+        if hasattr(self, 'file_model'): return False
+
+        # Internal data structures.
+        if not self.atom_num == []: return False
+        if not self.atom_name == []: return False
+        if not self.bonded == []: return False
+        if not self.chain_id == []: return False
+        if not self.element == []: return False
+        if not self.pdb_record == []: return False
+        if not self.res_name == []: return False
+        if not self.res_num == []: return False
+        if not self.seg_id == []: return False
+        if not self.x == []: return False
+        if not self.y == []: return False
+        if not self.z == []: return False
+
+        # Ok, now this thing must be empty.
+        return True
+
+
+    def last_residue(self):
+        """Return the number of the last residue.
+
+        @return:    The last residue number.
+        @rtype:     int
+        """
+
+        # Return the number.
+        return self.res_num[-1]
+
+
+    def merge(self, mol_cont=None):
+        """Merge the contents of the given molecule container into here.
+
+        @keyword mol_cont:      The data structure for the molecule to merge.
+        @type mol_cont:         MolContainer instance
+        """
+
+        # The current index.
+        curr_index = len(self.atom_num)
+
+        # Loop over all data.
+        for i in range(len(mol_cont.atom_num)):
+            # Add the atom.
+            self.atom_add(atom_num=curr_index+i+1, atom_name=mol_cont.atom_name[i], res_name=mol_cont.res_name[i], res_num=mol_cont.res_num[i], pos=[mol_cont.x[i], mol_cont.y[i], mol_cont.z[i]], element=mol_cont.element[i], chain_id=mol_cont.chain_id[i], pdb_record=mol_cont.pdb_record[i])
+
+            # Connect the atoms.
+            for j in range(len(mol_cont.bonded[i])):
+                self.atom_connect(index1=i+curr_index+1, index2=mol_cont.bonded[i][j]+curr_index+1)
+
+
+    def to_xml(self, doc, element):
+        """Create XML elements for the contents of this molecule container.
+
+        @param doc:     The XML document object.
+        @type doc:      xml.dom.minidom.Document instance
+        @param element: The element to add the molecule XML elements to.
+        @type element:  XML element object
+        """
+
+        # Create an XML element for this molecule and add it to the higher level element.
+        mol_element = doc.createElement('mol_cont')
+        element.appendChild(mol_element)
+
+        # Set the molecule attributes.
+        mol_element.setAttribute('desc', 'Molecule container')
+        mol_element.setAttribute('name', str(self.mol_name))
+
+        # Add all simple python objects within the MolContainer to the XML element.
+        fill_object_contents(doc, mol_element, object=self, blacklist=list(self.__class__.__dict__.keys()))
+
+
+
 class MolList(list):
     """List type data container for holding the different molecules of one model."""
 
@@ -112,12 +536,6 @@ class MolList(list):
 
         # Loop over the molecules.
         for mol_node in mol_nodes:
-            # Some imports (here to break circular import issues).
-            if id == 'internal':
-                from pipe_control.structure.internal import MolContainer
-            elif id == 'scientific':
-                from pipe_control.structure.scientific import MolContainer
-
             # Initialise a MolContainer instance.
             mol_cont = MolContainer()
 
