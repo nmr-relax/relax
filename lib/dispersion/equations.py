a578259d0c0a9cfdfb49cb6c9a15b7b0b72230f8
Edward d'Auvergne <edward@nmr-relax.com>
2013-03-27 16:49:33 +0000

    Created the lib.dispersion.equations module.
    This is a translation of Sebastien Morin's C code in the old relax_disp branch.  The old C files can
    be checked out with the command:
    
    svn co http://svn.gna.org/svn/relax/branches/relax_disp/maths_fns/dispersion.c@r18123
    svn co http://svn.gna.org/svn/relax/branches/relax_disp/maths_fns/dispersion.h@r18123

--- lib/dispersion/equations.py
+++ lib/dispersion/equations.py
@@ -1,5 +1,6 @@
 ###############################################################################
 #                                                                             #
+# Copyright (C) 2009 Sebastien Morin                                          #
 # Copyright (C) 2013 Edward d'Auvergne                                        #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
@@ -19,9 +20,26 @@
 #                                                                             #
 ###############################################################################
 
-# Package docstring.
-"""The relax-lib NMR package - a library of functions for relaxation dispersion."""
+# Module docstring.
+"""The relaxation dispersion equations."""
 
-__all__ = [
-    'equations'
-]
+# Python module imports.
+from math import tanh
+
+
+def fast_2site(params=None, cpmg_frqs=None, back_calc=None, num_times=None):
+    """Back calculate the effective transversal relaxation rate (R2eff).
+
+    The currently supported equation is that for CPMG relaxation dispersion in the fast exchange limit:
+
+        - Millet et al., JACS, 2000, 122, 2867-2877 (equation 19)
+        - Kovrigin et al., J. Mag. Res., 2006, 180, 93-104 (equation 1)
+
+    In the future, back-calculation should be available for CPMG relaxation dispersion in the slow exchange limit:
+            - Tollinger et al., JACS, 2001, 123, 11341-11352 (equation 2)
+    """
+
+    # Loop over the time points.
+    for i in range(num_times):
+        # Back calculate.
+        back_calc[i] = params[0] + params[1] * (1 - 2 * tanh(params[2] / (2 * 4 * cpmg_frqs[i])) * ((4 * cpmg_frqs[i] ) / params[2]))
