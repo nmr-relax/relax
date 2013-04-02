ad755bede56663cb0918b9b6e583297465dd5dd8
Edward d'Auvergne <edward@nmr-relax.com>
2013-04-02 09:39:55 +0000

    Converted the bug_20674_jw_mapping.py system test script to use the self._execute_uf() interface.
    This allows the script to be used in the GUI.

--- test_suite/system_tests/scripts/bug_20674_jw_mapping.py
+++ test_suite/system_tests/scripts/bug_20674_jw_mapping.py
@@ -8,50 +8,50 @@ import sys
 from status import Status; status = Status()
 
 
-# Create the run.
-pipe.create('jw_mapping', 'jw')
+# Create the data pipe.
+self._execute_uf(uf_name='pipe.create', pipe_name='jw_mapping', pipe_type='jw')
 
 # The data directory.
 path = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'consistency_testing'+sep+'bug_20674_ct_analysis_failure'
 
 # Load the PDB file.
-structure.read_pdb(file='2QFK_MONOMERHabc5.pdb', dir=path, read_mol=None, set_mol_name=None, read_model=None, set_model_num=None)
+self._execute_uf(uf_name='structure.read_pdb', file='2QFK_MONOMERHabc5.pdb', dir=path, read_mol=None, set_mol_name=None, read_model=None, set_model_num=None)
 
 # Set up the 15N and 1H spins (both backbone and Trp indole sidechains).
-structure.load_spins('@N', ave_pos=True)
-structure.load_spins('@H', ave_pos=True)
-spin.isotope('15N', spin_id='@N')
-spin.isotope('1H', spin_id='@H')
+self._execute_uf(uf_name='structure.load_spins', spin_id='@N', ave_pos=True)
+self._execute_uf(uf_name='structure.load_spins', spin_id='@H', ave_pos=True)
+self._execute_uf(uf_name='spin.isotope', isotope='15N', spin_id='@N')
+self._execute_uf(uf_name='spin.isotope', isotope='1H', spin_id='@H')
 
 # Load the relaxation data.
-bruker.read(ri_id='r1_700', file='T1 dhp 700.txt', dir=path)
-bruker.read(ri_id='r2_700', file='T2 dhp 700.txt', dir=path)
-bruker.read(ri_id='noe_700', file='NOE dhp 700.txt', dir=path)
+self._execute_uf(uf_name='bruker.read', ri_id='r1_700', file='T1 dhp 700.txt', dir=path)
+self._execute_uf(uf_name='bruker.read', ri_id='r2_700', file='T2 dhp 700.txt', dir=path)
+self._execute_uf(uf_name='bruker.read', ri_id='noe_700', file='NOE dhp 700.txt', dir=path)
 
 # Define the magnetic dipole-dipole relaxation interaction.
-dipole_pair.define(spin_id1='@N', spin_id2='@H', direct_bond=True)
-dipole_pair.set_dist(spin_id1='@N', spin_id2='@H', ave_dist=1.02 * 1e-10)
+self._execute_uf(uf_name='dipole_pair.define', spin_id1='@N', spin_id2='@H', direct_bond=True)
+self._execute_uf(uf_name='dipole_pair.set_dist', spin_id1='@N', spin_id2='@H', ave_dist=1.02 * 1e-10)
 
 # Define the chemical shift relaxation interaction.
-value.set(val=-172 * 1e-6, param='csa')
+self._execute_uf(uf_name='value.set', val=-172 * 1e-6, param='csa')
 
 # Set the frequency.
-jw_mapping.set_frq(frq=700.17 * 1e6)
+self._execute_uf(uf_name='jw_mapping.set_frq', frq=700.17 * 1e6)
 
 # Consistency tests.
-calc()
+self._execute_uf(uf_name='calc')
 
 # Monte Carlo simulations.
-monte_carlo.setup(number=10)
-monte_carlo.create_data()
-calc()
-monte_carlo.error_analysis()
+self._execute_uf(uf_name='monte_carlo.setup', number=10)
+self._execute_uf(uf_name='monte_carlo.create_data')
+self._execute_uf(uf_name='calc')
+self._execute_uf(uf_name='monte_carlo.error_analysis')
 
 # Create grace files.
-grace.write(y_data_type='j0', file='devnull', force=True)
-grace.write(y_data_type='f_eta', file='devnull', force=True)
-grace.write(y_data_type='f_r2', file='devnull', force=True)
+self._execute_uf(uf_name='grace.write', y_data_type='j0', file='devnull', force=True)
+self._execute_uf(uf_name='grace.write', y_data_type='f_eta', file='devnull', force=True)
+self._execute_uf(uf_name='grace.write', y_data_type='f_r2', file='devnull', force=True)
 
 # Finish.
-results.write(file='devnull', force=True)
-state.save('devnull')
+self._execute_uf(uf_name='results.write', file='devnull', force=True)
+self._execute_uf(uf_name='state.save', file='devnull')
