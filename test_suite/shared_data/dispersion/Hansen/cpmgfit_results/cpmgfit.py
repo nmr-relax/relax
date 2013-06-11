b8003b3aac8e9e227fd0545e95d8bdf8d3e59bb3
Edward d'Auvergne <edward@nmr-relax.com>
2013-06-11 13:07:34 +0000

    Fix for the cpmgfit script for an edit accidentally committed at r20033.

--- test_suite/shared_data/dispersion/Hansen/cpmgfit_results/cpmgfit.py
+++ test_suite/shared_data/dispersion/Hansen/cpmgfit_results/cpmgfit.py
@@ -17,7 +17,7 @@ data_path = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'disper
 state.load(data_path+sep+'r2eff_values')
 
 # Loop over the models.
-for model in ['IT99']:
+for model in ['LM63', 'CR72', 'IT99']:
     # Set up the model.
     relax_disp.select_model(model)
 
