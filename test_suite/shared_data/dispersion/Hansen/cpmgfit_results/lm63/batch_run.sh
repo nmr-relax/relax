d27e0fd8f5d710d91d432f5d8a505215e1e222cb
Edward d'Auvergne <edward@nmr-relax.com>
2013-06-11 13:06:40 +0000

    Updated the CPMGFit results for the IT99 grid search fixes of the last commit.

--- test_suite/shared_data/dispersion/Hansen/cpmgfit_results/lm63/batch_run.sh
+++ test_suite/shared_data/dispersion/Hansen/cpmgfit_results/lm63/batch_run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-cpmgfit -xmgr -f spin_:70@N.in
-cpmgfit -xmgr -f spin_:71@N.in
+cpmgfit -grid -xmgr -f spin_:70@N.in | tee spin_:70@N.out
+cpmgfit -grid -xmgr -f spin_:71@N.in | tee spin_:71@N.out
