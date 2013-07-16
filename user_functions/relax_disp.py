243a35e39ab2ec4ef54b5bb263c120c8943a1cae
Edward d'Auvergne <edward@nmr-relax.com>
2013-07-16 07:58:48 +0000

    Added the 'CR72 red' model to the relax_disp.select_model user function frontend.
    This is the Carver and Richards 1972 analytic model with the simplification R20A = R20B.  The
    current 'CR72' makes the same assumption, but that model will be expanded to support R20A and R20B
    later.
    
    This commit follows step 2 of the relaxation dispersion model addition tutorial
    (http://thread.gmane.org/gmane.science.nmr.relax.devel/3907).

--- user_functions/relax_disp.py
+++ user_functions/relax_disp.py
@@ -39,7 +39,7 @@ from specific_analyses.relax_disp.cpmgfit import cpmgfit_execute, cpmgfit_input
 from specific_analyses.relax_disp.disp_data import cpmg_frq, plot_disp_curves, plot_exp_curves, relax_time, spin_lock_field
 from specific_analyses.relax_disp.nessy import nessy_input
 from specific_analyses.relax_disp.sherekhan import sherekhan_input
-from specific_analyses.relax_disp.variables import MODEL_CR72, MODEL_DPL94, MODEL_IT99, MODEL_LM63, MODEL_M61, MODEL_M61B, MODEL_NOREX, MODEL_R2EFF, MODEL_NS_2SITE_STAR, MODEL_NS_2SITE_STAR_RED
+from specific_analyses.relax_disp.variables import MODEL_CR72, MODEL_CR72_RED, MODEL_DPL94, MODEL_IT99, MODEL_LM63, MODEL_M61, MODEL_M61B, MODEL_NOREX, MODEL_R2EFF, MODEL_NS_2SITE_STAR, MODEL_NS_2SITE_STAR_RED
 from specific_analyses.setup import relax_disp_obj
 from user_functions.data import Uf_info; uf_info = Uf_info()
 from user_functions.objects import Desc_container
@@ -440,6 +440,7 @@ uf.add_keyarg(
         "%s: {R2eff/R1rho, I0}" % MODEL_R2EFF,
         "%s: {R20, ...}" % MODEL_NOREX,
         "%s: {R20, ..., phi_ex, kex}" % MODEL_LM63,
+        "%s: {R20, ..., pA, dw, kex}" % MODEL_CR72_RED,
         "%s: {R20, ..., pA, dw, kex}" % MODEL_CR72,
         "%s: {R20, ..., phi_ex, padw2, kex}" % MODEL_IT99,
         "%s: {R20, ..., pA, dw, kex}" % MODEL_NS_2SITE_STAR_RED,
@@ -452,6 +453,7 @@ uf.add_keyarg(
         MODEL_R2EFF,
         MODEL_NOREX,
         MODEL_LM63,
+        MODEL_CR72_RED,
         MODEL_CR72,
         MODEL_IT99,
         MODEL_NS_2SITE_STAR_RED,
@@ -471,6 +473,7 @@ uf.desc[-1].add_paragraph("The analytic models are closed solutions to the Bloch
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_R2EFF, "This is the model used to determine the R2eff values and errors required as the base data for all other models,")
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_NOREX, "This is the model for no chemical exchange being present,")
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_LM63, "The original Luz and Meiboom (1963) 2-site fast exchange equation with parameters {R20, ..., phi_ex, kex},")
+uf.desc[-1].add_item_list_element("'%s'" % MODEL_CR72_RED, "The Carver and Richards (1972) 2-site equation for all time scales whereby the simplification R20A = R20B is assumed.  The parameters are {R20, ..., pA, dw, kex}.")
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_CR72, "The Carver and Richards (1972) 2-site equation for all time scales with parameters {R20, ..., pA, dw, kex}.")
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_IT99, "The Ishima and Torchia (1999) 2-site model for all time scales with pA >> pB and with parameters {R20, ..., phi_ex, padw2, kex}.")
 uf.desc[-1].add_paragraph("For the R1rho-type experiment, the currently supported models are:")
