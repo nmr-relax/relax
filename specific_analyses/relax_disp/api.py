c4fd87b29bbfb553b8253cce7bc5ca5dcee4c82a
Edward d'Auvergne <edward@nmr-relax.com>
2013-07-17 15:17:02 +0000

    Added support for the 'NS 2-site expanded' model to the relax_disp.select_model user function back end.
    This is the numerical model for the 2-site Bloch-McConnell equations expanded using Maple by Nikolai
    Skrynnikov.  It originates as optimization function number 5 from the fitting_main_kex.py script
    from Mathilde Lescanne, Paul Schanda, and Dominique Marion (see
    http://thread.gmane.org/gmane.science.nmr.relax.devel/4138,
    https://gna.org/task/?7712#comment2 and
    https://gna.org/support/download.php?file_id=18262).
    
    This commit follows step 6 of the relaxation dispersion model addition tutorial
    (http://thread.gmane.org/gmane.science.nmr.relax.devel/3907).

--- specific_analyses/relax_disp/api.py
+++ specific_analyses/relax_disp/api.py
@@ -48,7 +48,7 @@ from specific_analyses.api_base import API_base
 from specific_analyses.api_common import API_common
 from specific_analyses.relax_disp.disp_data import average_intensity, find_intensity_keys, loop_cluster, loop_frq, loop_frq_point, loop_frq_point_key, loop_frq_point_time, loop_point, loop_time, relax_time, return_cpmg_frqs, return_index_from_disp_point, return_index_from_frq, return_key_from_disp_point_index, return_param_key_from_data, return_r2eff_arrays, return_spin_lock_nu1, return_value_from_frq_index, spin_ids_to_containers
 from specific_analyses.relax_disp.parameters import assemble_param_vector, assemble_scaling_matrix, disassemble_param_vector, linear_constraints, loop_parameters, param_conversion, param_index_to_param_info, param_num
-from specific_analyses.relax_disp.variables import CPMG_EXP, FIXED_TIME_EXP, MODEL_LIST_FULL, MODEL_LM63, MODEL_CR72, MODEL_CR72_RED, MODEL_DPL94, MODEL_IT99, MODEL_M61, MODEL_M61B, MODEL_NOREX, MODEL_NS_2SITE_3D, MODEL_NS_2SITE_3D_RED, MODEL_NS_2SITE_STAR, MODEL_NS_2SITE_STAR_RED, MODEL_R2EFF, R1RHO_EXP, VAR_TIME_EXP
+from specific_analyses.relax_disp.variables import CPMG_EXP, FIXED_TIME_EXP, MODEL_LIST_FULL, MODEL_LM63, MODEL_CR72, MODEL_CR72_RED, MODEL_DPL94, MODEL_IT99, MODEL_M61, MODEL_M61B, MODEL_NOREX, MODEL_NS_2SITE_3D, MODEL_NS_2SITE_3D_RED, MODEL_NS_2SITE_EXPANDED, MODEL_NS_2SITE_STAR, MODEL_NS_2SITE_STAR_RED, MODEL_R2EFF, R1RHO_EXP, VAR_TIME_EXP
 from target_functions.relax_disp import Dispersion
 from user_functions.data import Uf_tables; uf_tables = Uf_tables()
 from user_functions.objects import Desc_container
@@ -716,6 +716,14 @@ class Relax_disp(API_base, API_common):
         # NS 2-site 3D red model.
         elif model == MODEL_NS_2SITE_3D_RED:
             print("The numerical solution for the 2-site Bloch-McConnell equations using 3D magnetisation vectors, whereby the simplification R20A = R20B is assumed.")
+            params = []
+            for frq in loop_frq():
+                params.append('r2')
+            params += ['pA', 'dw', 'kex']
+
+        # NS 2-site expanded model.
+        elif model == MODEL_NS_2SITE_EXPANDED:
+            print("The numerical solution for the 2-site Bloch-McConnell equations expanded using Maple by Nikolai Skrynnikov.")
             params = []
             for frq in loop_frq():
                 params.append('r2')
