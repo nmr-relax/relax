119768fe48b700adae8a05ae336bb49b0883c67b
Edward d'Auvergne <edward@nmr-relax.com>
2013-10-15 16:43:02 +0000

    Added the 'MQ CR72' model to the relax_disp.select_model user function frontend.
    This is the Carver and Richards (1972) 2-site model expanded for MQ CPMG data by Korzhnev et al.,
    2004.
    
    This follows the tutorial for adding relaxation dispersion models at:
    http://wiki.nmr-relax.com/Tutorial_for_adding_relaxation_dispersion_models_to_relax#The_relax_disp.select_model_user_function_front_end

--- user_functions/relax_disp.py
+++ user_functions/relax_disp.py
@@ -42,7 +42,7 @@ from specific_analyses.relax_disp.disp_data import cpmg_frq, exp_type, plot_disp
 from specific_analyses.relax_disp.nessy import nessy_input
 from specific_analyses.relax_disp.parameters import copy
 from specific_analyses.relax_disp.sherekhan import sherekhan_input
-from specific_analyses.relax_disp.variables import EXP_TYPE_CPMG, EXP_TYPE_MQ_CPMG, EXP_TYPE_MQ_R1RHO, EXP_TYPE_R1RHO, MODEL_CR72, MODEL_CR72_FULL, MODEL_DPL94, MODEL_IT99, MODEL_LM63, MODEL_LM63_3SITE, MODEL_M61, MODEL_M61B, MODEL_MQ_NS_CPMG_2SITE, MODEL_NOREX, MODEL_R2EFF, MODEL_NS_CPMG_2SITE_3D, MODEL_NS_CPMG_2SITE_3D_FULL, MODEL_NS_CPMG_2SITE_EXPANDED, MODEL_NS_CPMG_2SITE_STAR, MODEL_NS_CPMG_2SITE_STAR_FULL, MODEL_NS_R1RHO_2SITE, MODEL_TP02, MODEL_TSMFK01
+from specific_analyses.relax_disp.variables import EXP_TYPE_CPMG, EXP_TYPE_MQ_CPMG, EXP_TYPE_MQ_R1RHO, EXP_TYPE_R1RHO, MODEL_CR72, MODEL_CR72_FULL, MODEL_DPL94, MODEL_IT99, MODEL_LM63, MODEL_LM63_3SITE, MODEL_M61, MODEL_M61B, MODEL_MQ_CR72, MODEL_MQ_NS_CPMG_2SITE, MODEL_NOREX, MODEL_R2EFF, MODEL_NS_CPMG_2SITE_3D, MODEL_NS_CPMG_2SITE_3D_FULL, MODEL_NS_CPMG_2SITE_EXPANDED, MODEL_NS_CPMG_2SITE_STAR, MODEL_NS_CPMG_2SITE_STAR_FULL, MODEL_NS_R1RHO_2SITE, MODEL_TP02, MODEL_TSMFK01
 from specific_analyses.setup import relax_disp_obj
 from user_functions.data import Uf_info; uf_info = Uf_info()
 from user_functions.objects import Desc_container
@@ -549,6 +549,7 @@ uf.add_keyarg(
         "%s: {%s, ..., %s, %s}" % (MODEL_DPL94, r1rho_prime, phi_ex, kex),
         "%s: {%s, ..., pA, %s, %s}" % (MODEL_TP02, r1rho_prime, dw, kex),
         "%s: {%s, ..., pA, %s, %s}" % (MODEL_NS_R1RHO_2SITE, r1rho_prime, dw, kex),
+        "%s: {%s, ..., pA, %s, %s, %s}" % (MODEL_MQ_CR72, r2, dw, dwH, kex),
         "%s: {%s, ..., pA, %s, %s, %s}" % (MODEL_MQ_NS_CPMG_2SITE, r2, dw, dwH, kex)
     ],
     wiz_combo_data = [
@@ -570,6 +571,7 @@ uf.add_keyarg(
         MODEL_DPL94,
         MODEL_TP02,
         MODEL_NS_R1RHO_2SITE,
+        MODEL_MQ_CR72,
         MODEL_MQ_NS_CPMG_2SITE
     ],
     wiz_read_only = True
@@ -606,6 +608,7 @@ uf.desc[-1].add_item_list_element("'%s'" % MODEL_NS_R1RHO_2SITE, "The numerical 
 # MQ CPMG-type data.
 uf.desc.append(Desc_container('The MQ CPMG-type experiments'))
 uf.desc[-1].add_paragraph("The currently supported models are:")
+uf.desc[-1].add_item_list_element("'%s'" % MODEL_MQ_CR72, "The the Carver and Richards (1972) 2-site model for all time scales expanded for MQ CPMG data by Korzhnev et al., 2004, whereby the simplification R20A = R20B is assumed.  Its parameters are {R20, ..., pA, dw, dwH, kex}.")
 uf.desc[-1].add_item_list_element("'%s'" % MODEL_MQ_NS_CPMG_2SITE, "The reduced numerical solution for the 2-site Bloch-McConnell equations using 2D magnetisation vectors whereby the simplification R20A = R20B is assumed.  Its parameters are {R20, ..., pA, dw, dwH, kex}.")
 # MQ R1rho-type data.
 uf.desc.append(Desc_container('The MQ R1rho-type experiments'))
