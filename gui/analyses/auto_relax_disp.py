a67c97d61a8355bfde3377a9e4edd625a7f19d41
Edward d'Auvergne <edward@nmr-relax.com>
2013-10-15 17:51:52 +0000

    Added the 'MQ CR72' model to the GUI model list.
    This is the Carver and Richards (1972) 2-site model expanded for MQ CPMG data by Korzhnev et al.,
    2004.
    
    This follows the tutorial for adding relaxation dispersion models at:
    http://wiki.nmr-relax.com/Tutorial_for_adding_relaxation_dispersion_models_to_relax#The_GUI.

--- gui/analyses/auto_relax_disp.py
+++ gui/analyses/auto_relax_disp.py
@@ -48,7 +48,7 @@ from lib.text.gui import dw, dwH, i0, kex, padw2, phi_ex, phi_exB, phi_exC, r1, 
 from pipe_control.mol_res_spin import exists_mol_res_spin_data, spin_loop
 from pipe_control.pipes import has_bundle, has_pipe
 from specific_analyses.relax_disp.disp_data import has_cpmg_exp_type, has_r1rho_exp_type
-from specific_analyses.relax_disp.variables import MODEL_CR72, MODEL_CR72_FULL, MODEL_DPL94, MODEL_IT99, MODEL_LIST_CPMG, MODEL_LIST_R1RHO, MODEL_LM63, MODEL_LM63_3SITE, MODEL_M61, MODEL_M61B, MODEL_MQ_NS_CPMG_2SITE, MODEL_NOREX, MODEL_NS_CPMG_2SITE_3D, MODEL_NS_CPMG_2SITE_3D_FULL, MODEL_NS_CPMG_2SITE_EXPANDED, MODEL_NS_CPMG_2SITE_STAR, MODEL_NS_CPMG_2SITE_STAR_FULL, MODEL_NS_R1RHO_2SITE, MODEL_R2EFF, MODEL_TP02, MODEL_TSMFK01
+from specific_analyses.relax_disp.variables import MODEL_CR72, MODEL_CR72_FULL, MODEL_DPL94, MODEL_IT99, MODEL_LIST_CPMG, MODEL_LIST_R1RHO, MODEL_LM63, MODEL_LM63_3SITE, MODEL_M61, MODEL_M61B, MODEL_MQ_CR72, MODEL_MQ_NS_CPMG_2SITE, MODEL_NOREX, MODEL_NS_CPMG_2SITE_3D, MODEL_NS_CPMG_2SITE_3D_FULL, MODEL_NS_CPMG_2SITE_EXPANDED, MODEL_NS_CPMG_2SITE_STAR, MODEL_NS_CPMG_2SITE_STAR_FULL, MODEL_NS_R1RHO_2SITE, MODEL_R2EFF, MODEL_TP02, MODEL_TSMFK01
 from status import Status; status = Status()
 
 
@@ -671,6 +671,7 @@ class Disp_model_list(Model_list):
         MODEL_TP02,
         MODEL_NS_R1RHO_2SITE,
         None,
+        MODEL_MQ_CR72,
         MODEL_MQ_NS_CPMG_2SITE
     ]
     params = [
@@ -697,6 +698,7 @@ class Disp_model_list(Model_list):
         "{%s, ..., pA, %s, %s}" % (r1rho_prime, dw, kex),
         None,
         "{%s, ..., pA, %s, %s, %s}" % (r2, dw, dwH, kex),
+        "{%s, ..., pA, %s, %s, %s}" % (r2, dw, dwH, kex)
     ]
     model_desc = [
         "The base model for determining the %s/%s values and errors for all other models." % (r2eff, r1rho),
@@ -721,6 +723,7 @@ class Disp_model_list(Model_list):
         "The Trott and Palmer (2002) 2-site equation for all time scales.",
         "The 2-site numerical solution using 3D magnetisation vectors.",
         None,
+        "The CR72 2-site model extended to MQ CPMG data by Korzhnev et al., 2004.",
         "The 2-site numerical solution of Korzhnev et al. (2004) from multi-quantum CPMG data."
     ]
     tooltip = "The list of all relaxation dispersion models to be optimised as part of the protocol."
