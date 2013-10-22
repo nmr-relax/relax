5a91015dd7a0dd8d54a05d1b4faa502911d4bb22
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2013-10-22 11:09:22 +0000

    Modified settings script for R1rho test dataset.

--- test_suite/shared_data/dispersion/Kjaergaard_et_al_2013/r1rho_3_spectra_settings.py
+++ test_suite/shared_data/dispersion/Kjaergaard_et_al_2013/r1rho_3_spectra_settings.py
@@ -5,12 +5,8 @@ NR_exp = -1
 
 # The lock power to field, has been found in an calibration experiment.
 # lock_powers = [35.0, 39.0, 41.0, 43.0, 46.0, 48.0]
-spin_lock_field_strengths_Hz = {'35': 321.1, '39': 509., '41': 640.8, '43': 806.7, '46': 1139.6, '48': 1434.7}
+spin_lock_field_strengths_Hz = {'35': 431.0, '39': 651.2, '41': 800.5, '43': 984.0, '46': 1341.11, '48': 1648.5}
 ncycs = [0, 4, 10, 14, 20, 40]
-
-# dw(ppm) = dw(rad.s^-1) * 10^6 * 1/(2*pi) * (gyro1H/(gyro15N*spectrometer_freq)) = 2.45E3 * 1E6 / (2 * math.pi) * (26.7522212E7/(-2.7126E7 * 599.8908622E6)) = -6.41 ppm.
-gyro1H = 26.7522212E7
-gyro15N = 2.7126E7
 
 # Load the experiments settings file.
 expfile = open('exp_parameters_sort.txt','r')
@@ -37,7 +33,6 @@ for i in range(len(expfileslines)):
         ss = int(line.split()[6])
         set_sfrq = float(line.split()[7])
         spin_lock_field_strength = spin_lock_field_strengths_Hz[dpwr2slock]
-        #offset_ppm = 
 
         # Calculate spin_lock time
         time_sl = 2*ncyc*trim
@@ -63,18 +58,17 @@ for i in range(len(expfileslines)):
         # Set The spin-lock field strength, nu1, in Hz
         relax_disp.spin_lock_field(spectrum_id=sp_id, field=spin_lock_field_strength)
 
-        omega_rf_ppm_direct = float(deltadof2) * 1E6 * 1/(2 * math.pi) * (1 / ( set_sfrq*1E6 ) )
-        omega_rf_ppm_indirect = float(deltadof2) * 1E6 * 1/(2 * math.pi) * (gyro1H / (gyro15N * set_sfrq*1E6 ) )
-        writefile.write("%s %s \n"%(omega_rf_ppm_direct, omega_rf_ppm_indirect))
+        # Conversion to ppm from Hz.
+        omega_rf_ppm = float(deltadof2) / (set_sfrq * 1E6) * 1E6
+        writefile.write("%s \n"%(omega_rf_ppm))
 
         # Set The spin-lock offset, omega_rf, in ppm.
-        relax_disp.spin_lock_offset(spectrum_id=sp_id, offset=omega_rf_ppm_indirect)
+        relax_disp.spin_lock_offset(spectrum_id=sp_id, offset=omega_rf_ppm)
 
         # Set the relaxation times.
         relax_fit.relax_time(time=time_sl, spectrum_id=sp_id)
 
         # Set the spectrometer frequency.
         spectrometer.frequency(id=sp_id, frq=set_sfrq, units='MHz')
-
 
 writefile.close()
