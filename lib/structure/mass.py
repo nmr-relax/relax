9352573235f416fb86424f02732ad0600d912ee2
Edward d'Auvergne <edward@nmr-relax.com>
2013-11-17 19:39:27 +0000

    Bug fix for the lib.structure.mass.centre_of_mass() function warning when the element is not known.
    This warning was buggy and resulted tracebacks.

--- lib/structure/mass.py
+++ lib/structure/mass.py
@@ -59,7 +59,7 @@ def centre_of_mass(pos=None, elements=None, verbosity=1):
         try:
             mass = return_atomic_mass(elements[i])
         except RelaxError:
-            warn(RelaxWarning("Skipping the atom '%s' as the element '%s' is unknown." % (id, elements[i])))
+            warn(RelaxWarning("Skipping the atom index %s as the element '%s' is unknown." % (i, elements[i])))
 
         # Total mass.
         M = M + mass
