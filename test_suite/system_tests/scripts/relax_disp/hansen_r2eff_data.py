5aceffd34f6f7103d5bb5b665f61fd23ad75e01f
Edward d'Auvergne <edward@nmr-relax.com>
2013-11-24 12:37:12 +0000

    Updates for the dispersion auto-analysis system tests using Flemming Hansen's data.
    The grid search increments have been increased by one to make sure the solution is always found.

--- test_suite/system_tests/scripts/relax_disp/hansen_r2eff_data.py
+++ test_suite/system_tests/scripts/relax_disp/hansen_r2eff_data.py
@@ -26,7 +26,7 @@ if not hasattr(ds, 'numeric_only'):
     ds.numeric_only = False
 
 # The grid search size (the number of increments per dimension).
-GRID_INC = 5
+GRID_INC = 6
 
 # The number of Monte Carlo simulations to be used for error analysis at the end of the analysis.
 MC_NUM = 3
