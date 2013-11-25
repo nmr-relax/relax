0d53c44b43fc3a353973496159762dc7d0c46ff1
Edward d'Auvergne <edward@nmr-relax.com>
2013-11-25 09:56:49 +0000

    The relaxation dispersion target function class can now handle cpmg_frqs arguments of None.
    This is useful for R1rho models.

--- target_functions/relax_disp.py
+++ target_functions/relax_disp.py
@@ -172,7 +172,7 @@ class Dispersion:
             self.num_disp_points.append([])
             for frq_index in range(len(values[exp_type_index][0])):
                 self.num_disp_points[-1].append([])
-                if len(cpmg_frqs[exp_type_index][frq_index]):
+                if cpmg_frqs != None and len(cpmg_frqs[exp_type_index][frq_index]):
                     self.num_disp_points[-1][-1] = len(self.cpmg_frqs[exp_type_index][frq_index])
                 else:
                     self.num_disp_points[-1][-1] = len(self.spin_lock_nu1[exp_type_index][frq_index])
