7d4071b9a1280c418f72a00c15d5ddeaf9066330
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2013-12-06 11:12:43 +0000

    Expanded the warning message for a systemtest.
    Work in progress for Support Request #3044, (https://gna.org/support/index.php?3044) - Load spins from SPARKY list.

--- lib/spectrum/nmrpipe.py
+++ lib/spectrum/nmrpipe.py
@@ -127,7 +127,7 @@ def read_seriestab(peak_list=None, file_data=None, int_col=None):
         try:
             res_name1 = row1[-4]
         except:
-            warn(RelaxWarning("Improperly formatted NMRPipe SeriesTab file, cannot process the residue name for dimension 1 in assignment: %s." % line[0]))
+            warn(RelaxWarning("Improperly formatted NMRPipe SeriesTab file, cannot process the residue name for dimension 1 in assignment: %s. Setting residue name to None." % line[0]))
             res_name1 = None
 
         # The residue name for dimension 2.
