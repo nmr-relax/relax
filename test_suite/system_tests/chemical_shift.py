8b4a530067b2b2baed944fa180c97dc445b16cfe
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2013-12-06 18:23:58 +0000

    Added system test for reading chemical shift from NMRPipe SeriesTab file.
    Work in progress for Support Request #3044, (https://gna.org/support/index.php?3044) - Load spins from SPARKY list.

--- test_suite/system_tests/chemical_shift.py
+++ test_suite/system_tests/chemical_shift.py
@@ -43,6 +43,26 @@ class Chemical_shift(SystemTestCase):
         self.interpreter.pipe.create('cs', 'mf')
 
 
+    def test_read_nmrpipe_seriestab(self):
+        """Test the reading of chemical shifts from an NMRPipe seriesTab peak list."""
+
+        # Read the spins from peak list.
+        self.interpreter.spectrum.read_spins(file="seriesTab.ser", dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists', dim=1)
+
+        # Read the chemical shift from the list.
+        self.interpreter.chemical_shift.read(file="seriesTab.ser", dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'peak_lists')
+
+        # Test the data.
+        cs = [128.374, 127.066, 124.523]
+        i = 0
+        for spin in spin_loop():
+            # Check the shift.
+            self.assertEqual(spin.chemical_shift, cs[i])
+
+            # Increment the index.
+            i += 1
+
+
     def test_read_nmrview(self):
         """Test the reading of chemical shifts from an NMRView peak list."""
 
