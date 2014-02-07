3119883276c33ebe5168060a599e4412d123c311
Edward d'Auvergne <edward@nmr-relax.com>
2014-02-07 16:48:28 +0000

    Added a copyright notice to the python_seek.py script.
    The descriptive text has also been converted into a docstring.  The copyright is simply to show who
    wrote the script and how old it is.

--- devel_scripts/python_seek.py
+++ devel_scripts/python_seek.py
@@ -1,7 +1,30 @@
 #! /usr/bin/env python
 
-# Script for finding all Python binaries on the system and to print out various package versions.
-# This requires access to a shell and the locate, grep, and other programs.
+###############################################################################
+#                                                                             #
+# Copyright (C) 2013-2014 Edward d'Auvergne                                   #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+"""Script for finding all Python binaries on the system and to print out various package versions.
+
+This requires access to a shell and the locate, grep, and other programs.
+"""
 
 
 # Python module imports.
