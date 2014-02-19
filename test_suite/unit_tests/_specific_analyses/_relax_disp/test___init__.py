2174cc6b5688befd5add047f9c73b657b6aba230
Edward d'Auvergne <edward@nmr-relax.com>
2014-02-19 12:59:52 +0000

    Added the unit test infrastructure for testing the specific_analyses.relax_disp package.
    This currently includes the package __all__ list unit test.

---
+++ test_suite/unit_tests/_specific_analyses/_relax_disp/test___init__.py
@@ -0,0 +1,39 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009-2014 Edward d'Auvergne                                   #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from os import sep
+import sys
+
+# relax module imports.
+from test_suite.unit_tests.package_checking import PackageTestCase
+from specific_analyses import relax_disp
+
+
+class Test___init__(PackageTestCase):
+    """Unit tests for the specific_analyses.relax_disp package."""
+
+    def setUp(self):
+        """Set up for the package checking."""
+
+        self.package = relax_disp
+        self.package_name = 'specific_analyses.relax_disp'
+        self.package_path = sys.path[0] + sep + 'specific_analyses' + sep + 'relax_disp'
