43d5ec06231ca9bf9219c662a26d4bf24d48717e
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-02-24 16:06:33 +0000

    Modified unit test to pass.
    Regarding bug #21665, (https://gna.org/bugs/?21665) - Running a CPMG analysis with two fields at two delay times.
    
    Implemented as suggested in: https://mail.gna.org/public/relax-devel/2014-02/msg00142.html

--- test_suite/unit_tests/_specific_analyses/_relax_disp/test_disp_data.py
+++ test_suite/unit_tests/_specific_analyses/_relax_disp/test_disp_data.py
@@ -214,7 +214,7 @@ class Test_disp_data(UnitTestCase):
         # Original indices (ei, mi, oi).
         indices = [
             [0, 0, 0, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 0],
-            [0, 1, 0, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 1]
+            [0, 1, 0, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 0]
         ]
 
         # Check the number of iterations.
