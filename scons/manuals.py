5dac5fd1baec0b8665eadee900f941745d7b2469
Edward d'Auvergne <edward@nmr-relax.com>
2014-03-08 15:00:04 +0000

    Copyright update.

--- scons/manuals.py
+++ scons/manuals.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2006-2013 Edward d'Auvergne                                   #
+# Copyright (C) 2006-2014 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
