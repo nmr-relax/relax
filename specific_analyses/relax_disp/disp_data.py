9233c4b8bfe42185937e375364c8100054c0ed13
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-03-14 22:21:20 +0000

    Removed the writing out of R1rho as function of Effective field w_eff relax_disp.write_disp_curves() as this surves no purpose.
    Regarding sr #3124, (https://gna.org/support/index.php?3124) - Grace graphs production for R1rho analysis with R2_eff as function of Omega_eff.

--- specific_analyses/relax_disp/disp_data.py
+++ specific_analyses/relax_disp/disp_data.py
@@ -3672,7 +3672,7 @@ def write_disp_curves(dir=None, force=None):
         if spin.model in MODEL_LIST_R1RHO_FULL and has_r1rho_exp_type() and hasattr(spin, 'isotope'):
             # Add additonal looping over writing parameters.
             writing_vars.append(['disp_theta',("Experiment_name", "Field_strength_(MHz)", "Tilt_angle_(rad)", "R2eff_(measured)", "R2eff_(back_calc)", "R2eff_errors")])
-            writing_vars.append(['disp_w_eff',("Experiment_name", "Field_strength_(MHz)", "Effective_field_(rad_s-1))", "R2eff_(measured)", "R2eff_(back_calc)", "R2eff_errors")])
+            #writing_vars.append(['disp_w_eff',("Experiment_name", "Field_strength_(MHz)", "Effective_field_(rad_s-1))", "R2eff_(measured)", "R2eff_(back_calc)", "R2eff_errors")])
 
         # Loop over writing vars
         for wvar in writing_vars:
