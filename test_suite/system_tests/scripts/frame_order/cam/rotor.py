325e2b190f6e95fb7a7993dfa2ce28832ec45304
Edward d'Auvergne <edward@nmr-relax.com>
2014-03-31 08:52:12 +0000

    Reverted r22192 as the parameter conversion is now performed by the base script.
    This is only for the rotor CaM frame order system test script.  The changes to the base script have
    already been rewritten.
    
    The command used was:
    svn merge -r22192:22191 test_suite/system_tests/scripts/frame_order/cam/rotor.py

--- test_suite/system_tests/scripts/frame_order/cam/rotor.py
+++ test_suite/system_tests/scripts/frame_order/cam/rotor.py
@@ -22,12 +22,8 @@
 # Module docstring.
 """Script for optimising the rotor frame order test model of CaM."""
 
-# Python module imports
-from numpy import arcsin, array, dot, float64, zeros
-
 # relax module imports.
 from base_script import Base_script
-from lib.geometry.coord_transform import spherical_to_cartesian
 
 
 class Analysis(Base_script):
@@ -35,14 +31,10 @@ class Analysis(Base_script):
     # Set up some class variables.
     DIRECTORY = 'rotor'
     MODEL = 'rotor'
+    AXIS_THETA = 0.9600799785953431
+    AXIS_PHI = 4.0322755062196229
     CONE_SIGMA_MAX = 30.0 / 360.0 * 2.0 * pi
 
-    # Translate the system.
-    axis_theta = 0.9600799785953431
-    axis_phi = 4.0322755062196229
-    r_ax = zeros(3, float64)
-    spherical_to_cartesian([1, axis_theta, axis_phi], r_ax)
-    AXIS_ALPHA = arcsin(dot(r_ax, array([0, 0, 1], float64)))
 
 # Execute the analysis.
 Analysis(self._execute_uf)
