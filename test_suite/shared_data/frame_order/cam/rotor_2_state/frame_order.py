b45b63bcc0b51fb5deae255a699d207b96f8a76f
Edward d'Auvergne <edward@nmr-relax.com>
2014-04-04 15:46:47 +0000

    Updated all of the frame order optimisation scripts for the CaM test suite data.
    These simple testing scripts were very much out of date and non-function for the current code.  Half
    of the code in these scripts is now implemented in the concise frame_order.pdb_model user function.

--- test_suite/shared_data/frame_order/cam/rotor_2_state/frame_order.py
+++ test_suite/shared_data/frame_order/cam/rotor_2_state/frame_order.py
@@ -1,34 +1,12 @@
 # Script for optimising the rotor frame order test model applied to a 2-state system of CaM.
 
 # Python module imports.
-from numpy import array, float64, transpose, zeros
-
-# relax module imports.
-from lib.geometry.rotations import euler_to_R_zyz
+from numpy import array
 
 
 class Analysis:
     def __init__(self):
         """Execute the frame order analysis."""
-
-        # Optimise.
-        self.optimisation()
-
-        # Load the original structure.
-        self.original_structure()
-
-        # Domain transformation.
-        self.transform()
-
-        # Display in pymol.
-        self.pymol_display()
-
-        # Save the state.
-        state.save('frame_order', force=True)
-
-
-    def optimisation(self):
-        """Optimise the frame order model."""
 
         # Create the data pipe.
         pipe.create(pipe_name='frame order', pipe_type='frame order')
@@ -129,65 +107,17 @@ class Analysis:
         eliminate()
         monte_carlo.error_analysis()
 
-
-    def original_structure(self):
-        """Load the original structure into a dedicated data pipe."""
-
-        # Create a special data pipe for the original rigid body position.
-        pipe.create(pipe_name='orig pos', pipe_type='frame order')
-
-        # Load the structure.
-        structure.read_pdb('1J7P_1st_NH_rot.pdb', dir='..')
-
-
-    def pymol_display(self):
-        """Display the results in PyMOL."""
-
-        # Switch back to the main data pipe.
-        pipe.switch('frame order')
-
-        # Load the PDBs of the 2 domains.
-        structure.read_pdb('1J7O_1st_NH.pdb', dir='..')
-        structure.read_pdb('1J7P_1st_NH_rot.pdb', dir='..')
-
-        # Create the cone PDB file.
-        frame_order.cone_pdb(file='cone.pdb', force=True)
-
-        # Set the domains.
-        frame_order.domain_to_pdb(domain='N', pdb='1J7O_1st_NH.pdb')
-        frame_order.domain_to_pdb(domain='C', pdb='1J7P_1st_NH_rot.pdb')
+        # Create the PDB representation.
+        frame_order.pdb_model(force=True)
 
         # PyMOL.
         pymol.view()
         pymol.command('show spheres')
-        pymol.cone_pdb('cone.pdb')
+        pymol.cone_pdb('frame_order.pdb')
 
+        # Save the state.
+        state.save('frame_order', force=True)
 
-    def transform(self):
-        """Transform the domain to the average position."""
-
-        # Switch back to the main data pipe.
-        pipe.switch('frame order')
-
-        # The rotation matrix.
-        R = zeros((3, 3), float64)
-        euler_to_R_zyz(cdp.ave_pos_alpha, cdp.ave_pos_beta, cdp.ave_pos_gamma, R)
-        print("Rotation matrix:\n%s\n" % R)
-        R = transpose(R)
-        print("Inverted rotation:\n%s\n" % R)
-        pivot = cdp.pivot
-
-        # Create a special data pipe for the average rigid body position.
-        pipe.create(pipe_name='ave pos', pipe_type='frame order')
-
-        # Load the structure.
-        structure.read_pdb('1J7P_1st_NH_rot.pdb', dir='..')
-
-        # Rotate all atoms.
-        structure.rotate(R=R, origin=pivot)
-
-        # Write out the new PDB.
-        structure.write_pdb('ave_pos', force=True)
 
 
 # Execute the analysis.
