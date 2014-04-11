0d80a0d3c1851c2c8401a1c3b42811b9e86f1b98
Edward d'Auvergne <edward@nmr-relax.com>
2014-04-11 16:21:46 +0000

    Fixes for the definitions of the N-state model analysis parameters.
    This analysis does not use the CSA value, and the paramagnetic centre is a list, not a float.

--- specific_analyses/n_state_model/parameter_object.py
+++ specific_analyses/n_state_model/parameter_object.py
@@ -40,14 +40,13 @@ class N_state_params(Param_list):
 
         # Add the base data.
         self._add_align_data()
-        self._add_csa()
 
         # Add up the model parameters.
         self._add('probs', scope='global', default=0.0, desc='The probabilities of each state', py_type=list, set='params', err=True, sim=True)
         self._add('alpha', scope='global', units='rad', default=0.0, desc='The alpha Euler angles (for the rotation of each state)', py_type=list, set='params', err=True, sim=True)
         self._add('beta', scope='global', units='rad', default=0.0, desc='The beta Euler angles (for the rotation of each state)', py_type=list, set='params', err=True, sim=True)
         self._add('gamma', scope='global', units='rad', default=0.0, desc='The gamma Euler angles (for the rotation of each state)', py_type=list, set='params', err=True, sim=True)
-        self._add('paramagnetic_centre', scope='global', units='Angstrom', desc='The paramagnetic centre', py_type=float, set='params', err=True, sim=True)
+        self._add('paramagnetic_centre', scope='global', units='Angstrom', desc='The paramagnetic centre', py_type=list, set='params', err=True, sim=True)
 
         # Add the minimisation data.
         self._add_min_data(min_stats_global=False, min_stats_spin=True)
