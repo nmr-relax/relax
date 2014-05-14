01e4e21ba873a782b6ffa5ec27286fdc7cc85a09
Edward d'Auvergne <edward@nmr-relax.com>
2014-05-14 20:46:53 +0000

    Created a script for finding all dead http://www.nmr-relax.com links in files of a directory tree.

---
+++ devel_scripts/find_dead_relax_links.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Script for extracting all http://www.nmr-relax.com URLs within all text files in a directory tree, and then determining which links are dead.
+
+while read line
+do
+    if ! curl --output /dev/null --silent --head --fail $line; then
+        echo "$line"
+    fi
+done < <(grep -r http . \
+| sed "s/http/\nhttp/g" \
+| grep "http://www.nmr-relax" \
+| sed "s/\">.*//g" \
+| sed "s/<\/.>.*//g" \
+| sed "s/\" .*//g" \
+| sed "s/&quot//g" \
+| sed "s/;\///g" \
+| sed "s/}.*//g" \
+| sed "s/ .*//g" \
+| sed "s/\\\\_/_/g" \
+| sed "s/\"//g" \
+| sed "s/<br>.*//g" \
+| sed "s/,//g" \
+| sed "s/#.*//g" \
+| sort -u)
