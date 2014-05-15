facadec9b5ac1d27b196580f4ce36d079c064d30
Edward d'Auvergne <edward@nmr-relax.com>
2014-05-15 15:05:36 +0000

    Modified the pivot point PDB representation script to include the shifted pivot.
    This is for the 2nd rotor model of CaM frame order in the test suite.

--- test_suite/shared_data/frame_order/cam/rotor2/pivot_point.py
+++ test_suite/shared_data/frame_order/cam/rotor2/pivot_point.py
@@ -14,8 +14,11 @@ structure.add_molecule(name='piv')
 # Alias the single molecule from the single model.
 mol = structure.structural_data[0].mol[0]
 
-# Add the pivot.
+# Add the original pivot.
 mol.atom_add(atom_name='N', res_name='PIV', res_num=1, pos=[ 37.254, 0.5, 16.7465], element='N')
+
+# Add the shifted pivot.
+mol.atom_add(atom_name='N', res_name='SFT', res_num=2, pos=[ 38.968163825736738,   2.6192628972111,   13.499077238576398], element='N')
 
 # Save as a PDB file.
 file = open_write_file('pivot_point.pdb', force=True)
