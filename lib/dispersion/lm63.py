dbe1c92a960bd4e79cb077b80ba522bfe082e314
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-05-21 11:02:59 +0000

    Align math-domain catching for model LM63 with trunk implementation.
    task #7793: (https://gna.org/task/?7793) Speed-up of dispersion models.
    
    This is to implement catching of math domain errors, before they occur.
    
    The catching of errors have to be more careful.

--- lib/dispersion/lm63.py
+++ lib/dispersion/lm63.py
@@ -85,6 +85,15 @@ def r2eff_LM63(r20=None, phi_ex=None, kex=None, cpmg_frqs=None, num_points=None)
     @type num_points:       int
     """
 
+    # Catch divide with zeros (to avoid pointless mathematical operations).
+    if kex == 0.0:
+        return array([1e100]*num_points)
+
+    # Catch zeros (to avoid pointless mathematical operations).
+    # This will result in no exchange, returning flat lines.
+    if phi_ex == 0.0:
+        return array([r20]*num_points)
+
     # Repetitive calculations (to speed up calculations).
     rex = phi_ex / kex
     kex_4 = 4.0 / kex
