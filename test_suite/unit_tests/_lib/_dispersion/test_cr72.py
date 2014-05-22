6d14114b12abd3b40b24fc6073b503d36356db46
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-05-22 17:27:31 +0000

    Unit test _lib/_dispersion/test_b14.py copied to _lib/_dispersion/test_cr72.py.
    They are both of CPMG type, and can be re-used.

---
+++ test_suite/unit_tests/_lib/_dispersion/test_cr72.py
@@ -0,0 +1,171 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2014 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+# Python module imports.
+from numpy import array, float64, int16, pi, zeros
+from unittest import TestCase
+
+# relax module imports.
+from lib.dispersion.b14 import r2eff_B14
+
+
+class Test_b14(TestCase):
+    """Unit tests for the lib.dispersion.b14 relax module."""
+
+    def setUp(self):
+        """Set up for all unit tests."""
+
+        # Default parameter values.
+        self.r20a = 2.0
+        self.r20b = 2.0
+        self.pA = 0.99
+        self.dw = 2.0
+        self.kex = 1000.0
+
+        # Required data structures.
+        self.num_points = 7
+        self.ncyc = array([2, 4, 8, 10, 20, 40, 500])
+        relax_times = 0.04
+        cpmg_frqs = self.ncyc / relax_times
+        self.inv_relax_times = 1.0 / relax_times
+        self.tau_cpmg = 0.25 / cpmg_frqs
+
+        # The spin Larmor frequencies.
+        self.sfrq = 200. * 1E6
+
+    def calc_r2eff(self):
+        """Calculate and check the R2eff values."""
+
+        # Parameter conversions.
+        k_AB, k_BA, pB, dw_frq  = self.param_conversion(pA=self.pA, kex=self.kex, dw=self.dw, sfrq=self.sfrq)
+
+        # Calculate the R2eff values.
+        R2eff = r2eff_B14(r20a=self.r20a, r20b=self.r20b, pA=self.pA, pB=pB, dw=dw_frq, kex=self.kex, k_AB=k_AB, k_BA=k_BA, ncyc=self.ncyc, inv_tcpmg=self.inv_relax_times, tcp=self.tau_cpmg, num_points=self.num_points)
+
+        # Check all R2eff values.
+        for i in range(self.num_points):
+            self.assertAlmostEqual(R2eff[i], self.r20a)
+
+
+    def param_conversion(self, pA=None, kex=None, dw=None, sfrq=None):
+        """Convert the parameters.
+
+        @keyword pA:    The population of state A.
+        @type pA:       float
+        @keyword kex:   The rate of exchange.
+        @type kex:      float
+        @keyword dw:    The chemical exchange difference between states A and B in ppm.
+        @type dw:       float
+        @keyword sfrq:  The spin Larmor frequencies in Hz.
+        @type sfrq:     float
+        @return:        The parameters {k_AB, k_BA, pB, dw_frq}.
+        @rtype:         tuple of float
+        """
+
+        # Calculate pB.
+        pB = 1.0 - pA
+
+        # Exchange rates.
+        k_BA = pA * kex
+        k_AB = pB * kex
+
+        # Calculate spin Larmor frequencies in 2pi.
+        frqs = sfrq * 2 * pi
+
+        # Convert dw from ppm to rad/s.
+        dw_frq = dw * frqs
+
+        # Return all values.
+        return k_AB, k_BA, pB, dw_frq
+
+
+    def test_b14_no_rex1(self):
+        """Test the r2eff_b14() function for no exchange when dw = 0.0."""
+
+        # Parameter reset.
+        self.dw = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex2(self):
+        """Test the r2eff_b14() function for no exchange when pA = 1.0."""
+
+        # Parameter reset.
+        self.pA = 1.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex3(self):
+        """Test the r2eff_b14() function for no exchange when kex = 0.0."""
+
+        # Parameter reset.
+        self.kex = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex4(self):
+        """Test the r2eff_b14() function for no exchange when dw = 0.0 and pA = 1.0."""
+
+        # Parameter reset.
+        self.pA = 1.0
+        self.dw = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex5(self):
+        """Test the r2eff_b14() function for no exchange when dw = 0.0 and kex = 0.0."""
+
+        # Parameter reset.
+        self.dw = 0.0
+        self.kex = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex6(self):
+        """Test the r2eff_b14() function for no exchange when pA = 1.0 and kex = 0.0."""
+
+        # Parameter reset.
+        self.pA = 1.0
+        self.kex = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
+
+
+    def test_b14_no_rex7(self):
+        """Test the r2eff_b14() function for no exchange when dw = 0.0, pA = 1.0, and kex = 0.0."""
+
+        # Parameter reset.
+        self.dw = 0.0
+        self.kex = 0.0
+
+        # Calculate and check the R2eff values.
+        self.calc_r2eff()
