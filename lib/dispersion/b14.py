dcfb33b08eb73a6b17f348fa315f064e9dfe464c
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-05-22 17:15:34 +0000

    Critical fix for the math domain catching of model B14.
    This was discovered with the added 7 unit tests demonstrating edge case 'no Rex' failures.
    
    This follows from the ideas in the post http://article.gmane.org/gmane.science.nmr.relax.devel/5858.
    This is related to: task #7793: (https://gna.org/task/?7793) Speed-up of dispersion models.
    
    This is to implement catching of math domain errors, before they occur.

--- lib/dispersion/b14.py
+++ lib/dispersion/b14.py
@@ -147,6 +147,10 @@ def r2eff_B14(r20a=None, r20b=None, pA=None, pB=None, dw=None, kex=None, k_AB=No
     @type num_points:       int
     """
 
+    # Catch parameter values that will result in no exchange, returning flat R2eff = R20 lines (when kex = 0.0, k_AB = 0.0).
+    if dw == 0.0 or pA == 1.0 or k_AB == 0.0:
+        return array([r20a]*num_points)
+
     # Repetitive calculations (to speed up calculations).
     deltaR2 = r20a - r20b
 
