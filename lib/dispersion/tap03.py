e253d8d5e64a9f166e093dcc179e3f5ab48cf35d
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-05-29 10:24:25 +0000

    Converting back to having back_calc as a function argument to model TAP03.
    This is to clean up the API.
    There can be bo no partial measures/implementations in the relax trunk.
    
    The problem is, that many numerical models can't be optimised further, since they
    evolve the spin-magnetisation in a matrix.  That spin evolvement can't be put into
    a larger numpy array.
    
    This is related to: task #7793: (https://gna.org/task/?7793) Speed-up of dispersion models.

--- lib/dispersion/tap03.py
+++ lib/dispersion/tap03.py
@@ -63,7 +63,7 @@ More information on the TAP03 model can be found in the:
 from numpy import arctan2, array, isfinite, min, sin, sqrt, sum
 
 
-def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=None, kex=None, R1=0.0, spin_lock_fields=None, spin_lock_fields2=None, num_points=None):
+def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=None, kex=None, R1=0.0, spin_lock_fields=None, spin_lock_fields2=None, back_calc=None, num_points=None):
     """Calculate the R1rho' values for the TP02 model.
 
     See the module docstring for details.  This is the Trott, Abergel and Palmer (2003) equation.
@@ -89,7 +89,9 @@ def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=
     @type spin_lock_fields:     numpy rank-1 float array
     @keyword spin_lock_fields2: The R1rho spin-lock field strengths squared (in rad^2.s^-2).  This is for speed.
     @type spin_lock_fields2:    numpy rank-1 float array
-    @keyword num_points:        The number of points on the dispersion curve, equal to the length of the spin_lock_fields.
+    @keyword back_calc:         The array for holding the back calculated R1rho values.  Each element corresponds to the combination of offset and spin lock field.
+    @type back_calc:            numpy rank-1 float array
+    @keyword num_points:        The number of points on the dispersion curve, equal to the length of the spin_lock_fields and back_calc arguments.
     @type num_points:           int
     """
 
@@ -116,7 +118,8 @@ def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=
 
     # Bad gamma.
     if min(gamma) < 0.0:
-        return array([1e100]*num_points)
+        back_calc[:] = array([1e100]*num_points)
+        return
 
     # Special omega values.
     waeff2 = gamma*spin_lock_fields2 + da**2     # Effective field at A.
@@ -136,7 +139,8 @@ def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=
     # Catch zeros (to avoid pointless mathematical operations).
     # This will result in no exchange, returning flat lines.
     if numer == 0.0:
-        return R1_cos_theta2 + R1rho_prime_sin_theta2
+        back_calc[:] = R1_cos_theta2 + R1rho_prime_sin_theta2
+        return
 
     # Denominator.
     denom = waeff2*wbeff2/weff2 + kex2 - 2.0*hat_sin_theta2*phi_ex + (1.0 - gamma)*spin_lock_fields2
@@ -149,4 +153,4 @@ def r1rho_TAP03(r1rho_prime=None, omega=None, offset=None, pA=None, pB=None, dw=
     if not isfinite(sum(R1rho)):
         R1rho = array([1e100]*num_points)
 
-    return R1rho
+    back_calc[:] = R1rho
