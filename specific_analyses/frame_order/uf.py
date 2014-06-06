54516145e0c32707ef22d7ea0c0e9ece92baf29b
Edward d'Auvergne <edward@nmr-relax.com>
2014-06-06 08:37:29 +0000

    Fix for the failure of the frame_order.pdb_model user function for the free rotor frame order model.
    This is due to the recent parameter conversion to the axis alpha angle.

--- specific_analyses/frame_order/uf.py
+++ specific_analyses/frame_order/uf.py
@@ -217,7 +217,7 @@ def pdb_geometric_rep(file=None, dir=None, size=30.0, inc=36, force=False, neg_c
         print("\nGenerating the z-axis system.")
 
         # The axis.
-        if cdp.model in ['rotor']:
+        if cdp.model in ['rotor', 'free rotor']:
             axis = create_rotor_axis_alpha(alpha=cdp.axis_alpha, pivot=pivot, point=com)
         else:
             axis = create_rotor_axis_spherical(theta=cdp.axis_theta, phi=cdp.axis_phi)
