f50e9e867bcb09171e25fe5de7fd1c0946cd035f
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-06-12 21:11:38 +0000

    Documentation fix for description of input arrays to lib functions.
    Task #7807 (https://gna.org/task/index.php?7807): Speed-up of dispersion models for Clustered analysis.

--- lib/dispersion/b14.py
+++ lib/dispersion/b14.py
@@ -149,7 +149,7 @@ def r2eff_B14(r20a=None, r20b=None, pA=None, pB=None, dw=None, dw_orig=None, kex
     @keyword back_calc:     The array for holding the back calculated R2eff values.  Each element corresponds to one of the CPMG nu1 frequencies.
     @type back_calc:        numpy float array of rank [NE][NS][[NM][NO][ND]
     @keyword num_points:    The number of points on the dispersion curve, equal to the length of the cpmg_frqs and back_calc arguments.
-    @type num_points:       int
+    @type num_points:       numpy int array of rank [NE][NS][[NM][NO][ND]
     """
 
     # Flag to tell if values should be replaced if math function is violated.
--- lib/dispersion/cr72.py
+++ lib/dispersion/cr72.py
@@ -125,7 +125,7 @@ def r2eff_CR72(r20a=None, r20a_orig=None, r20b=None, r20b_orig=None, pA=None, dw
     @keyword back_calc:     The array for holding the back calculated R2eff values.  Each element corresponds to one of the CPMG nu1 frequencies.
     @type back_calc:        numpy float array of rank [NE][NS][[NM][NO][ND]
     @keyword num_points:    The number of points on the dispersion curve, equal to the length of the cpmg_frqs and back_calc arguments.
-    @type num_points:       int
+    @type num_points:       numpy int array of rank [NE][NS][[NM][NO][ND]
     """
 
     # Flag to tell if values should be replaced if max_etapos in cosh function is violated.
