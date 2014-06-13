5836d64f126ddbd0b84cba0581615fc5d3292449
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-06-13 09:47:45 +0000

    Removed number of points, to be send to lib function of model CR72.
    Number of points are not used anymore.
    
    Fixed in target function.
    Fixed in lib function.
    Fixed in corresponding unit tests
    
    Task #7807 (https://gna.org/task/index.php?7807): Speed-up of dispersion models for Clustered analysis.

--- lib/dispersion/cr72.py
+++ lib/dispersion/cr72.py
@@ -98,7 +98,7 @@ from numpy.ma import fix_invalid, masked_greater_equal, masked_less, masked_wher
 # Repetitive calculations (to speed up calculations).
 eta_scale = 2.0**(-3.0/2.0)
 
-def r2eff_CR72(r20a=None, r20a_orig=None, r20b=None, r20b_orig=None, pA=None, dw=None, dw_orig=None, kex=None, cpmg_frqs=None, back_calc=None, num_points=None):
+def r2eff_CR72(r20a=None, r20a_orig=None, r20b=None, r20b_orig=None, pA=None, dw=None, dw_orig=None, kex=None, cpmg_frqs=None, back_calc=None):
     """Calculate the R2eff values for the CR72 model.
 
     See the module docstring for details.
@@ -124,8 +124,6 @@ def r2eff_CR72(r20a=None, r20a_orig=None, r20b=None, r20b_orig=None, pA=None, dw
     @type cpmg_frqs:        numpy float array of rank [NE][NS][[NM][NO][ND]
     @keyword back_calc:     The array for holding the back calculated R2eff values.  Each element corresponds to one of the CPMG nu1 frequencies.
     @type back_calc:        numpy float array of rank [NE][NS][[NM][NO][ND]
-    @keyword num_points:    The number of points on the dispersion curve, equal to the length of the cpmg_frqs and back_calc arguments.
-    @type num_points:       numpy int array of rank [NE][NS][[NM][NO][ND]
     """
 
     # Flag to tell if values should be replaced if max_etapos in cosh function is violated.
