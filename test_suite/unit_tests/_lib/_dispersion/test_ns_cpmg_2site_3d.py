eed52fa9c85d30b037f4945c79665b49679d0eff
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-06-15 06:53:34 +0000

    Fix for unit tests of model ns cpmg 2site 3d to the reduced input to the lib function.
    Task #7807 (https://gna.org/task/index.php?7807): Speed-up of dispersion models for Clustered analysis.

--- test_suite/unit_tests/_lib/_dispersion/test_ns_cpmg_2site_3d.py
+++ test_suite/unit_tests/_lib/_dispersion/test_ns_cpmg_2site_3d.py
@@ -68,7 +68,7 @@ class Test_ns_cpmg_2site_3d(TestCase):
         k_AB, k_BA, pB, dw_frq, M0 = self.param_conversion(pA=self.pA, kex=self.kex, dw=self.dw, sfrq=self.sfrq, M0=self.M0)
 
         # Calculate the R2eff values.
-        r2eff_ns_cpmg_2site_3D(r180x=self.r180x, M0=M0, r20a=self.r20a, r20b=self.r20b, pA=self.pA, pB=pB, dw=dw_frq, k_AB=k_AB, k_BA=k_BA, inv_tcpmg=self.inv_relax_times, tcp=self.tau_cpmg, back_calc=self.R2eff, num_points=self.num_points, power=self.ncyc)
+        r2eff_ns_cpmg_2site_3D(r180x=self.r180x, M0=M0, r20a=self.r20a, r20b=self.r20b, pA=self.pA, dw=dw_frq, kex=self.kex, inv_tcpmg=self.inv_relax_times, tcp=self.tau_cpmg, back_calc=self.R2eff, num_points=self.num_points, power=self.ncyc)
 
         if self.kex >= 1.e5:
             for i in range(self.num_points):
