92ca8165f08eef0fe7e48fd945e0e57cfdd27a9c
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-06-20 06:19:17 +0000

    Moved the costly finding of matrix exponential out of for loops for eR_tcp.
    Task #7807 (https://gna.org/task/index.php?7807): Speed-up of dispersion models for Clustered analysis.

--- lib/dispersion/ns_cpmg_2site_star.py
+++ lib/dispersion/ns_cpmg_2site_star.py
@@ -63,7 +63,7 @@ from numpy.ma import fix_invalid, masked_where
 # relax module imports.
 from lib.float import isNaN
 from lib.dispersion.ns_matrices import rcpmg_star_rankN
-from lib.linear_algebra.matrix_exponential import matrix_exponential
+from lib.linear_algebra.matrix_exponential import matrix_exponential, matrix_exponential_rankN
 from lib.linear_algebra.matrix_power import square_matrix_power
 
 
@@ -141,6 +141,8 @@ def r2eff_ns_cpmg_2site_star(Rr=None, Rex=None, RCS=None, R=None, M0=None, r20a=
     # The matrix R that contains all the contributions to the evolution, i.e. relaxation, exchange and chemical shift evolution.
     R_mat, cR2_mat, Rr_mat, Rex_mat, RCS_mat = rcpmg_star_rankN(R2A=r20a, R2B=r20b, pA=pA, pB=pB, dw=dw, k_AB=k_AB, k_BA=k_BA, tcp=tcp)
 
+    eR_mat = matrix_exponential_rankN(R_mat)
+
     # Loop over the spins
     for si in range(NS):
         # Loop over the spectrometer frequencies.
@@ -200,7 +202,7 @@ def r2eff_ns_cpmg_2site_star(Rr=None, Rex=None, RCS=None, R=None, M0=None, r20a=
                     print tcp_si_mi_di - tcp[0, si, mi, 0, di]
                     print asd
 
-                eR_tcp = matrix_exponential(R_mat_i)
+                eR_tcp = eR_mat[0, si, mi, 0, di]
 
                 # This is the propagator for an element of [delay tcp; 180 deg pulse; 2 times delay tcp; 180 deg pulse; delay tau], i.e. for 2 times tau-180-tau.
                 prop_2 = dot(dot(eR_tcp, matrix_exponential(cR2_mat_i)), eR_tcp)
