b26b0632611690d02bbeecf8f09e3b24abb6170a
Edward d'Auvergne <edward@nmr-relax.com>
2014-07-09 13:31:21 +0000

    Modified the specific_analyses.frame_order.parameters.update_model() function.
    The pivot parameters are now set to 0.0 if they do not currently exist.  They were previously
    excluded from this initialisation, but this is required for the frame order models if the pivot is
    not initially fixed at a give location in the PDB space.

--- specific_analyses/frame_order/parameters.py
+++ specific_analyses/frame_order/parameters.py
@@ -266,5 +266,5 @@ def update_model():
 
     # Initialise the parameters in the current data pipe.
     for param in cdp.params:
-        if not param in ['pivot_x', 'pivot_y', 'pivot_z'] and not hasattr(cdp, param):
+        if not hasattr(cdp, param):
             setattr(cdp, param, 0.0)
