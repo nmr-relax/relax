366df6901d2c08044e164e0178acea1a9e68c6d8
Edward d'Auvergne <edward@nmr-relax.com>
2014-07-09 15:19:52 +0000

    Modified the script for recreating the frame order PDB representation and displaying it in PyMOL.
    The state loading, domain redefinition, and representation creation parts have all been removed, as
    these will soon all be redundant as the frame order analysis for all models is being redone.  All
    that remains are the pymol.frame_order() function calls for displaying all the representations.

--- test_suite/shared_data/frame_order/cam/represent_frame_order.py
+++ test_suite/shared_data/frame_order/cam/represent_frame_order.py
@@ -1,13 +1,3 @@
-# Load the final state.
-state.load('frame_order')
-
-# Define the domains.
-domain(id='N', spin_id="#N-dom")
-domain(id='C', spin_id="#C-dom")
-
-# Re-create the PDB representation (for debugging).
-frame_order.pdb_model(ave_pos='ave_pos', rep='frame_order', dist=None, compress_type=2, force=True)
-
 # PyMOL.
 pymol.frame_order(ave_pos='ave_pos_true', rep='frame_order_true', dist=None)
 pymol.frame_order(ave_pos='ave_pos_fixed_piv', rep='frame_order_fixed_piv', dist=None)
