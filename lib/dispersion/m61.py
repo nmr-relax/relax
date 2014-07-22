e7150fa99613b3c926e40b276efa7ce4d71e8e6d
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2014-07-22 14:51:37 +0000

    Code validation of lib/dispersion/m61.py.
    Task #7807 (https://gna.org/task/index.php?7807): Speed-up of dispersion models for Clustered analysis.

--- lib/dispersion/m61.py
+++ lib/dispersion/m61.py
@@ -68,6 +68,7 @@ More information on the M61 model can be found in the:
 from numpy import any, isfinite, min, sum
 from numpy.ma import fix_invalid, masked_where
 
+
 def r1rho_M61(r1rho_prime=None, phi_ex=None, kex=None, spin_lock_fields2=None, back_calc=None):
     """Calculate the R2eff values for the M61 model.
 
@@ -106,7 +107,7 @@ def r1rho_M61(r1rho_prime=None, phi_ex=None, kex=None, spin_lock_fields2=None, b
     denom = kex2 + spin_lock_fields2
 
     # Catch math domain error of dividing with 0.
-    # This is when denom=0.
+    # This is when denom = 0.
     mask_denom_zero = denom == 0.0
     if any(mask_denom_zero):
         t_denom_zero = True
