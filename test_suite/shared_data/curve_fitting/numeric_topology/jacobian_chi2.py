757fb23c7912123c1ddddd8edb8b9f6420fde5a9
Edward d'Auvergne <edward@nmr-relax.com>
2014-08-29 16:24:14 +0000

    Added a script and log for calculating the numerical chi-squared Jacobian for an exponential curve.
    This uses the data at http://thread.gmane.org/gmane.science.nmr.relax.devel/6807/focus=6840 and
    calculates the chi-squared Jacobian using the numdifftools.Jacobian object construct and obtain the
    matrix, both at the minimum and at a point away from the minimum.  The values will be used to
    construct a unit test to check the C module implementation.

---
+++ test_suite/shared_data/curve_fitting/numeric_topology/jacobian_chi2.py
@@ -0,0 +1,57 @@
+# Script for numerically calculating the exponential curve gradient.
+
+# Python module imports.
+from math import exp
+from numpy import array, float64
+from numdifftools import Jacobian
+
+
+def func(params):
+    """Calculate the chi-squared value."""
+
+    global times, I, errors
+
+    # Unpack the parameters.
+    R, I0 = params
+
+    # The intensities.
+    back_calc = []
+    for i in range(len(times)):
+        back_calc.append(I0 * exp(-R*times[i]))
+
+    # The chi2.
+    chi2 = 0.0
+    for i in range(len(times)):
+        chi2 += (I[i] - back_calc[i])**2 / errors[i]**2
+
+    # Return the value.
+    return chi2
+
+
+# The real parameters.
+R = 1.0
+I0 = 1000.0
+
+# The time points.
+times = [0.0, 1.0, 2.0, 3.0, 4.0]
+
+# The intensities for the above I0 and R.
+I = [1000.0, 367.879441171, 135.335283237, 49.7870683679, 18.3156388887]
+
+# The intensity errors.
+errors = [10.0, 10.0, 10.0, 10.0, 10.0]
+
+# Set up the Jacobian function.
+jacobian = Jacobian(func)
+
+# The numeric Jacobian at the minimum.
+print("\n\nOn-minimum:\n")
+matrix = jacobian([R, I0])
+print("The Chi2 Jacobian at %s is:\n%s" % ([R, I0], matrix))
+
+# The numeric Jacobian off the minimum.
+print("\n\nOff-minimum:\n")
+R = 2.0
+I0 = 500.0
+matrix = jacobian([R, I0])
+print("The Chi2 Jacobian at %s is:\n%s" % ([R, I0], matrix))
