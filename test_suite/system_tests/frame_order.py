f8efbcf22ce3790c7c173e8de760abf34cba52ba
Edward d'Auvergne <edward@nmr-relax.com>
2014-09-14 13:41:15 +0000

    Created the Frame_order.test_count_sobol_points_free_rotor system test.
    This is to demonstrate that the frame_order.count_sobol_points user function currently fails for the
    free-rotor model.

--- test_suite/system_tests/frame_order.py
+++ test_suite/system_tests/frame_order.py
@@ -976,6 +976,23 @@ class Frame_order(SystemTestCase):
         self.assertEqual(cdp.used_sobol_points, 12)
 
 
+    def test_count_sobol_points_free_rotor(self):
+        """Test the frame_order.count_sobol_points user function for the free-rotor model."""
+
+        # Reset.
+        self.interpreter.reset()
+
+        # Load the state file.
+        data_path = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'frame_order'+sep+'cam'+sep+'free_rotor'
+        self.interpreter.state.load(data_path+sep+'frame_order')
+
+        # Call the user function.
+        self.interpreter.frame_order.count_sobol_points()
+
+        # Check the count.
+        self.assertEqual(cdp.used_sobol_points, 5834)
+
+
     def test_count_sobol_points_rigid(self):
         """Test the frame_order.count_sobol_points user function for the rigid model."""
 
