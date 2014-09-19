2438ea86b8180c52192f33d3269d9c34463fc9b7
Edward d'Auvergne <edward@nmr-relax.com>
2014-09-19 09:32:04 +0000

    Added numdifftools to the extern package __all__ list.

--- extern/__init__.py
+++ extern/__init__.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2011-2013 Edward d'Auvergne                                   #
+# Copyright (C) 2011-2014 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
@@ -23,6 +23,7 @@
 """External Python modules bundled with relax."""
 
 __all__ = [
+    'numdifftools',
     'numpy_future',
     'sobol'
 ]
