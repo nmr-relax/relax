749bea8558b91f6fdbc29b1c1282f519235cdaeb
Edward d'Auvergne <edward@nmr-relax.com>
2014-09-26 09:31:29 +0000

    Created a special Check class based on the strategy design pattern.
    This is in the new lib.checks module.  The class will be used to simplify and unify all of the
    check_*() functions in the pipe_control and specific_analyses packages.

---
+++ lib/checks.py
@@ -0,0 +1,72 @@
+from __future__ import absolute_import
+###############################################################################
+#                                                                             #
+# Copyright (C) 2014 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+# Module docstring.
+"""Module for the Check class based on the strategy design pattern."""
+
+# Python module imports.
+from types import MethodType
+from warnings import warn
+
+# relax module imports.
+from lib.errors import RelaxError
+from lib.warnings import RelaxWarning
+
+
+class Check:
+    """Data checking class based on the U{strategy design pattern<https://en.wikipedia.org/wiki/Strategy_pattern>}."""
+
+    def __init__(self, function):
+        """Convert the function argument into a class instance method.
+
+        @param function:    The function to convert into the self.checks class instance method which is called from the __call__ method.
+        @type function:     function
+        """
+
+        # Convert the function into a method of this class instance.
+        self.checks = MethodType(function, self, Check)
+
+
+    def __call__(self, escalate=0, *args, **kargs):
+        """Make the object callable, and perform the checks.
+
+        This will call the function used to initialise the class and then
+
+
+        @keyword escalate:      The feedback to give if the check fails.  This can be 0 for no printouts, 1 to throw a RelaxWarning, or 2 to raise a RelaxError.
+        @type escalate:         int
+        @raises RelaxError:     If escalate is set to 2 and the check fails.
+        @return:                True if the check passes, False otherwise.
+        @rtype:                 bool
+        """
+
+        # Perform the check.
+        check_ok, msg = self.checks(*args, **kargs)
+
+        # Warnings and errors.
+        if not check_ok and escalate == 1:
+            warn(RelaxWarning(msg))
+        elif not check_ok and escalate == 2:
+            raise RelaxError(msg)
+ 
+        # Return the status.
+        return check_ok
