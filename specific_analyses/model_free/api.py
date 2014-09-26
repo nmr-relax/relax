b0b684ecc5771ce8caa24d4efe2c1932fa913ca3
Edward d'Auvergne <edward@nmr-relax.com>
2014-09-26 08:06:58 +0000

    The model-free bmrb_write() API method now checks for spectrometer information.
    This is via a call to the pipe_control.spectrometer.check_spectrometer_setup() function.

--- specific_analyses/model_free/api.py
+++ specific_analyses/model_free/api.py
@@ -51,6 +51,7 @@ from pipe_control.bmrb import list_sample_conditions
 from pipe_control.exp_info import bmrb_write_citations, bmrb_write_methods, bmrb_write_software
 from pipe_control.interatomic import return_interatom_list
 from pipe_control.mol_res_spin import count_spins, exists_mol_res_spin_data, find_index, get_molecule_names, return_spin, return_spin_from_index, return_spin_indices, spin_loop
+from pipe_control.spectrometer import check_spectrometer_setup
 from specific_analyses.api_base import API_base
 from specific_analyses.api_common import API_common
 from specific_analyses.model_free.bmrb import sf_csa_read, sf_model_free_read, to_bmrb_model
@@ -180,6 +181,9 @@ class Model_free(API_base, API_common):
         @keyword version:   The BMRB NMR-STAR dictionary format to output to.
         @type version:      str
         """
+
+        # Checks.
+        check_spectrometer_setup(escalate=2)
 
         # Alias the current data pipe.
         cdp = pipes.get_pipe()
