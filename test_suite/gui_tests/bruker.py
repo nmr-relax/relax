d5506741269310ff4140a5d581d94655f26b00ca
Edward d'Auvergne <edward@nmr-relax.com>
2014-10-17 13:05:32 +0000

    Resign of how the new analysis wizard is handled in the GUI tests.
    This relates to bug #22818 (https://gna.org/bugs/?22818), the GUI test suite failures in MS Windows
    - PyAssertionError: C++ assertion "Assert failure".
    
    The GUI test base class method new_analysis_wizard() has been created to simplify the process.  When
    a new analysis is desired, this method should be called.  It will return the analysis page GUI
    element for use in the test.  The method standardises the execution of the new analysis wizard and
    sets up the analysis in the GUI.  It also properly destroys the wizard to avoid the memory leaking
    issues such as bug #22818.
    
    All GUI tests have been converted to use new_analysis_wizard().  This allows the GUI tests to pass
    on MS Windows.  However there are still significant sources of memory leaks (the USER Objects count)
    visible in the Windows Task Manager.

--- test_suite/gui_tests/bruker.py
+++ test_suite/gui_tests/bruker.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2012-2013 Edward d'Auvergne                                   #
+# Copyright (C) 2012-2014 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
@@ -61,22 +61,7 @@ class Bruker(GuiTestCase, system_tests.bruker.Bruker):
         """Test the reading of a DC file, catching U{bug #20152<https://gna.org/bugs/?20152>}."""
 
         # Simulate the new analysis wizard.
-        self.app.gui.analysis.menu_new(None)
-        page = self.app.gui.analysis.new_wizard.wizard.get_page(0)
-        page.select_mf(None)
-        page.analysis_name.SetValue(str_to_gui("Model-free test"))
-        self.app.gui.analysis.new_wizard.wizard._go_next(None)
-        page = self.app.gui.analysis.new_wizard.wizard.get_page(1)
-        self.app.gui.analysis.new_wizard.wizard._go_next(None)
-
-        # Get the data.
-        analysis_type, analysis_name, pipe_name, pipe_bundle, uf_exec = self.app.gui.analysis.new_wizard.get_data()
-
-        # Set up the analysis.
-        self.app.gui.analysis.new_analysis(analysis_type=analysis_type, analysis_name=analysis_name, pipe_name=pipe_name, pipe_bundle=pipe_bundle)
-
-        # Alias the analysis.
-        analysis = self.app.gui.analysis.get_page_from_name("Model-free test")
+        analysis = self.new_analysis_wizard(analysis_type='mf', analysis_name='Model-free test')
 
         # Change the results directory.
         analysis.field_results_dir.SetValue(str_to_gui(ds.tmpdir))
