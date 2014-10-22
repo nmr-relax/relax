5ca440f7088e96d85718896f2b2c983642457e6b
Edward d'Auvergne <edward@nmr-relax.com>
2014-10-22 12:34:35 +0000

    Updated the N_state_model.test_CaM_IQ_tensor_fit system test so it operates correctly as a GUI test.
    All user functions are now executed through the special self._execute_uf() method to allow either
    the prompt interpreter or the GUI to execute the user function.

--- test_suite/system_tests/scripts/n_state_model/CaM_IQ_tensor_fit.py
+++ test_suite/system_tests/scripts/n_state_model/CaM_IQ_tensor_fit.py
@@ -14,27 +14,27 @@ DATA_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'align_
 STRUCT_PATH = status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'structures'
 
 # Create a data pipe for all the data.
-pipe.create('CaM N-dom', 'N-state')
+self._execute_uf(uf_name='pipe.create', pipe_name='CaM N-dom', pipe_type='N-state')
 
 # Load the CaM structure.
-structure.read_pdb('2BE6_core_I_IV.pdb', dir=STRUCT_PATH, set_mol_name=['CaM_A', 'IQ_A', 'Metals_A', 'CaM_B', 'IQ_B', 'Metals_B', 'CaM_C', 'IQ_C', 'Metals_C'])
+self._execute_uf(uf_name='structure.read_pdb', file='2BE6_core_I_IV.pdb', dir=STRUCT_PATH, set_mol_name=['CaM_A', 'IQ_A', 'Metals_A', 'CaM_B', 'IQ_B', 'Metals_B', 'CaM_C', 'IQ_C', 'Metals_C'])
 
 # Load the spins.
-structure.load_spins('@N', from_mols=['CaM_A', 'CaM_B', 'CaM_C'], mol_name_target='CaM', ave_pos=False)
-structure.load_spins('@H', from_mols=['CaM_A', 'CaM_B', 'CaM_C'], mol_name_target='CaM', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id='@N', from_mols=['CaM_A', 'CaM_B', 'CaM_C'], mol_name_target='CaM', ave_pos=False)
+self._execute_uf(uf_name='structure.load_spins', spin_id='@H', from_mols=['CaM_A', 'CaM_B', 'CaM_C'], mol_name_target='CaM', ave_pos=False)
 
 # Select only the superimposed spins (skipping mobile residues :2-4,42,56-57,76-80, identified from model-free order parameters).
-select.spin(':5-31,53-55,58-75', change_all=True)
-select.display()
+self._execute_uf(uf_name='select.spin', spin_id=':5-31,53-55,58-75', change_all=True)
+self._execute_uf(uf_name='select.display')
 
 # Define the magnetic dipole-dipole relaxation interaction.
-interatom.define(spin_id1='@N', spin_id2='@H', direct_bond=True)
-interatom.set_dist(spin_id1='@N', spin_id2='@H', ave_dist=NH_BOND_LENGTH_RDC)
-interatom.unit_vectors(ave=False)
+self._execute_uf(uf_name='interatom.define', spin_id1='@N', spin_id2='@H', direct_bond=True)
+self._execute_uf(uf_name='interatom.set_dist', spin_id1='@N', spin_id2='@H', ave_dist=NH_BOND_LENGTH_RDC)
+self._execute_uf(uf_name='interatom.unit_vectors', ave=False)
 
 # Set the nuclear isotope and element.
-spin.isotope(isotope='15N', spin_id='@N')
-spin.isotope(isotope='1H', spin_id='@H')
+self._execute_uf(uf_name='spin.isotope', isotope='15N', spin_id='@N')
+self._execute_uf(uf_name='spin.isotope', isotope='1H', spin_id='@H')
 
 # The alignment data.
 align_data = [
@@ -55,57 +55,57 @@ for i in range(len(align_data)):
     FRQ = align_data[i][3]
 
     # RDCs.
-    rdc.read(align_id=TAG, file=RDC_FILE, dir=DATA_PATH, data_type='D', spin_id1_col=1, spin_id2_col=2, data_col=3, error_col=4)
+    self._execute_uf(uf_name='rdc.read', align_id=TAG, file=RDC_FILE, dir=DATA_PATH, data_type='D', spin_id1_col=1, spin_id2_col=2, data_col=3, error_col=4)
 
     # PCSs.
-    pcs.read(align_id=TAG, file=PCS_FILE, dir=DATA_PATH, res_num_col=1, data_col=2, error_col=4, spin_id='@N')
-    pcs.read(align_id=TAG, file=PCS_FILE, dir=DATA_PATH, res_num_col=1, data_col=3, error_col=4, spin_id='@H')
+    self._execute_uf(uf_name='pcs.read', align_id=TAG, file=PCS_FILE, dir=DATA_PATH, res_num_col=1, data_col=2, error_col=4, spin_id='@N')
+    self._execute_uf(uf_name='pcs.read', align_id=TAG, file=PCS_FILE, dir=DATA_PATH, res_num_col=1, data_col=3, error_col=4, spin_id='@H')
 
     # The temperature.
-    spectrometer.temperature(id=TAG, temp=303.0)
+    self._execute_uf(uf_name='spectrometer.temperature', id=TAG, temp=303.0)
 
     # The frequency.
-    spectrometer.frequency(id=TAG, frq=FRQ, units='MHz')
+    self._execute_uf(uf_name='spectrometer.frequency', id=TAG, frq=FRQ, units='MHz')
 
 # The paramagnetic centre (average Ca2+ position).
 ave = array([6.382, 9.047, 14.457]) + array([6.031, 8.301, 13.918]) + array([6.345, 8.458, 13.868])
 ave = ave / 3
-paramag.centre(pos=ave)
+self._execute_uf(uf_name='paramag.centre', pos=ave)
 
 # Set up the model.
-n_state_model.select_model('fixed')
+self._execute_uf(uf_name='n_state_model.select_model', model='fixed')
 
 # Tensor optimisation.
 print("\n\n# Tensor optimisation.\n\n")
-minimise.grid_search(inc=3)
-minimise.execute('newton', constraints=False)
-state.save('devnull', force=True)
+self._execute_uf(uf_name='minimise.grid_search', inc=3)
+self._execute_uf(uf_name='minimise.execute', min_algor='newton', constraints=False)
+self._execute_uf(uf_name='state.save', state='devnull', force=True)
 
 # PCS structural noise.
 print("\n\n# Tensor optimisation with PCS structural noise.\n\n")
-pcs.structural_noise(rmsd=0.3, sim_num=100, file='devnull', force=True)
+self._execute_uf(uf_name='pcs.structural_noise', rmsd=0.3, sim_num=100, file='devnull', force=True)
 
 # Optimisation of everything.
-paramag.centre(fix=False)
-minimise.execute('bfgs', constraints=False)
+self._execute_uf(uf_name='paramag.centre', fix=False)
+self._execute_uf(uf_name='minimise.execute', min_algor='bfgs', constraints=False)
 
 # Monte Carlo simulations.
-monte_carlo.setup(number=3)
-monte_carlo.create_data()
-monte_carlo.initial_values()
-minimise.execute('bfgs', constraints=False, max_iter=5)
-monte_carlo.error_analysis()
+self._execute_uf(uf_name='monte_carlo.setup', number=3)
+self._execute_uf(uf_name='monte_carlo.create_data')
+self._execute_uf(uf_name='monte_carlo.initial_values')
+self._execute_uf(uf_name='minimise.execute', min_algor='bfgs', constraints=False, max_iter=5)
+self._execute_uf(uf_name='monte_carlo.error_analysis')
 
 # Show the tensors.
-align_tensor.display()
+self._execute_uf(uf_name='align_tensor.display')
 
 # Q-factors.
-rdc.calc_q_factors()
-pcs.calc_q_factors()
+self._execute_uf(uf_name='rdc.calc_q_factors')
+self._execute_uf(uf_name='pcs.calc_q_factors')
 
 # Correlation plots.
-rdc.corr_plot(file="devnull", force=True)
-pcs.corr_plot(file="devnull", force=True)
+self._execute_uf(uf_name='rdc.corr_plot', file="devnull", force=True)
+self._execute_uf(uf_name='pcs.corr_plot', file="devnull", force=True)
 
 # Save the program state.
-state.save('devnull', force=True)
+self._execute_uf(uf_name='state.save', state='devnull', force=True)
