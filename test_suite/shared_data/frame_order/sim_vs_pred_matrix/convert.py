8908f9b4d615e9fabee0fb0af6a16ca1fd638f19
Edward d'Auvergne <edward@nmr-relax.com>
2014-11-01 17:04:10 +0000

    Modified the frame order matrix Grace graph to EPS/PNG format conversion script.
    The binary being called is now 'grace' rather than 'xmgrace'.  This allows different Grace versions
    to be used.

--- test_suite/shared_data/frame_order/sim_vs_pred_matrix/convert.py
+++ test_suite/shared_data/frame_order/sim_vs_pred_matrix/convert.py
@@ -17,5 +17,5 @@ for file_name in listdir('.'):
     file_root = basename(file_root)
 
     # Convert to EPS then PDF.
-    system("xmgrace -hdevice EPS -hardcopy -printfile %s.eps %s" % (file_root, file_name))
-    system("xmgrace -hdevice PNG -hardcopy -printfile %s.png %s" % (file_root, file_name))
+    system("grace -hdevice EPS -hardcopy -printfile %s.eps %s" % (file_root, file_name))
+    system("grace -hdevice PNG -hardcopy -printfile %s.png %s" % (file_root, file_name))
