cf478c63567b2baf70dce0b8725a01713ffa3537
Edward d'Auvergne <edward@nmr-relax.com>
2014-11-29 23:57:32 +0000

    Added a script for calculating the expected peak intensities for an inversion recovery curve.
    This is based on the values used by Sébastien Morin in his inversion-recovery branch, as the
    check_curve_fitting_exp_3param_inv_neg() function of the test_suite/system_tests/relax_fit.py file.

---
+++ test_suite/shared_data/curve_fitting/inversion_recovery/calc.py
@@ -0,0 +1,14 @@
+# Python module imports.
+from math import exp
+
+
+# Data.
+relax_times = [0.2, 0.45, 0.75, 0.75, 1.1, 1.5, 1.95, 1.95, 2.45]
+rx = 1.2
+i0 = 30
+iinf = 22
+
+# Calculate I(t) for each t.
+for time in relax_times:
+    print("%s, %s" % (time, iinf - i0 * exp(-rx*time)))
+
