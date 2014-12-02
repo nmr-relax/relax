30f5e263a5e0559b8880cf9d00850c40ef41fcc9
Edward d'Auvergne <edward@nmr-relax.com>
2014-12-02 11:21:37 +0000

    Updated manual_c_module.py development script for the C module changes.
    The numpy and numarray include paths have been added.

--- devel_scripts/manual_c_module.py
+++ devel_scripts/manual_c_module.py
@@ -13,8 +13,10 @@ path = '/data/python/'
 # The list of build commands to run.
 cmd = []
 
-# Python.h.
+# Python.h, numpy/arrayobject.h, and numpy/libnumarray.h.
 include = '%s/include/python%s/' % (path, target)
+include_numpy = "%s/lib/python%s/site-packages/numpy/core/include" % (path, target)
+include_numarray = "%s/lib/python%s/site-packages/numpy/numarray/include" % (path, target)
 if target == '3.2':
     include = '%s/include/python3.2m/' % path
 elif target == '3.3':
@@ -23,11 +25,11 @@ elif target == '3.4':
     include = '%s/include/python3.4m/' % path
 
 # Python 3.2 installed in the home directory.
-cmd.append("gcc -o target_functions/c_chi2.os -c -I%s -fPIC target_functions/c_chi2.c" % include)
-cmd.append("gcc -o target_functions/exponential.os -c -I%s -fPIC target_functions/exponential.c" % include)
-cmd.append("gcc -o target_functions/exponential_inv.os -c -I%s -fPIC target_functions/exponential_inv.c" % include)
-cmd.append("gcc -o target_functions/exponential_sat.os -c -I%s -fPIC target_functions/exponential_sat.c" % include)
-cmd.append("gcc -o target_functions/relax_fit.os -c -I%s -fPIC target_functions/relax_fit.c" % include)
+cmd.append("gcc -o target_functions/c_chi2.os -c -I%s -I%s -I%s -fPIC target_functions/c_chi2.c" % (include, include_numpy, include_numarray))
+cmd.append("gcc -o target_functions/exponential.os -c -I%s -I%s -I%s -fPIC target_functions/exponential.c" % (include, include_numpy, include_numarray))
+cmd.append("gcc -o target_functions/exponential_inv.os -c -I%s -I%s -I%s -fPIC target_functions/exponential_inv.c" % (include, include_numpy, include_numarray))
+cmd.append("gcc -o target_functions/exponential_sat.os -c -I%s -I%s -I%s -fPIC target_functions/exponential_sat.c" % (include, include_numpy, include_numarray))
+cmd.append("gcc -o target_functions/relax_fit.os -c -I%s -I%s -I%s -fPIC target_functions/relax_fit.c" % (include, include_numpy, include_numarray))
 cmd.append("gcc -o target_functions/relax_fit.so -shared target_functions/c_chi2.os target_functions/exponential.os target_functions/exponential_inv.os target_functions/exponential_sat.os target_functions/relax_fit.os")
 
 
