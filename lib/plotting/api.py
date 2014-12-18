e3078693b753a98b663bcd18a2d6802dd052ca7b
Edward d'Auvergne <edward@nmr-relax.com>
2014-12-18 11:05:07 +0000

    Created the lib.plotting API function correlation_matrix().
    This is the lib.plotting.api.correlation_matrix() function.  It will be used for the visualisation
    of rank-2 correlation matrices.  The current basic API design here uses a dictionary of backend
    functions (currently empty) for calling the backend.

--- lib/plotting/api.py
+++ lib/plotting/api.py
@@ -19,10 +19,35 @@
 #                                                                             #
 ###############################################################################
 
-# Package docstring.
-"""The relax library plotting package - a library of functions for plotting data via different software packages."""
+# Module docstring.
+"""The relax library plotting API."""
 
-# The package content list.
-__all__ = [
-    'api'
-]
+# relax module imports.
+from lib.errors import RelaxError
+
+
+def correlation_matrix(format=None, matrix=None, labels=None, file=None, dir=None, force=False):
+    """Plotting API function for representing correlation matrices.
+
+    @keyword format:    The specific backend to use.
+    @type format:       str
+    @keyword matrix:    The correlation matrix.  This must be a square matrix.
+    @type matrix:       numpy rank-2 array.
+    @keyword labels:    The labels for each element of the matrix.  The same label is assumed for each [i, i] pair in the matrix.
+    @type labels:       list of str
+    @keyword file:      The name of the file to create.
+    @type file:         str
+    @keyword dir:       The directory where the PDB file will be placed.  If set to None, then the file will be placed in the current directory.
+    @type dir:          str or None
+    """
+
+    # The supported formats.
+    function = {
+    }
+
+    # Unsupported format.
+    if format not in function:
+        raise RelaxError("The plotting of correlation matrix data using the '%s' format is not supported." % format)
+
+    # Call the backend function.
+    function[format](matrix=matrix, labels=labels, file=file, dir=dir, force=force)
