74aec8edb4d4a2b91a5d6c233d843675900e78a9
Edward d'Auvergne <edward@nmr-relax.com>
2015-01-21 11:21:53 +0000

    Corrected the spelling of the BLOSUM62 matrix in lib.sequence_alignment.substitution_matrices.

--- lib/sequence_alignment/substitution_matrices.py
+++ lib/sequence_alignment/substitution_matrices.py
@@ -26,8 +26,8 @@
 from numpy import array, int16
 
 
-BLOSSUM62_SEQ = ['ARNDCQEGHILKMFPSTWYVBZX*']
-BLOSSUM62 = array([
+BLOSUM62_SEQ = ['ARNDCQEGHILKMFPSTWYVBZX*']
+BLOSUM62 = array([
     [ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
     [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
     [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
