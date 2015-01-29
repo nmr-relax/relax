e15c48b58fa69b71f662bd3ef653bf24ca273491
Edward d'Auvergne <edward@nmr-relax.com>
2015-01-29 17:30:04 +0000

    Corrections for the Structure.test_align_CaM_BLOSUM62 system test.
    The CaM N and C domains can not be aligned together in a global MSA as they would align very well to
    themselves, causing the atomic coordinate assembly function to fail.

--- test_suite/system_tests/scripts/n_state_model/structure_align.py
+++ test_suite/system_tests/scripts/n_state_model/structure_align.py
@@ -16,8 +16,11 @@ for code in codes:
     # Load the structure.
     structure.read_pdb('%s.pdb' % code, dir=status.install_path + sep+'test_suite'+sep+'shared_data'+sep+'structures')
 
-# Backbone heavy atom align all structures.
-structure.align(pipes=codes, atom_id='@N,C,CA,O', method='fit to first', matrix='BLOSUM62', gap_open_penalty=10.0, gap_extend_penalty=1.0, end_gap_open_penalty=0.0, end_gap_extend_penalty=0.0)
+# First align the N-domains - aligning 1OSA onto 1J70 to act as a scaffold.
+structure.align(pipes=['1J7O', '1OSA'], atom_id='@N,C,CA,O', method='fit to first', matrix='BLOSUM62', gap_open_penalty=10.0, gap_extend_penalty=1.0, end_gap_open_penalty=0.0, end_gap_extend_penalty=0.0)
+
+# Then align the C-domains - aligning 1J7P onto the 1OSA scaffold.
+structure.align(pipes=['1OSA', '1J7P'], atom_id='@N,C,CA,O', method='fit to first', matrix='BLOSUM62', gap_open_penalty=10.0, gap_extend_penalty=1.0, end_gap_open_penalty=0.0, end_gap_extend_penalty=0.0)
 
 # Write out the result.
 structure.write_pdb('devnull', force=True)
