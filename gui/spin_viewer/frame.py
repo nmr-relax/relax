2e4acb62f9171b268476a66b1d466356ec6903b0
Edward d'Auvergne <edward@nmr-relax.com>
2015-02-03 09:07:09 +0000

    Created a special Destroy() method for the spin viewer window.
    This is for greater control of the spin viewer window destruction.  First the methods registered
    with the observer objects are unregistered, then the children of the spin viewer window are
    destroyed, and finally the main spin viewer window is destroyed.  This change saves a lot of GUI
    resources in the GUI tests (there is a large reduction in 'User Objects' and 'GDI Objects' used on
    MS Windows systems, hence an equivalent resource reduction on other operating systems).

--- gui/spin_viewer/frame.py
+++ gui/spin_viewer/frame.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2010-2014 Edward d'Auvergne                                   #
+# Copyright (C) 2010-2015 Edward d'Auvergne                                   #
 # Copyright (C) 2013-2014 Troels E. Linnet                                    #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
@@ -119,6 +119,25 @@ class Spin_view_window(wx.Frame):
 
         # The user function menus.
         self.menu_uf_ids = build_uf_menus(parent=self, menubar=self.menubar)
+
+
+    def Destroy(self, event=None):
+        """Cleanly destroy the spin viewer window.
+
+        @keyword event: The wx event.
+        @type event:    wx event
+        """
+
+        # First unregister the methods from the observers.
+        status.observers.gui_uf.unregister(self.name)
+        status.observers.pipe_alteration.unregister(self.name)
+        status.observers.exec_lock.unregister(self.name)
+
+        # Destroy all children of the window.
+        super(Spin_view_window, self).DestroyChildren()
+
+        # Destroy the spin viewer window.
+        super(Spin_view_window, self).Destroy()
 
 
     def Show(self, show=True):
