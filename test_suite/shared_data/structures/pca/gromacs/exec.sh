453d28a210a06eba326a9e845cb1285136ff8853
Edward d'Auvergne <edward@nmr-relax.com>
2015-11-25 17:38:21 +0000

    Updated the Gromacs PCA results for the newest Gromacs version (5.1.1).

--- test_suite/shared_data/structures/pca/gromacs/exec.sh
+++ test_suite/shared_data/structures/pca/gromacs/exec.sh
@@ -1,7 +1,7 @@
 rm -f \#* average.pdb covara.xpm covar* eigen* fws-ev1* proj-*
-echo 0 | g_covar -f ../distribution.pdb -s ../distribution.pdb -nofit -xvgr -o eigenval.xvg -v eigenvec.trr -xpma covar.xpm -l covar.log
-xpm2ps -f covar.xpm -o covar.eps -do covar.m2p
-echo 0 | g_anaeig -v eigenvec.trr -f ../distribution.pdb -s ../distribution.pdb -first 1 -last 1 -nframes 10 -extr fws-ev1.pdb
-echo 0 | g_anaeig -f ../distribution.pdb -s ../distribution.pdb -first 1 -last 2 -2d proj-1-2.xvg
-echo 0 | g_anaeig -f ../distribution.pdb -s ../distribution.pdb -first 2 -last 3 -2d proj-2-3.xvg
-echo 0 | g_anaeig -f ../distribution.pdb -s ../distribution.pdb -first 3 -last 4 -2d proj-3-4.xvg
+echo 0 | gmx covar -f ../distribution.pdb -s ../distribution.pdb -nofit -o eigenval.xvg -v eigenvec.trr -xpma covar.xpm -l covar.log
+gmx xpm2ps -f covar.xpm -o covar.eps -do covar.m2p
+echo 0 | gmx anaeig -v eigenvec.trr -f ../distribution.pdb -s ../distribution.pdb -first 1 -last 1 -nframes 10 -extr fws-ev1.pdb
+echo 0 | gmx anaeig -f ../distribution.pdb -s ../distribution.pdb -first 1 -last 2 -2d proj-1-2.xvg
+echo 0 | gmx anaeig -f ../distribution.pdb -s ../distribution.pdb -first 2 -last 3 -2d proj-2-3.xvg
+echo 0 | gmx anaeig -f ../distribution.pdb -s ../distribution.pdb -first 3 -last 4 -2d proj-3-4.xvg
