f84d7211c1fa71933f48565f78f53ab9c6868974
Troels Emtekær Linnet <tlinnet@nmr-relax.com>
2015-11-28 20:36:46 +0000

    Expanding script for installation.

--- devel_scripts/deploy_google_computing_ubuntu.sh
+++ devel_scripts/deploy_google_computing_ubuntu.sh
@@ -11,4 +11,73 @@ VBMR=`lynx -dump "http://wiki.nmr-relax.com/Template:Current_version_bmrblib" | 
 VMPI=`lynx -dump "http://wiki.nmr-relax.com/Template:Current_version_mpi4py" | grep -A 10 "Template:Current version mpi4py" | grep -B 1 "Retrieved from" | head -n 1 | tr -d '[[:space:]]'`
 VREL=`lynx -dump "http://wiki.nmr-relax.com/Template:Current_version_relax" | grep -A 10 "Template:Current version relax" | grep -B 1 "Retrieved from" | head -n 1 | tr -d '[[:space:]]'`
 
-echo $VMIN $VMBR $VMPI $VREL
+echo "Current version of minfx is: $VMIN"
+echo "Current version of bmrblib is: $VMBR"
+echo "Current version of mpi4py is: $VMPI"
+echo "Current version of relax is: $VREL"
+
+# Install for server management
+sudo apt-get -y install htop
+
+# Install for running relax in multiple CPU mode
+sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
+
+# Then check server
+uptime
+whoami
+lscpu
+mpirun --version
+mpirun --report-bindings -np 4 echo "mpirun with 4 CPU echoes"
+
+# Install dependencies
+sudo apt-get -y install python-numpy
+sudo apt-get -y install python-scipy python-matplotlib python-pip
+sudo pip install mpi4py
+sudo pip install epydoc
+sudo apt-get -y install subversion scons grace
+
+# Make home bin
+mkdir -p $HOME/bin
+echo '' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/bin' >> $HOME/.bashrc
+source $HOME/.bashrc
+
+# Install minfx
+mkdir -p $HOME/Downloads
+cd $HOME/Downloads
+curl http://download.gna.org/minfx/minfx-$VMIN.tar.gz -o minfx-$VMIN.tar.gz
+tar -xzf minfx-$VMIN.tar.gz
+cd minfx-$VMIN
+sudo pip install .
+cd $HOME
+
+# Install bmrblib
+mkdir -p $HOME/Downloads
+cd $HOME/Downloads
+curl http://download.gna.org/bmrblib/bmrblib-$VBMR.tar.gz -o bmrblib-$VBMR.tar.gz
+tar -xzf bmrblib-$VBMR.tar.gz
+cd bmrblib-$VBMR
+sudo pip install .
+cd $HOME
+
+# Get latest compiled version of relax
+curl http://download.gna.org/relax/relax-$VREL.GNU-Linux.x86_64.tar.bz2 -o relax-$VREL.GNU-Linux.x86_64.tar.bz2
+tar xvjf relax-$VREL.GNU-Linux.x86_64.tar.bz2
+rm relax-$VREL.GNU-Linux.x86_64.tar.bz2
+ln -s $HOME/relax-4.0.0/relax $HOME/bin/relax-$VREL
+
+# Get the subversion of relax
+svn co svn://svn.gna.org/svn/relax/trunk relax_trunk
+
+# Build
+cd $HOME/relax_trunk
+scons
+ln -s $HOME/relax_trunk/relax $HOME/bin/relax_trunk
+cd $HOME
+
+# Print info
+which relax
+relax -i
+
+which relax_svn
+relax_svn -i
