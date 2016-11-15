8a947101f32293c13f58548029c23c591ece0d49
Edward d'Auvergne <edward@nmr-relax.com>
2016-11-15 13:47:29 +0100

    wxPython-Phoenix fixes for the GUI sequence and file input elements.
    The wx.Frame.Center() function call only works if the window is shown (i.e. it is broken in the test
    suite).

--- gui/input_elements/file.py
+++ gui/input_elements/file.py
@@ -635,7 +635,8 @@ class Selector_file_window(wx.Dialog):
 
         # Set the frame properties.
         self.SetSize(self.SIZE)
-        self.Centre()
+        if not dep_check.old_wx and status.show_gui:
+            self.Centre()
         self.SetFont(font.normal)
 
         # The main box sizer.
