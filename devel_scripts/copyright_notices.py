c30653225f37f6ee893ea481f887afb8619b9950
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-07 10:22:52 +0200

    FSF compliant copyright notices for all files in the documentation directory docs/latex/.
    This includes a README file with the copyright notices for the binary graphics.

--- devel_scripts/copyright_notices.py
+++ devel_scripts/copyright_notices.py
@@ -175,6 +175,10 @@ COMMITTER_ALT = {
 BLACKLISTED_FILES = [
     '.gitignore',                                               # Trivial file list.
     'docs/COPYING',                                             # The original GPLv3 licence text.
+    'docs/latex/gpl-3.0.tex',                                   # The original GPLv3 licence text.
+    'docs/latex/nth.sty',                                       # Public domain.
+    'docs/latex/relax_version.tex',                             # Single line auto-generated file.
+    'docs/latex/frame_order/compile_param_nesting',             # Trivial script.
 ]
 
 # Directories to skip.
@@ -217,10 +221,15 @@ GIT_START = {
         "Documented the svn to git repository migration. (2017-07-04 15:45:41 +0200)",
     "docs/devel/svn2git_migration/gitsvn_conversion.sh":
         "Documented the svn to git repository migration. (2017-07-04 15:45:41 +0200)",
+    "docs/latex/__init__.py":
+        "A complete shift from the make build system to the scons build system (and a bug fix). (2006-01-30 06:45:35 +0000)",
+    "docs/latex/licence.tex":
+        "Created the 'Licence' chapter in the manual. (2005-10-15 12:42:17 +0000)",
 }
 
 # Additional copyrights that are not present in the git log.
 ADDITIONAL_COPYRIGHT = {
+    'docs/latex/relax.bst' : ["Copyright (C) 1988, all rights reserved."],
 }
 
 # False positives (copyright notices in files to ignore, as they are not in the git log).
--- docs/latex/fetch_docstrings.py
+++ docs/latex/fetch_docstrings.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2005-2014 Edward d'Auvergne                                   #
+# Copyright (C) 2005-2006,2008-2009,2012-2014 Edward d'Auvergne               #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
