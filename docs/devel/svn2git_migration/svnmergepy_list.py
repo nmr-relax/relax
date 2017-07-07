561b217cd4a02a4022a1c6c7d8846096ca921162
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-07 09:12:47 +0200

    FSF compliant copyright notices for all files in the documentation directory docs/devel/.
    This includes two README files with the copyright notices for all of the patches.

--- docs/devel/svn2git_migration/svnmergepy_list.py
+++ docs/devel/svn2git_migration/svnmergepy_list.py
@@ -1,3 +1,25 @@
+###############################################################################
+#                                                                             #
+# Copyright (C) 2017 Edward d'Auvergne                                        #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
+
 SVNMERGEPY_MERGES = [
     ["master", "frame_order_cleanup",
         "r27959", "Merged revisions 26913-26920,26957,27240,27244,27329,27552,27609-27611,27641-27653,27663,27715,27721,27806,27878-27899,27912 via svnmerge from svn+ssh://bugman@svn.gna.org/svn/relax/branches/frame_order_cleanup (2015-10-02 13:10:10 +0000)",
