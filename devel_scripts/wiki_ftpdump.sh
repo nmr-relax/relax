3db7b5039bf0979dd90613befc00cb560defd0cf
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-11 11:51:06 +0200

    FSF compliant copyright notices for the entirety of the devel_scripts/ directory.

--- devel_scripts/wiki_ftpdump.sh
+++ devel_scripts/wiki_ftpdump.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 # -*- coding: UTF-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2013 Troels E. Linnet                                         #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
+
 # Script for dumping ftp server automatically using cron.
 #
 # This script was taken from the post Troels Linnet at: http://article.gmane.org/gmane.science.nmr.relax.devel/4168.
