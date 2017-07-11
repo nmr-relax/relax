f62d28646edf5d12cec9d80101097f178b3952b6
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-11 11:31:20 +0200

    Update to FSF compliant copyright notices for all modules in the data_store package.

--- data_store/interatomic.py
+++ data_store/interatomic.py
@@ -1,6 +1,7 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2007-2014 Edward d'Auvergne                                   #
+# Copyright (C) 2001-2004,2006-2008,2010,2012,2014 Edward d'Auvergne          #
+# Copyright (C) 2006 Chris MacRaild                                           #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
