fcfc342d880e4ee2761e8b24c67edeb521d638d5
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-19 09:08:21 +0200

    Update to FSF compliant copyright notices for all modules in the pipe_control package.

--- pipe_control/opendx.py
+++ pipe_control/opendx.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2014 Edward d'Auvergne                                   #
+# Copyright (C) 2002-2005,2007-2008,2012-2014 Edward d'Auvergne               #
 # Copyright (C) 2014 Troels E. Linnet                                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
--- pipe_control/value.py
+++ pipe_control/value.py
@@ -1,6 +1,7 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2014 Edward d'Auvergne                                   #
+# Copyright (C) 2003-2009,2011-2014 Edward d'Auvergne                         #
+# Copyright (C) 2008 Sebastien Morin                                          #
 # Copyright (C) 2014 Troels E. Linnet                                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
