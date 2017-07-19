864b93d79568004ca6810656ea012d219acc87aa
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-19 13:03:25 +0200

    Update to FSF compliant copyright notices for all modules in the specific_analyses package.

--- specific_analyses/frame_order/api.py
+++ specific_analyses/frame_order/api.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2009-2015 Edward d'Auvergne                                   #
+# Copyright (C) 2009-2011,2013-2014 Edward d'Auvergne                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
--- specific_analyses/model_free/data.py
+++ specific_analyses/model_free/data.py
@@ -1,7 +1,7 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2007-2014 Edward d'Auvergne                                   #
-# Copyright (C) 2007 Gary S Thompson (https://gna.org/users/varioustoxins)    #
+# Copyright (C) 2007-2009,2011,2014 Edward d'Auvergne                         #
+# Copyright (C) 2007 Gary S Thompson                                          #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
