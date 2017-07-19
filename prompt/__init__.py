7cb838af9093cca08c81d13269bca47719bab327
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-19 10:59:45 +0200

    Update to FSF compliant copyright notices for all modules in the prompt package.

--- prompt/__init__.py
+++ prompt/__init__.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2012 Edward d'Auvergne                                   #
+# Copyright (C) 2003-2004,2009,2012 Edward d'Auvergne                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
