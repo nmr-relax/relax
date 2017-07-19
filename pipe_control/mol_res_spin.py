213f9963d30cf2454eaa3c13d1da4337739e895c
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-19 09:08:21 +0200

    Update to FSF compliant copyright notices for all modules in the pipe_control package.

--- pipe_control/mol_res_spin.py
+++ pipe_control/mol_res_spin.py
@@ -1,6 +1,7 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2003-2015 Edward d'Auvergne                                   #
+# Copyright (C) 2003-2004,2006-2014 Edward d'Auvergne                         #
+# Copyright (C) 2006-2007 Chris MacRaild                                      #
 # Copyright (C) 2014 Troels E. Linnet                                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
