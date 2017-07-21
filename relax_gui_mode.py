649adab27c14a17da78e079c7a345080e5c6ff9f
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-21 13:10:55 +0200

    Update to FSF compliant copyright notices for all modules and files in the base relax directory.

--- relax_gui_mode.py
+++ relax_gui_mode.py
@@ -1,4 +1,25 @@
 #! /usr/bin/env python
+###############################################################################
+#                                                                             #
+# Copyright (C) 2001-2011 Edward d'Auvergne                                   #
+# Copyright (C) 2008 Sebastien Morin                                          #
+#                                                                             #
+# This file is part of the program relax (http://www.nmr-relax.com).          #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
 
 # This is a transient solution to forcing the GUI - a better solution would be an environmental variable.
 
