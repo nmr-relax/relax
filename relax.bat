8f6b8974a35d8c5dc79d98614f27df49d631a719
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-21 13:10:55 +0200

    Update to FSF compliant copyright notices for all modules and files in the base relax directory.

--- relax.bat
+++ relax.bat
@@ -1,4 +1,22 @@
 @echo off
+rem Copyright (C) 2006 Edward d'Auvergne
+rem
+rem This file is part of the program relax (http://www.nmr-relax.com).
+rem
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
+rem
+rem
 rem Execute this file to run relax in MS Windows.
 rem
 rem Both relax and python must be located within the system path.
