0e2addc8b1b4cb38b28fe5a4af662808a40543e8
Edward d'Auvergne <edward@nmr-relax.com>
2017-07-27 18:12:23 +0200

    Update to FSF compliant copyright notices for all system test modules and scripts.

--- test_suite/system_tests/consistency_tests.py
+++ test_suite/system_tests/consistency_tests.py
@@ -2,7 +2,7 @@
 #                                                                             #
 # Copyright (C) 2006 Chris MacRaild                                           #
 # Copyright (C) 2007-2008 Sebastien Morin                                     #
-# Copyright (C) 2010-2013 Edward d'Auvergne                                   #
+# Copyright (C) 2007,2010 Edward d'Auvergne                                   #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
