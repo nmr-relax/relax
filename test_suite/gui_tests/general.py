b71a993e5bd5c7d3747c085f010abe0f59f44352
Edward d'Auvergne <edward@nmr-relax.com>
2017-08-02 11:51:07 +0200

    Update to FSF compliant copyright notices for all GUI test modules.

--- test_suite/gui_tests/general.py
+++ test_suite/gui_tests/general.py
@@ -1,6 +1,7 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2014-2015 Edward d'Auvergne                                   #
+# Copyright (C) 2014 Edward d'Auvergne                                        #
+# Copyright (C) 2015 Troels E. Linnet                                         #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
 #                                                                             #
