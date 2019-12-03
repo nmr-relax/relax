nmr-relax/relax

commit 7ba8a595ccbe4fe09513930e7d7e7c40d33de2c2

Python 3.8 support: The platform.linux_distibution() function no longer exists.
This is now replaced by the distro site-package.  The lib.compat package deals with this difference.

lib/compat.py
@@ -120,6 +120,12 @@ try:
 except:
     numpy_norm_axis = False
 
+# Linux distribution information.
+try:
+    from platform import linux_distribution
+except ImportError:
+    from distro import linux_distribution
+
 
 def bz2_open(file, mode='r'):
     """Abstract the numerous ways BZ2 files are handled in Python.

info.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #                                                                             #
-# Copyright (C) 2001-2014,2016-2017 Edward d'Auvergne                         #
+# Copyright (C) 2001-2014,2016-2017,2019 Edward d'Auvergne                    #
 # Copyright (C) 2008 Sebastien Morin                                          #
 #                                                                             #
 # This file is part of the program relax (http://www.nmr-relax.com).          #
@@ -47,6 +47,7 @@ import sys
 from textwrap import wrap
 
 # relax module imports.
+from lib.compat import linux_distribution
 from status import Status; status = Status()
 from version import repo_head, repo_type, repo_url, version, version_full
 
@@ -818,8 +819,8 @@ class Info_box(object):
             text = text + (format % ("Version: ", platform.version()))
         if hasattr(platform, 'win32_ver') and platform.win32_ver()[0]:
             text = text + (format % ("Win32 version: ", (platform.win32_ver()[0] + " " + platform.win32_ver()[1] + " " + platform.win32_ver()[2] + " " + platform.win32_ver()[3])))
-        if hasattr(platform, 'linux_distribution') and platform.linux_distribution()[0]:
-            text = text + (format % ("GNU/Linux version: ", (platform.linux_distribution()[0] + " " + platform.linux_distribution()[1] + " " + platform.linux_distribution()[2])))
+        if linux_distribution()[0]:
+            text = text + (format % ("GNU/Linux version: ", (linux_distribution()[0] + " " + linux_distribution()[1] + " " + linux_distribution()[2])))
         if hasattr(platform, 'mac_ver') and platform.mac_ver()[0]:
             text = text + (format % ("Mac version: ", (platform.mac_ver()[0] + " (" + platform.mac_ver()[1][0] + ", " + platform.mac_ver()[1][1] + ", " + platform.mac_ver()[1][2] + ") " + platform.mac_ver()[2])))
         if hasattr(platform, 'dist'):

test_suite/system_tests/frame_order.py
@@ -30,6 +30,7 @@ from tempfile import mkdtemp
 # relax module imports.
 from data_store import Relax_data_store; ds = Relax_data_store()
 import dep_check
+from lib.compat import linux_distribution
 from lib.frame_order.conversions import create_rotor_axis_alpha, create_rotor_axis_spherical
 from lib.frame_order.variables import MODEL_DOUBLE_ROTOR, MODEL_FREE_ROTOR, MODEL_ISO_CONE, MODEL_ISO_CONE_FREE_ROTOR, MODEL_ISO_CONE_TORSIONLESS, MODEL_PSEUDO_ELLIPSE, MODEL_PSEUDO_ELLIPSE_TORSIONLESS, MODEL_RIGID, MODEL_ROTOR
 from lib.geometry.coord_transform import cartesian_to_spherical
@@ -43,7 +44,7 @@ SYSTEM = platform.system()
 RELEASE = platform.release()
 VERSION = platform.version()
 WIN32_VER = platform.win32_ver()
-DIST = platform.dist()
+DIST = linux_distribution()
 ARCH = platform.architecture()
 MACH = platform.machine()
 PROC = platform.processor()

test_suite/system_tests/model_free.py
@@ -32,6 +32,7 @@ from tempfile import mkdtemp, mkstemp
 from auto_analyses.dauvergne_protocol import dAuvergne_protocol
 from data_store import Relax_data_store; ds = Relax_data_store()
 import dep_check
+from lib.compat import linux_distribution
 from pipe_control import pipes
 from pipe_control.interatomic import interatomic_loop
 from pipe_control.mol_res_spin import spin_loop
@@ -47,7 +48,7 @@ SYSTEM = platform.system()
 RELEASE = platform.release()
 VERSION = platform.version()
 WIN32_VER = platform.win32_ver()
-DIST = platform.dist()
+DIST = linux_distribution()
 ARCH = platform.architecture()
 MACH = platform.machine()
 PROC = platform.processor()

commit bb4e9eef0961026891d06f9d23c15913056c7a59

Fix for Python 2.5 support - the bug was introduced in 7ba8a595ccbe4fe09513930e7d7e7c40d33de2c2.

lib/compat.py
@@ -124,7 +124,10 @@ except:
 try:
     from platform import linux_distribution
 except ImportError:
-    from distro import linux_distribution
+    try:
+        from distro import linux_distribution
+    except ImportError:
+        def linux_distribution(): return [None]
 
 
 def bz2_open(file, mode='r'):